d6a8dadc398920d1b14cf9486217e871c40a37f7
andrew <gagasusrock@gmail.com>
2022-04-08 18:54:47 +0300

    10 robot, state monads

---
+++ clean-architecture/10_robot/api.py
@@ -0,0 +1,44 @@
+import pure_robot
+from functools import reduce
+
+def transfer_to_cleaner(message):
+    print(message)
+
+def createDefaultState():
+    return pure_robot.createDefaultState()
+
+def activate_cleaner(code):
+
+    def move(cmd):
+        return lambda state: pure_robot.move(transfer_to_cleaner,int(cmd[1]),state)
+
+    def turn(cmd):
+        return lambda state: pure_robot.turn(transfer_to_cleaner,int(cmd[1]),state)
+
+    def set_state(cmd):
+        return lambda state: pure_robot.set_state(transfer_to_cleaner,cmd[1],state)
+
+    def start(cmd):
+        return lambda state: pure_robot.start(transfer_to_cleaner,state)
+
+    def stop(cmd):
+        return lambda state: pure_robot.stop(transfer_to_cleaner,state)
+
+    def bind(f1, f2):
+        return lambda x : f2(f1(x))
+        
+    def bindAll(funcs):  
+        return reduce(bind, funcs)
+    
+    def createFunc(command):
+        cmd = command.split(' ')
+        return funcs[cmd[0]](cmd)
+
+    funcs = {
+        'move': move,
+        'turn': turn,
+        'set': set_state,
+        'start': start,
+        'stop': stop,
+    }
+    return bindAll(list(map(createFunc, code)))
---
+++ clean-architecture/10_robot/main.py
@@ -0,0 +1,14 @@
+from api import activate_cleaner, createDefaultState
+
+if __name__ == '__main__':
+
+    func = activate_cleaner((
+        'move 100',
+        'turn -90',
+        'set soap',
+        'start',
+        'move 50',
+        'stop'
+    ))
+    state = func(createDefaultState())
+    print(state.x, state.y, state.angle, state.state)
