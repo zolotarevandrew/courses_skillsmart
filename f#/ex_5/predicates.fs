a59713c84014b5a01393afe21df91451cb987db5
Andrey Zolotarev <a.zolotarev@pnlfin.tech>
2022-08-28 11:22:44 +0300

    f# predicates fix func format

--- f#/ex_5/predicates.fs
+++ f#/ex_5/predicates.fs
@@ -1,5 +1,5 @@
 // 16.1
-let notDivisible (m,n) = m % n = 0
+let notDivisible (n,m) = m % n = 0
 
 // 16.2
 let prime n = 
@@ -8,5 +8,5 @@ let prime n =
     let rec primeInternal = function
       | (n, c) when n <= 1 -> false
       | (n, c) when c < 2 -> true
-      | (n, c) -> notDivisible(n, c) = false && primeInternal(n, c - 1)
+      | (n, c) -> notDivisible(c, n) = false && primeInternal(n, c - 1)
     primeInternal(n, iterations)
