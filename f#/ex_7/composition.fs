97acd39028869151e3a17a7a43bbe3160d70b65c
Andrey Zolotarev <a.zolotarev@pnlfin.tech>
2022-08-31 21:46:53 +0300

    f# 7 fix syntax

--- f#/ex_7/composition.fs
+++ f#/ex_7/composition.fs
@@ -1,5 +1,5 @@
 // 20.3.1
-let vat n x  =
+let vat n x =
     let nn: int = n
     let xx: float = x
     if xx <= 0 || xx > 100 then
@@ -8,13 +8,13 @@ let vat n x  =
         float nn + ((float nn) / 100.0) * xx
 
 // 20.3.2
-let unvat n x = 
+let unvat n x =
     let nn: int = n
     let xx: float = x
     (float (xx - float nn) / float nn) * 100.0
 
 // 20.3.3
-let rec min f = 
+let rec min f =
     let rec minInternal = function
       | (f, n: int) when f n = 0 -> n
       | (f, n: int) -> minInternal(f, n + 1)
