2daa65b970754361e2708fa882eb16e669c875b9
Andrey Zolotarev <a.zolotarev@pnlfin.tech>
2022-09-20 19:13:04 +0300

    f# ex 20 seq

---
+++ f#/ex_20/seq.fs
@@ -0,0 +1,17 @@
+// 49.5.1
+let even_seq = Seq.initInfinite (fun i -> 2 * i)
+
+// 49.5.2
+let fac_seq = 
+    let fact n = 
+      let rec _fact v acc = 
+        match v with
+          | 0 -> acc
+          | 1 -> acc
+          | _ -> _fact (v - 1) (acc * v)
+      _fact n 1
+    Seq.initInfinite fact
+
+
+// 49.5.3
+let seq_seq = Seq.initInfinite (fun i -> if i % 2 = 0 then i / 2 else -(i+1)/2)
