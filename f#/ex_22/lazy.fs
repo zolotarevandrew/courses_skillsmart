0e349e43930f5d834e0958a8cf431b14e980dadd
Andrey Zolotarev <a.zolotarev@pnlfin.tech>
2022-09-24 20:56:18 +0300

    f# 22 lazy

---
+++ f#/ex_22/lazy.fs
@@ -0,0 +1,22 @@
+type 'a cell = Nil | Cons of 'a * Lazy<'a cell>
+
+let hd (s : 'a cell) : 'a =
+  match s with
+    Nil -> failwith "hd"
+  | Cons (x, _) -> x
+
+let tl (s : 'a cell) : Lazy<'a cell> =
+  match s with
+    Nil -> failwith "tl"
+  | Cons (_, g) -> g
+
+// 51.3
+let rec nth (s : 'a cell) (n : int) : 'a = 
+   let rec loop iter ls = 
+     match (hd ls, tl ls) with
+       | (head, tail) when iter = n -> head
+       | (head, tail) -> loop (iter + 1) (tail.Force())
+   loop 0 s
+
+
+
