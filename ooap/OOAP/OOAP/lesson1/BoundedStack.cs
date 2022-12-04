997c02d729c69a94dca1f7044ec3d4003a8ab36c
Andrey Zolotarev <a.zolotarev@pnlfin.tech>
2022-12-04 13:35:03 +0300

    lesson1: bounded stack first version

--- ooap/OOAP/OOAP/lesson1/BoundedStack.cs
+++ ooap/OOAP/OOAP/lesson1/BoundedStack.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Drawing;
 
 namespace OOAP.lesson1
 {
@@ -12,17 +11,16 @@ namespace OOAP.lesson1
         /// постусловие: создан новый стек c размером = maxElements
         public BoundedStack(uint maxElements)
         {
-            
+            MaxElements = maxElements;
         }
         
         /// постусловие: создан новый стек c размером = DefaultMaxElements
         protected BoundedStack()
         {
-            
+            MaxElements = DefaultMaxElements;
         }
 
-        // команды 
-        
+ 
         /// предусловие, стэк не пустой
         public abstract T Peek();
         
@@ -38,20 +36,20 @@ namespace OOAP.lesson1
         public abstract void Clear();
         
         
-        // запросы, в форме getter-ов удобнее вызывать - не смешивается с protected set, чтобы при необходимости добавить логику
-        
         /// статус команды pop
         public abstract BoundedStackPopStatus PopStatus { get; }
+        
         /// статус команды push
         public abstract BoundedStackPushStatus PushStatus { get; }
+        
         /// статус команды peek
         public abstract BoundedStackPeekStatus PeekStatus { get; }
         
         /// текущее количество элементов стэка
         public abstract int Size { get; }
-        
+
         /// максимально допустимое количество элементов в стеке
-        public abstract int MaxElements { get; }
+        public uint MaxElements { get; }
     }
     
     public enum BoundedStackPopStatus
@@ -81,9 +79,7 @@ namespace OOAP.lesson1
         private BoundedStackPushStatus _pushStatus;
         private BoundedStackPeekStatus _peekStatus;
         private int _size;
-        private int _maxElements;
         private List<T> _store;
-        
         
         public override T Peek()
         {
@@ -129,7 +125,7 @@ namespace OOAP.lesson1
 
         public override void Clear()
         {
-            _store = new List<T>(_maxElements);
+            _store = new List<T>((int)MaxElements);
             _peekStatus = BoundedStackPeekStatus.None;
             _pushStatus = BoundedStackPushStatus.None;
             _popStatus = BoundedStackPopStatus.None;
@@ -140,6 +136,5 @@ namespace OOAP.lesson1
         public override BoundedStackPushStatus PushStatus => _pushStatus;
         public override BoundedStackPeekStatus PeekStatus => _peekStatus;
         public override int Size => _size;
-        public override int MaxElements => _maxElements;
     }
 }
