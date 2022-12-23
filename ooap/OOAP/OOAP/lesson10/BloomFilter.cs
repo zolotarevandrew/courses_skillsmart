00b1ab4c7a2b79f43b78942f34a4611d83bc9d50
Andrey Zolotarev <a.zolotarev@pnlfin.tech>
2022-12-23 10:08:59 +0300

    lesson10/BloomFilter.cs

---
+++ ooap/OOAP/OOAP/lesson10/BloomFilter.cs
@@ -0,0 +1,20 @@
+﻿namespace OOAP.lesson10
+{
+    public abstract class BloomFilter<T>
+    {
+        //конструктор
+        public BloomFilter()
+        {
+            
+        }
+
+        //команды
+        
+        //постусловие элемент добавлен
+        public abstract void PutValue(T value);
+        
+        //запросы
+        public abstract bool HasValue(T value);
+        
+    }
+}
