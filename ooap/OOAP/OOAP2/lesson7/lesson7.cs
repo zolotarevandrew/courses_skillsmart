c6dd0bec994ea174332608f88806dbed539ce4b7
Andrey Zolotarev <a.zolotarev@pnlfin.tech>
2023-01-17 19:31:50 +0300

    ooap2: lesson7

---
+++ ooap/OOAP/OOAP2/lesson7/lesson7.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OOAP2.lesson7
+{
+    public class SendResult
+    {
+        public bool Ok { get; set; }
+        public string MessageId { get; set; }
+    }
+    
+    public class SlackNotifier
+    {
+        public async Task<SendResult> Notify(string message)
+        {
+            await Task.CompletedTask;
+            return new SendResult
+            {
+                Ok = true,
+                MessageId = "slack"
+            };
+        }
+    }
+
+    public class TelegramNotifier
+    {
+        public async Task<SendResult> Notify(string message)
+        {
+            await Task.CompletedTask;
+            return new SendResult
+            {
+                Ok = true,
+                MessageId = "telegram"
+            };
+        }
+    }
+
+    public class RequestHandler
+    {
+        // В c# динамическое связывание доступно только через type dynamic
+        // В данном случае, возлагаем на компилятор проверку, чтобы передавался правильный типа notifier-а, с методом и результатом
+        public async Task Execute(dynamic notifier, string message)
+        {
+            SendResult result = await notifier.Notify(message);
+            if (result.Ok)
+            {
+                Console.WriteLine(result.MessageId);
+            }
+        }
+    }
+    
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var slackNotifier = new SlackNotifier();
+
+            var telegramNotifier = new TelegramNotifier();
+
+            var rqHandler = new RequestHandler();
+
+            await rqHandler.Execute(slackNotifier, "new user added");
+            await rqHandler.Execute(telegramNotifier, "new user added");
+            
+            //выведет
+            
+            //slack
+            //telegram
+        }
+    }
+}
---
+++ ooap/OOAP/TestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using OOAP2.lesson7;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var slackNotifier = new SlackNotifier();
+
+            var telegramNotifier = new TelegramNotifier();
+
+            var rqHandler = new RequestHandler();
+
+            await rqHandler.Execute(slackNotifier, "new user added");
+            await rqHandler.Execute(telegramNotifier, "new user added");
+        }
+    }
+}
