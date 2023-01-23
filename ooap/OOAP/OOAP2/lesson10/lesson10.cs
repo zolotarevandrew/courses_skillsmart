dee7689179d14bb270d55c6ef646fbabafa3651e
Andrey Zolotarev <a.zolotarev@pnlfin.tech>
2023-01-23 18:42:45 +0300

    ooap2: lesson10

---
+++ ooap/OOAP/OOAP2/lesson10/lesson10.cs
@@ -0,0 +1,30 @@
+﻿namespace OOAP2.lesson10
+{
+    public class General
+    {
+        public virtual General Clone()
+        {
+            return default;
+        }
+    }
+    
+    //возможно только таким хаком, когда приходится наследоваться и не давать дальше по цепочке переопределять методы
+    public class SealedGeneral : General
+    {
+        public sealed override General Clone()
+        {
+            return default;
+        }
+    }
+
+    public class Any : SealedGeneral
+    {
+        //compile error
+        /*
+        public override General Clone()
+        {
+            return default;
+        }
+        */
+    }
+}
