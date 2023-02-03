f84654dfbbf0de96c63d538b555a97aae9ab9bb6
Andrey Zolotarev <a.zolotarev@pnlfin.tech>
2023-02-03 18:19:31 +0300

    ooap2: lesson 15

---
+++ ooap/OOAP/OOAP2/lesson15/lesson15.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OOAP2.lesson15;
+
+
+public enum TicketFromType
+{
+    User = 1,
+    ExternalService = 2
+}
+
+public class TicketV1
+{
+    public Guid Id { get; init; }
+    public TicketFromType From { get; init; }
+    public string UserName { get; init; }
+    public string ExternalServiceName { get; init; }
+}
+
+public class TicketV2
+{
+    public Guid Id { get; init; }
+}
+
+
+public class UserTicket : TicketV2
+{
+    public string UserName { get; init; }
+}
+
+public class ExternalServiceTicket : TicketV2
+{
+    public string ExternalServiceName { get; init; }
+}
+
+// вместо if (ticket.From == TicketFromType.User 
+// используем типизированные тикеты UserTicket, ExternalServiceTicket, в БД это может храниться как модель TicketV1, но эта модель будет преобразована в зависимости от типа
