d7ce3f0d4933237e006afafd7d2d63195019d638
Andrey Zolotarev <a.zolotarev@pnlfin.tech>
2023-02-05 20:21:28 +0300

    ooap2: lesson16

---
+++ ooap/OOAP/OOAP2/lesson16/lesson16.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OOAP2.lesson16;
+
+public interface IConsumer<T>
+{
+    Task ProcessBatchAsync(IEnumerable<T> data);
+}
+
+public class RabbitConsumer<T> : IConsumer<T>
+{
+    public Task ProcessBatchAsync(IEnumerable<T> data)
+    {
+        return Task.CompletedTask;
+    }
+}
+
+public class InMemoryConsumer<T> : IConsumer<T>
+{
+    public Task ProcessBatchAsync(IEnumerable<T> data)
+    {
+        return Task.CompletedTask;
+    }
+}
+
+public class Event {}
+public class UserEvent : Event {}
+
+
+public static class Lesson16Program
+{
+    public static void Main(string[] args)
+    {
+        //x = y
+        IConsumer<Event> consumer = new RabbitConsumer<Event>();
+        
+        //ienumerable ковариантный интерфейс
+        consumer.ProcessBatchAsync(new List<Event>());
+        
+        //x.foo(z)
+        consumer.ProcessBatchAsync(new List<UserEvent>());
+    }
+}
+    
+
