57133bcab8aaca8f13b285c578c57a878f3b50b0
Andrey Zolotarev <a.zolotarev@pnlfin.tech>
2023-02-14 20:26:32 +0300

    ooap2: lesson20

---
+++ ooap/OOAP/OOAP2/lesson20/lesson20.cs
@@ -0,0 +1,9 @@
+﻿namespace OOAP2.lesson20;
+
+
+//наследование вариаций, базовый класс TicketEditedNotification отправляет нотификацию об изменении тикета,
+//и класс TicketEditedWithChangesNotification переопределяет логику TicketEditedNotification добавляя дополнительный текст в нотификацию со списком изменений
+
+//наследование с конкретизацией, базовый класс HttpAuthorizationHandler с набором предопределенных методов через http контекст и его реализации BasicAuthAuthorizationHandler, JwtTokenAuthorizationHandler
+
+//структурное наследование, класс CompanyTotalRisk реализует интерфейс IRiskEntity, позволяющий считать риски по сущностям IRiskEntity из списка или другим образом
