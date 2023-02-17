4495db78783c4d0ac91348d8b3f9bea3c34e4600
Andrey Zolotarev <a.zolotarev@pnlfin.tech>
2023-02-17 19:23:33 +0300

    ooap2: lesson21

---
+++ ooap/OOAP/OOAP2/lesson21/lesson21.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OOAP2.lesson21;
+
+//наследование реализации
+
+public abstract class Notification
+{
+    public abstract Task Send(string message);
+}
+
+public class SlackNotification : Notification
+{
+    public override Task Send(string message)
+    {
+        Console.WriteLine("message sended to slack " + message);
+        return Task.CompletedTask;
+    }
+}
+
+public abstract class Alert : SlackNotification
+{
+    
+}
+
+public class DeployFailedAlert : Alert
+{
+    //наследуем реализацию слак нотификации, для более удобной отсылки Алертов
+}
+
+//льготное наследование
+//P.S подсказал chat gpt:)
+
+public interface ILogger
+{
+    void Log(string message);
+}
+
+public class FileLogger : ILogger
+{
+    public void Log(string message)
+    {
+        // Log the message to a file
+    }
+}
+
+public class ConsoleLogger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
