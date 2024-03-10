8f57e9ef77e963b2d5b9600310b49d12b5b5c494
Andrey Zolotarev <a.zolotarev@pnlfin.tech>
2024-03-10 11:15:04 +0200

    hard_work: lesson43

---
+++ hard_work/h_work/lesson43/example1/Example1.cs
@@ -0,0 +1,81 @@
+﻿namespace h_work.lesson43.example1;
+
+public enum EVerificationListType
+{
+    Selectable,
+    Strict
+}
+
+public class Person
+{
+    public Guid Id { get; set; }
+}
+
+public class PersonVerification
+{
+    public bool IsSuccess { get; set; }
+    public bool IsFraud { get; set; }
+    public bool IsFailed { get; set; }
+    public bool IsPending { get; set; }
+    public bool IsPossibleRetry { get; set; }
+    public Person Person { get; set; }
+
+    public bool IsLinkShared()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public record PersonVerificationGroup(
+    List<PersonVerification> Items,
+    EVerificationListType ListType,
+    Guid? RegistratorPersonId)
+{
+    public PersonVerification Registrator => Items.FirstOrDefault(x => x.Person.Id == RegistratorPersonId);
+    public List<PersonVerification> WithoutRegistratorItems => Items.Where(x => x.Person.Id != RegistratorPersonId).ToList();
+    public PersonVerification NonRegistratorWithReverification => WithoutRegistratorItems.FirstOrDefault(x => x.IsPossibleRetry);
+
+
+    public bool HasOnlyRegistrator => Registrator != null && WithoutRegistratorItems.Count == 0;
+
+    public bool IsRegistratorSharedLinks
+    {
+        get
+        {
+            if (Registrator is null) return false;
+            bool isRegistratorFinished = Registrator.IsSuccess || Registrator.IsPending;
+            if (!isRegistratorFinished) return false;
+            if (HasOnlyRegistrator) return true;
+
+            if (ListType == EVerificationListType.Selectable) return WithoutRegistratorItems.Any(it => it.IsLinkShared());
+            if (ListType == EVerificationListType.Strict) return WithoutRegistratorItems.All(it => it.IsLinkShared());
+
+            throw new NotImplementedException("Unknown list type " + ListType);
+        }
+    }
+
+    public bool IsFraud => Items.Any(v => v.IsFraud);
+    public bool IsFailed => Items.Any(v => v.IsFailed);
+
+    public bool IsSuccess
+    {
+        get
+        {
+            if (ListType == EVerificationListType.Selectable)
+            {
+                if (Registrator.IsSuccess && HasOnlyRegistrator) return true;
+
+                return Registrator.IsSuccess && WithoutRegistratorItems.Any(it => it.IsSuccess);
+            }
+
+            if (ListType == EVerificationListType.Strict)
+            {
+                return Registrator.IsSuccess && WithoutRegistratorItems.All(it => it.IsSuccess);
+            }
+
+            throw new NotImplementedException("Unknown list type " + ListType);
+        }
+    }
+
+    public bool IsCompleted => IsFailed || IsFraud || IsSuccess;
+}
---
+++ hard_work/h_work/lesson43/example2/Example2.cs
@@ -0,0 +1,86 @@
+﻿namespace h_work.lesson43.example2;
+
+public enum EAdditionalQuestionsSessionStatus
+{
+    Created,
+    Started,
+    Finished
+}
+
+public class AdditionalQuestionsSessionState
+{
+    public Guid Id { get; set; }
+    public EAdditionalQuestionsSessionStatus Status { get; set; }
+}
+
+public class AdditionalQuestionInfo
+{
+    public Guid Id { get; set; }
+    public EQuestionType Type { get; set; }
+    public bool IsAnswered { get; set; }
+}
+
+public class EQuestionType
+{
+    
+}
+
+public interface IFinomAdditionalQuestionsSessionOld
+{
+    Guid Id { get; }
+    EAdditionalQuestionsSessionStatus Status { get; }
+
+    /// <summary>
+    /// Creates internal state to persist
+    /// </summary>
+    /// <returns>A new copy of internal state</returns>
+    AdditionalQuestionsSessionState GetInternalState();
+
+    IReadOnlyList<AdditionalQuestionInfo> AllQuestions();
+    IReadOnlyList<EQuestionType> GetQuestionTypes();
+    bool HasSimpleQuestions();
+    bool HasAnsweredQuestionsOfType(EQuestionType type);
+    AdditionalQuestionInfo CurrentQuestionInfo();
+    bool MoveBack();
+    bool MoveNext();
+    bool HasNext();
+    bool HasPrevious();
+
+    /// <summary>
+    /// Adds new questions to a session, skips questions which are already in a session
+    /// </summary>
+    /// <param name="session">Current questions session</param>
+    /// <param name="questions">Questions from dossier to add</param>
+    /// <returns>Returns question id's and types of added questions</returns>
+    List<AdditionalQuestionInfo> AddQuestions(IEnumerable<AdditionalQuestionInfo> questions);
+
+    bool TrySetCurrentAnsweredQuestion(Guid? questionId);
+    void Start();
+    void Finish();
+}
+
+public interface IFinomAdditionalQuestionsSessionV2
+{
+    AdditionalQuestionsSessionStateV2 State { get; }
+
+    List<AdditionalQuestionInfo> AddQuestions(IEnumerable<AdditionalQuestionInfo> questions);
+
+    void Start();
+    void Finish();
+}
+
+public class AdditionalQuestionsSessionStateV2
+{
+    public Guid Id { get; set; }
+    public EAdditionalQuestionsSessionStatus Status { get; init; }
+
+    public bool MoveBack()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool MoveNext()
+    {
+        throw new NotImplementedException();
+    }
+}
