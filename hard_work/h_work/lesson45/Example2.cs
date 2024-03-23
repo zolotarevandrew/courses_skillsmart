1ef438c52b70d085fb681f44557a5251fca2fb71
Andrey Zolotarev <a.zolotarev@pnlfin.tech>
2024-03-23 21:02:17 +0200

    hard_work: lesson45

---
+++ hard_work/h_work/lesson45/Example2.cs
@@ -0,0 +1,79 @@
+﻿namespace h_work.lesson45;
+
+public class RelatedDocument {}
+
+public class ActiveDocument
+{
+    public EDocumentType Type { get; set; }
+}
+public class DocumentRelation {}
+public enum EDocumentType {}
+public class Document {}
+public class TemporaryFile {}
+
+public class UploadDocumentFilesContract {}
+
+public interface IRelatedDocumentsService
+{
+    Task<RelatedDocument?> GetById(Guid documentId);
+
+    Task<ActiveDocument?> GetLastActiveOrDefault(DocumentRelation relation, EDocumentType documentType);
+
+    Task UploadDocumentFiles(UploadDocumentFilesContract dataContract);
+
+    public Task<ActiveDocument> Add(
+        DocumentRelation relation,
+        Document document,
+        params TemporaryFile[] files);
+}
+
+public class InactiveDocument
+{
+    public EDocumentType Type { get; set; }
+}
+
+public class DocumentsGroup
+{
+    private readonly List<ActiveDocument> _documents;
+    private readonly List<InactiveDocument> _inactiveDocuments;
+
+    public DocumentsGroup(List<ActiveDocument> documents, List<InactiveDocument> inactiveDocuments)
+    {
+        _documents = documents;
+        _inactiveDocuments = inactiveDocuments;
+    }
+    
+    public ActiveDocument? LastActive(EDocumentType documentType)
+    {
+        return _documents.FirstOrDefault(x => x.Type == documentType);
+    }
+    
+    public ActiveDocument? LastActive()
+    {
+        return _documents.FirstOrDefault();
+    }
+    
+    public InactiveDocument? LastInactive(EDocumentType documentType)
+    {
+        return _inactiveDocuments.FirstOrDefault(x => x.Type == documentType);
+    }
+    
+    public InactiveDocument? LastInactive()
+    {
+        return _inactiveDocuments.FirstOrDefault();
+    }
+}
+
+public interface IRelatedDocumentsV2Service
+{
+    Task<RelatedDocument?> GetById(Guid documentId);
+
+    Task<DocumentsGroup> GetRelated(DocumentRelation relation, params EDocumentType[] documentTypes);
+
+    Task UploadDocumentFiles(UploadDocumentFilesContract dataContract);
+
+    public Task<ActiveDocument> Add(
+        DocumentRelation relation,
+        Document document,
+        params TemporaryFile[] files);
+}
---
+++ hard_work/h_work/lesson45/Example3.cs
@@ -0,0 +1,65 @@
+﻿namespace h_work.lesson45;
+
+public class DataCollectionModel 
+{
+}
+
+public class DataCollectionContext 
+{
+}
+
+public class DataCollectionClientState {}
+
+public class DataCollectionInfo {}
+
+public interface IDataCollectionService
+{
+    Task<DataCollectionModel> Init(DataCollectionContext context, DateTime startedAt);
+   
+    Task<DataCollectionClientState> GetClientState(DataCollectionModel state, Guid userId, string language);
+    Task<DataCollectionInfo> BuildInfo(DataCollectionModel state);
+    
+    Task<DataCollectionModel?> GetState(DataCollectionContext context);
+    Task<DataCollectionModel?> GetState(Guid dataCollectionId);
+}
+
+public enum EDataCollectionStatus {
+}
+
+public class DataCollectionState {
+    public Guid Id { get; }
+    public Guid CompanyId { get; }
+    public Guid UserId { get; }
+    public string CountryCode { get; }
+}
+
+public abstract class DataCollection 
+{
+    public DataCollectionState State { get; }
+    public DataPointsStates DataPoints { get; }
+     
+    public abstract EDataCollectionStatus CalcStatus();
+    public abstract Task<string> CalcBannerText(string userLanguage);
+}
+
+public class DataPointState 
+{
+
+ 
+}
+
+public class DataPointsStates {
+    private List<DataPointState> _states;
+
+    public DataPointsStates(List<DataPointState> states)
+    {
+        _states = states;
+    }
+}
+
+public interface IDataCollectionServiceV2 
+{
+    Task<DataCollection> Init(DataCollectionContext context, DateTime startedAt);
+    Task<DataCollection?> GetState(DataCollectionContext context);
+}
+
