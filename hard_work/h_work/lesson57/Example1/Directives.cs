8d7eddeb7802e3327e2ae8ff977f53d2a246f513
Andrey Zolotarev <a.zolotarev@pnlfin.tech>
2024-06-15 12:49:41 +0300

    hard_work: lesson57

---
+++ hard_work/h_work/lesson57/Example1/Directives.cs
@@ -0,0 +1,50 @@
+﻿#define V2
+
+
+using System;
+
+
+public class Contract
+{
+    public string Name { get; set; }
+#if V1
+    public string V1Field { get;set; }
+#elif V2
+    public string V2Field { get;set; }
+#else
+        
+#endif
+}
+
+public static class Test
+{
+    public static void TestV0()
+    {
+        var contract = new Contract
+        {
+            Name = "test"
+        };
+    }
+    
+    public static void TestV1()
+    {
+#if V1
+        var contract = new Contract
+        {
+            Name = "test",
+            V1Field = "v2"
+        };
+#endif
+    }
+    
+    public static void TestV2()
+    {
+#if V2
+        var contract = new Contract
+        {
+            Name = "test",
+            V2Field = "v2"
+        };
+#endif
+    }
+}
---
+++ hard_work/h_work/lesson57/Example2/Attributes.cs
@@ -0,0 +1,42 @@
+﻿namespace h_work.lesson57.Example2;
+
+
+
+public class ApiVersionAttribute : Attribute
+{
+    public string Version { get; }
+
+    public ApiVersionAttribute(string version)
+    {
+        Version = version;
+    }
+}
+
+public class RequiredFieldAttribute : Attribute
+{
+
+    public RequiredFieldAttribute()
+    {
+       
+    }
+}
+
+[ApiVersion("1.0.0")]
+public class ContractV1
+{
+    [RequiredField]
+    public string Name { get; set; }
+    
+    [RequiredField]
+    public string V1Field { get; set; }
+}
+
+[ApiVersion("1.0.1")]
+public class ContractV101
+{
+    [RequiredField]
+    public string Name { get; set; }
+    
+    [RequiredField]
+    public string V2Field { get; set; }
+}
