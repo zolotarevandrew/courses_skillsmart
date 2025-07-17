751ea90bf003405a580917e7271beab86ae729c7
a.zolotarev <a.zolotarev@travelline.ru>
2025-07-17 09:25:07 +0300

    Array maximum container

---
+++ algorithms/Tasks/Arrays/MaximumContainerArea/ArrayMaximumContainerArea.cs
@@ -0,0 +1,49 @@
+﻿namespace Tasks.Arrays.MaximumContainerArea;
+
+public class ArrayMaximumContainerArea
+{
+    /*
+     * Given an array of height where height[i] is the height of the vertical line at position i.
+     * Find the maximum area of water a container can store, where the container by the two lines and the x-axis.
+     */
+    public static int Run( int[] array )
+    {
+        int area = 0;
+        for ( int left = 0; left < array.Length; left++ )
+        {
+            for ( int right = left + 1; right < array.Length; right++ )
+            {
+                int height = Math.Min( array[left], array[right] );
+                int width = right - left;
+                int curArea = height * width;
+                area = Math.Max( area, curArea );
+            }
+        }
+        return area;
+    }
+    
+    public static int RunTwoPointers( int[] array )
+    {
+        int area = 0;
+        int left = 0;
+        int right = array.Length - 1;
+        while ( left < right )
+        {
+            int height = Math.Min( array[left], array[right] );
+            int width = right - left;
+            int curArea = height * width;
+            area = Math.Max( area, curArea );
+
+            if ( height == array[left] )
+            {
+                left++;
+            }
+
+            if ( height == array[right] )
+            {
+                right--;
+            }
+        }
+        return area;
+    }
+}
---
+++ algorithms/Tasks/Arrays/MaximumContainerArea/ArrayMaximumContainerAreaTests.cs
@@ -0,0 +1,56 @@
+﻿namespace Tasks.Arrays.MaximumContainerArea;
+
+public class ArrayMaximumContainerAreaTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var array = new int[] { 2, 3, 10, 5, 7, 8, 9 };
+        
+        // Act
+        var result = ArrayMaximumContainerArea.Run(array);
+        
+        // Assert
+        Assert.Equal( 36, result );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var array = new int[] { 1, 1, 1, 1 };
+        
+        // Act
+        var result = ArrayMaximumContainerArea.Run(array);
+        
+        // Assert
+        Assert.Equal( 3, result );
+    }
+    
+    [Fact]
+    public void RunTwoPointers_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var array = new int[] { 2, 3, 10, 5, 7, 8, 9 };
+        
+        // Act
+        var result = ArrayMaximumContainerArea.RunTwoPointers(array);
+        
+        // Assert
+        Assert.Equal( 36, result );
+    }
+    
+    [Fact]
+    public void RunTwoPointers_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var array = new int[] { 1, 1, 1, 1 };
+        
+        // Act
+        var result = ArrayMaximumContainerArea.RunTwoPointers(array);
+        
+        // Assert
+        Assert.Equal( 3, result );
+    }
+}
