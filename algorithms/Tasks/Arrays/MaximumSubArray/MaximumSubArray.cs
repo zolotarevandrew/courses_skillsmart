7ce486b0c26a3627b26f2aff074a5a795c229ce7
a.zolotarev <a.zolotarev@travelline.ru>
2025-07-18 10:12:54 +0300

    array: MaximumSubArray

---
+++ algorithms/Tasks/Arrays/MaximumSubArray/MaximumSubArray.cs
@@ -0,0 +1,42 @@
+﻿namespace Tasks.Arrays.MaximumSubArray;
+
+public static class MaximumSubArray
+{
+    /*
+     * Given an array of integers, identify the contigious subarray containing at least one element.
+     * That has the maximum sum and return the sum
+     */
+    public static int Run(int [] array)
+    {
+        if (array.Length == 0) return 0;
+
+        int maxSum = array[0];
+        
+        for ( int i = 0; i < array.Length; i++ )
+        {
+            int curSum = array[i];
+            for ( int j = i + 1; j < array.Length; j++ )
+            {
+                curSum += array[j];
+                maxSum = Math.Max( maxSum, curSum );
+            }
+        }
+        return maxSum;
+    }
+    
+    public static int RunSlidingWindow(int [] array)
+    {
+        if ( array.Length == 0 ) return 0;
+
+        int maxSum = array[0];
+        int currentSum = array[0];
+
+        for ( int i = 1; i < array.Length; i++ )
+        {
+            currentSum = Math.Max( array[i], currentSum + array[i] );
+            maxSum = Math.Max( maxSum, currentSum );
+        }
+
+        return maxSum;
+    }
+}
---
+++ algorithms/Tasks/Arrays/MaximumSubArray/MaximumSubArrayTests.cs
@@ -0,0 +1,82 @@
+﻿namespace Tasks.Arrays.MaximumSubArray;
+
+public class MaximumSubArrayTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var array = new int[] { -1, 2, 3, -5, 4 };
+        
+        // Act
+        var result = MaximumSubArray.Run(array);
+        
+        // Assert
+        Assert.Equal( result, 5 );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var array = new int[] { 0 };
+        
+        // Act
+        var result = MaximumSubArray.Run(array);
+        
+        // Assert
+        Assert.Equal( result, 0 );
+    }
+    
+    [Fact]
+    public void Run_Case3_ShouldBeOk( )
+    {
+        // Arrange
+        var array = new int[] { 10, -2, 3, 1 };
+        
+        // Act
+        var result = MaximumSubArray.Run(array);
+        
+        // Assert
+        Assert.Equal( result, 12 );
+    }
+    
+    [Fact]
+    public void RunSlidingWindow_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var array = new int[] { -1, 2, 3, -5, 4 };
+        
+        // Act
+        var result = MaximumSubArray.RunSlidingWindow(array);
+        
+        // Assert
+        Assert.Equal( result, 5 );
+    }
+    
+    [Fact]
+    public void RunSlidingWindow_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var array = new int[] { 0 };
+        
+        // Act
+        var result = MaximumSubArray.RunSlidingWindow(array);
+        
+        // Assert
+        Assert.Equal( result, 0 );
+    }
+    
+    [Fact]
+    public void RunSlidingWindow_Case3_ShouldBeOk( )
+    {
+        // Arrange
+        var array = new int[] { 10, -2, 3, 1 };
+        
+        // Act
+        var result = MaximumSubArray.RunSlidingWindow(array);
+        
+        // Assert
+        Assert.Equal( result, 12 );
+    }
+}
