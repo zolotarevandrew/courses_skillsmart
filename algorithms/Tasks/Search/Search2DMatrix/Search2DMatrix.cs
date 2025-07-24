719e117f2c2e6beed8fa9e08eb1a1f7450386ed2
a.zolotarev <a.zolotarev@travelline.ru>
2025-07-24 10:36:39 +0300

    Search: Search 2d matrix

---
+++ algorithms/Tasks/Search/Search2DMatrix/Search2DMatrix.cs
@@ -0,0 +1,54 @@
+﻿namespace Tasks.Search.Search2DMatrix;
+
+public static class Search2DMatrix
+{
+    /*
+     * Given m * n integer matrix.
+     * Each row is sorted in ascending order.
+     * The first number in each row is greater than the last number in the previous row.
+     *
+     * Write a function that returns true if the value val is in the matrix.
+     * The solution must use O log(m * n) time
+     *
+     */
+    public static bool RunBinarySearch( int[][] matrix, int target )
+    {
+        if ( matrix.Length == 0 ) return false;
+
+        int rows = matrix.Length;
+        int columns = matrix[0].Length;
+        int lastIndex = rows * columns - 1;
+
+        int left = 0, right = lastIndex;
+        while ( left <= right )
+        {
+            int mid = ( left + right ) / 2;
+            int midValue = GetByFlatIndex( matrix, mid );
+            if ( midValue == target )
+            {
+                return true;
+            }
+            
+            if ( midValue < target )
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        
+        return false;
+    }
+
+    public static int GetByFlatIndex( int[][] matrix, int flatIndex )
+    {
+        int columns = matrix[0].Length;
+        
+        int row = flatIndex / columns;
+        int column = flatIndex % columns;
+
+        return matrix[row][column];
+    }
+}
---
+++ algorithms/Tasks/Search/Search2DMatrix/Search2DMatrixTests.cs
@@ -0,0 +1,37 @@
+﻿namespace Tasks.Search.Search2DMatrix;
+
+public class Search2DMatrixTests
+{
+    [Fact]
+    public void RunBinarySearch_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        int[][] matrix = new int[][]
+        {
+            [1, 2, 3], [4, 5, 6], [7, 8, 9]
+        };
+
+        // Act
+        var result = Search2DMatrix.RunBinarySearch( matrix, 5 );
+
+        // Assert
+        Assert.Equal( result, true );
+    }
+    
+    [Fact]
+    public void GetByFlatIndex_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        int[][] matrix = new int[][]
+        {
+            [1, 2, 3, 4], 
+            [5, 6, 7, 8]
+        };
+        // Act
+        for ( int idx = 0; idx < 8; idx++ )
+        {
+            int result = Search2DMatrix.GetByFlatIndex( matrix, idx );
+            Assert.Equal( result, idx + 1 );
+        }
+    }
+}
