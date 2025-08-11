e998f378fdb9a3e7ca74160c19275026e93ede15
a.zolotarev <a.zolotarev@travelline.ru>
2025-08-11 09:19:29 +0300

    Hashtables: contains duplicate

---
+++ algorithms/Tasks/Hashtables/ContainsDuplicates.cs
@@ -0,0 +1,15 @@
+﻿namespace Tasks.Hashtables;
+
+public class ContainsDuplicates
+{
+    /*
+     * Given an integer array of nums
+     * determine if any value appears at least twice.
+     * Return true if duplicate exists, and false if all elements are unique.
+     */
+    public static bool Run( int[] array )
+    {
+        HashSet<int> hashtable = [];
+        return array.Any( t => !hashtable.Add( t ) );
+    }
+}
---
+++ algorithms/Tasks/Hashtables/ContainsDuplicatesTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Tasks.Hashtables;
+
+public class ContainsDuplicatesTest
+{
+    [Fact]
+    public void HasDuplicate_ShouldReturnTrue( )
+    {
+        // Arrange 
+        var arr = new int[]
+        {
+            2,7,8,2
+        };
+
+        // Act
+        bool exists = ContainsDuplicates.Run( arr );
+
+        // Assert
+        Assert.True( exists );
+    }
+    
+    [Fact]
+    public void HasMultipleDuplicate_ShouldReturnTrue( )
+    {
+        // Arrange 
+        var arr = new int[]
+        {
+            1,1,1,2,2,3
+        };
+
+        // Act
+        bool exists = ContainsDuplicates.Run( arr );
+
+        // Assert
+        Assert.True( exists );
+    }
+    
+    [Fact]
+    public void UniqueElements_ShouldReturnFalse( )
+    {
+        // Arrange 
+        var arr = new int[]
+        {
+            10,20,30,40
+        };
+
+        // Act
+        bool exists = ContainsDuplicates.Run( arr );
+
+        // Assert
+        Assert.False( exists );
+    }
+}
