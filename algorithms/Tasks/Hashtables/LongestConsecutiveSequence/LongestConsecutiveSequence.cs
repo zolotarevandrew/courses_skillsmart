25f9e8547a534461049036ec443379f6bf342ddb
a.zolotarev <a.zolotarev@travelline.ru>
2025-08-14 09:21:25 +0300

    Hashsets: done

---
+++ algorithms/Tasks/Hashtables/LongestConsecutiveSequence/LongestConsecutiveSequence.cs
@@ -0,0 +1,51 @@
+﻿namespace Tasks.Hashtables.LongestConsecutiveSequence;
+
+public class LongestConsecutiveSequence
+{
+    /*
+     * given an array of colors consisting red, green, blue, by number 0,1,2
+     * sort the array so all red, green, blue are in order
+     */
+    public static int Run( int[] array )
+    {
+        HashSet<int> set = new HashSet<int>( array );
+        int longest = 0;
+        foreach ( int t in array )
+        {
+            int streak = 0;
+            int cur = t;
+            while ( set.Contains( cur ) )
+            {
+                streak++;
+                cur++;
+            }
+            
+            longest = Math.Max( longest, streak );
+        }
+
+        return longest;
+    }
+    
+    public static int Run2( int[] array )
+    {
+        HashSet<int> set = new HashSet<int>( array );
+        int longest = 0;
+        foreach ( int value in set )
+        {
+            if ( set.Contains( value - 1 ) ) continue;
+
+            int streak = 1;
+            int cur = value + 1;
+            while ( set.Contains( cur ) )
+            {
+                streak++;
+                cur++;
+            }
+            
+            longest = Math.Max( longest, streak );
+            
+        }
+
+        return longest;
+    }
+}
---
+++ algorithms/Tasks/Hashtables/LongestConsecutiveSequence/LongestConsecutiveSequenceTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Tasks.Hashtables.LongestConsecutiveSequence;
+
+public class LongestConsecutiveSequenceTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange 
+        var color = new int[] { -2, 1, -3, 4, -1, 2, 1, 4, 3, 8, 5 };
+        
+        // Act
+        var result = LongestConsecutiveSequence.Run(color);
+        
+        // Assert
+        Assert.Equal( 5, result );
+    }
+    
+    [Fact]
+    public void Run2_Case1_ShouldBeOk( )
+    {
+        // Arrange 
+        var color = new int[] { -2, 1, -3, 4, -1, 2, 1, 4, 3, 8, 5 };
+        
+        // Act
+        var result = LongestConsecutiveSequence.Run2(color);
+        
+        // Assert
+        Assert.Equal( 5, result );
+    }
+}
