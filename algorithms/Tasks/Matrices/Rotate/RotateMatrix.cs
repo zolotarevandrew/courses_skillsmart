75890278c6bc615de620caf1ad657cf371f2de6b
a.zolotarev <a.zolotarev@travelline.ru>
2025-09-09 09:15:50 +0300

    Matrix: rotate

---
+++ algorithms/Tasks/Matrices/Rotate/RotateMatrix.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Tasks.Matrices.Rotate;
+
+public class RotateMatrix
+{
+    public static int[,] Run( int[,] matrix )
+    {
+        int len = matrix.GetLength( 0 );
+        if ( len != matrix.GetLength( 1 ) ) return matrix;
+
+        for ( int row = 0; row < len - 1; row++ )
+        {
+            for ( int column = row + 1; column < len; column++ )
+            {
+                ( matrix[row, column], matrix[column, row] ) = ( matrix[column, row], matrix[row, column] );
+            }
+        }
+
+        Span<int> flat = MemoryMarshal.CreateSpan( ref matrix[0, 0], matrix.Length );
+        int cols = matrix.GetLength( 1 );
+        for ( int row = 0; row < len; row++ )
+        {
+            Span<int> rowSpan = flat.Slice( row * cols, cols );
+            rowSpan.Reverse();
+        }
+        
+        return matrix;
+    }
+}
---
+++ algorithms/Tasks/Matrices/Rotate/RotateMatrixTests.cs
@@ -0,0 +1,77 @@
+﻿using Tasks.Matrices.Toeplitz;
+
+namespace Tasks.Matrices.Rotate;
+
+public class RotateMatrixTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange 
+        var matrix = new int[,]
+        {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+        
+        var expected = new int[,]
+        {
+            { 7, 4, 1 },
+            { 8, 5, 2 },
+            { 9, 6, 3 },
+        };
+        
+        // Act
+        var res = RotateMatrix.Run( matrix );
+        
+        // Assert
+        Assert.Equal( res, expected );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange 
+        var matrix = new int[,]
+        {
+            { 1, 2 },
+            { 4, 5 },
+        };
+        
+        var expected = new int[,]
+        {
+            { 4, 1 },
+            { 5, 2 },
+        };
+        
+        // Act
+        var res = RotateMatrix.Run( matrix );
+        
+        // Assert
+        Assert.Equal( res, expected );
+    }
+    
+    [Fact]
+    public void Run_Case3_ShouldBeOk( )
+    {
+        // Arrange 
+        var matrix = new int[,]
+        {
+            { 1 },
+            { 2 },
+        };
+        
+        var expected = new int[,]
+        {
+            { 1 },
+            { 2 },
+        };
+        
+        // Act
+        var res = RotateMatrix.Run( matrix );
+        
+        // Assert
+        Assert.Equal( res, expected );
+    }
+}
