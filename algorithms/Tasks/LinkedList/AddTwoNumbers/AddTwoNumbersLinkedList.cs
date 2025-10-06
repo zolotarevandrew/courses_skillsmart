c0b282dbb731fefa351da788acd3637956ff0296
a.zolotarev <a.zolotarev@travelline.ru>
2025-10-06 09:35:50 +0300

    LinkedList: add two numbers

---
+++ algorithms/Tasks/LinkedList/AddTwoNumbers/AddTwoNumbersLinkedList.cs
@@ -0,0 +1,57 @@
+﻿namespace Tasks.LinkedList.AddTwoNumbers;
+
+public class ListNode
+{
+    public int Value { get; set; }
+    public ListNode? Next { get; set; }
+
+    public List<int> AsArray()
+    {
+        var res = new List<int>();
+        var curr = this;
+        while (curr != null)
+        {
+            res.Add(curr.Value);
+            curr = curr.Next;
+        }
+        return res;
+    }
+}
+
+public class AddTwoNumbersLinkedList
+{
+    /*
+     * Given two not empty linked lists, where each represents non negative integer.
+     * The digits of the numbers stored in reverse order, with each node containing a single digit.
+     * Add the two numbers and return result as a linked list
+     */
+    public static ListNode Run( ListNode first, ListNode second )
+    {
+        ListNode result = new ListNode
+        {
+            Value = 0
+        };
+        
+        ListNode? curFirst = first;
+        ListNode? curSecond = second;
+        ListNode curResult = result;
+        int carry = 0;
+        while ( curFirst != null || curSecond != null || carry != 0 )
+        {
+            int val1 = curFirst?.Value ?? 0;
+            int val2 = curSecond?.Value ?? 0;
+            int total = val1 + val2 + carry;
+            
+            curResult.Next = new ListNode
+            {
+                Value = total % 10
+            };
+            carry = total / 10;
+            curResult = curResult.Next;
+            curFirst = curFirst?.Next;
+            curSecond = curSecond?.Next;
+        }
+
+        return result.Next;
+    }
+}
---
+++ algorithms/Tasks/LinkedList/AddTwoNumbers/AddTwoNumbersLinkedListTest.cs
@@ -0,0 +1,129 @@
+﻿namespace Tasks.LinkedList.AddTwoNumbers;
+
+public class AddTwoNumbersLinkedListTest
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var node1 = new ListNode
+        {
+            Value = 5,
+            Next = new ListNode
+            {
+                Value = 4,
+                Next = new ListNode
+                {
+                    Value = 3
+                }
+            }
+        };
+        var node2 = new ListNode
+        {
+            Value = 5,
+            Next = new ListNode
+            {
+                Value = 6,
+                Next = new ListNode
+                {
+                    Value = 4
+                }
+            }
+        };
+        
+        var expected = new int[]
+        {
+           0,1,8
+        }.ToList();
+
+        // Act
+        var res = AddTwoNumbersLinkedList.Run(node1, node2);
+
+        // Assert
+        Assert.NotNull( res );
+        Assert.Equal( expected, res.AsArray() );
+
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var node1 = new ListNode
+        {
+            Value = 2,
+            Next = new ListNode
+            {
+                Value = 4,
+                Next = new ListNode
+                {
+                    Value = 3
+                }
+            }
+        };
+        var node2 = new ListNode
+        {
+            Value = 5,
+            Next = new ListNode
+            {
+                Value = 6
+            }
+        };
+        
+        var expected = new int[]
+        {
+            7,0,4
+        }.ToList();
+
+        // Act
+        var res = AddTwoNumbersLinkedList.Run(node1, node2);
+
+        // Assert
+        Assert.NotNull( res );
+        Assert.Equal( expected, res.AsArray() );
+
+    }
+    
+    [Fact]
+    public void Run_Case3_ShouldBeOk( )
+    {
+        // Arrange
+        var node1 = new ListNode
+        {
+            Value = 2,
+            Next = new ListNode
+            {
+                Value = 4,
+                Next = new ListNode
+                {
+                    Value = 3
+                }
+            }
+        };
+        var node2 = new ListNode
+        {
+            Value = 5,
+            Next = new ListNode
+            {
+                Value = 6,
+                Next = new ListNode
+                {
+                    Value = 7
+                }
+            }
+        };
+        
+        var expected = new int[]
+        {
+            7,0,1,1
+        }.ToList();
+
+        // Act
+        var res = AddTwoNumbersLinkedList.Run(node1, node2);
+
+        // Assert
+        Assert.NotNull( res );
+        Assert.Equal( expected, res.AsArray() );
+
+    }
+}
