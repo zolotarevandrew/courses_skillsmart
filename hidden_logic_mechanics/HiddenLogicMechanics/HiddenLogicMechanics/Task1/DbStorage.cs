50826e77e002b51d4a3ada4fb7bd183f30f02fed
a.zolotarev <a.zolotarev@travelline.ru>
2025-10-14 16:48:14 +0300

    HiddenLogicMechanics: task1

---
+++ hidden_logic_mechanics/HiddenLogicMechanics/HiddenLogicMechanics/Task1/DbStorage.cs
@@ -0,0 +1,42 @@
+﻿using Npgsql;
+
+namespace HiddenLogicMechanics.Task1;
+
+public interface IStorage
+{
+    Task<int> SaveAsync( string data );
+    Task<string?> RetrieveAsync( int id );
+}
+
+public class DbStorage( string connectionString ) : IStorage
+{
+    private readonly string _connectionString = connectionString;
+
+    public async Task<int> SaveAsync( string data )
+    {
+        const string sql = "INSERT INTO storage (data) VALUES (@data) RETURNING id;";
+
+        await using var connection = new NpgsqlConnection( _connectionString );
+        await connection.OpenAsync( );
+
+        await using var command = new NpgsqlCommand( sql, connection );
+        command.Parameters.AddWithValue( "data", data );
+
+        object? res = await command.ExecuteScalarAsync( );
+        return (int)res!;
+    }
+
+    public async Task<string?> RetrieveAsync( int id )
+    {
+        const string sql = "SELECT data FROM storage WHERE id = @id;";
+
+        await using NpgsqlConnection connection = new NpgsqlConnection( _connectionString );
+        await connection.OpenAsync( );
+
+        await using NpgsqlCommand command = new NpgsqlCommand( sql, connection );
+        command.Parameters.AddWithValue( "id", id );
+
+        object? result = await command.ExecuteScalarAsync();
+        return result as string;
+    }
+}
