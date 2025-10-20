b3f4af9a7b94a048b6a41548baa5428b1848b254
a.zolotarev <a.zolotarev@travelline.ru>
2025-10-20 09:37:31 +0300

    Queues: first non repeating char

---
+++ algorithms/Tasks/Queues/NonRepeatingChar/FirstNonRepeatingChar.cs
@@ -0,0 +1,42 @@
+﻿namespace Tasks.Queues.NonRepeatingChar;
+
+public class FirstNonRepeatingChar
+{
+    /*
+     * Given a string s, return the first non-repeating char.
+     * (a char with frequency of 1 within string)
+     */
+    public static char? RunBruteforce( string s )
+    {
+        foreach ( char c in s )
+        {
+            int cnt = s.Count( c2 => c == c2 );
+            if ( cnt == 1 ) return c;
+        }
+        return null;
+    }
+
+    public static char? RunQueue( string s )
+    {
+        Dictionary<char, int> count = new Dictionary<char, int>( );
+        Queue<char> queue = new Queue<char>( );
+        foreach ( char c in s )
+        {
+            if ( count.TryAdd( c, 1 ) )
+            {
+                queue.Enqueue( c );
+                continue;
+            }
+
+            count[c] += 1;
+        }
+
+        while ( queue.Count > 0 )
+        {
+            char c = queue.Dequeue( );
+            if ( count[c] == 1 ) return c;
+        }
+
+        return null;
+    }
+}
---
+++ algorithms/Tasks/Queues/NonRepeatingChar/FirstNonRepeatingCharTests.cs
@@ -0,0 +1,60 @@
+﻿namespace Tasks.Queues.NonRepeatingChar;
+
+public class FirstNonRepeatingCharTests
+{
+    [Fact]
+    public void RunBruteforce_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var s = "hellothere";
+
+        // Act
+        var res = FirstNonRepeatingChar.RunBruteforce( s );
+
+        // Assert
+        Assert.NotNull( res );
+        Assert.Equal( 'o', res );
+    }
+    
+    [Fact]
+    public void RunBruteforce_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var s = "civicservant";
+
+        // Act
+        var res = FirstNonRepeatingChar.RunBruteforce( s );
+
+        // Assert
+        Assert.NotNull( res );
+        Assert.Equal( 's', res );
+    }
+    
+    [Fact]
+    public void RunQueue_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var s = "hellothere";
+
+        // Act
+        var res = FirstNonRepeatingChar.RunQueue( s );
+
+        // Assert
+        Assert.NotNull( res );
+        Assert.Equal( 'o', res );
+    }
+    
+    [Fact]
+    public void RunQueue_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var s = "civicservant";
+
+        // Act
+        var res = FirstNonRepeatingChar.RunQueue( s );
+
+        // Assert
+        Assert.NotNull( res );
+        Assert.Equal( 's', res );
+    }
+}
