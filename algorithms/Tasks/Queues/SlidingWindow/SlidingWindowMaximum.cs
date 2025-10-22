12c351359adf107c894aea62510833ec26de0f59
a.zolotarev <a.zolotarev@travelline.ru>
2025-10-22 09:15:48 +0300

    Queues: Sliding Window maximum

---
+++ algorithms/Tasks/Queues/SlidingWindow/SlidingWindowMaximum.cs
@@ -0,0 +1,31 @@
+﻿namespace Tasks.Queues.SlidingWindow;
+
+public class SlidingWindowMaximum
+{
+    /*
+     * You are given an array of integers num, and a sliding window of size k
+     * that moves from the very left of the array to the very right.
+     * You can only see the k numbers in the window at each move.
+     * The sliding window moves one position to the right at a time.
+     * Return the array of the maximums for each window position.
+     */
+    public static List<int> Run( int[] num, int k )
+    {
+        List<int> res = new List<int>();
+
+        if ( num.Length < k || k <= 0 ) return res;
+
+        for ( int left = 0; left <= num.Length - k; left++ )
+        {
+            int right = left + k;
+            int max = num[left];
+            for ( int i = left + 1; i < right; i++ )
+            {
+                max = Math.Max( max, num[i] );
+            }
+            res.Add( max );
+        }
+
+        return res;
+    }
+}
---
+++ algorithms/Tasks/Queues/SlidingWindow/SlidingWindowMaximumTests.cs
@@ -0,0 +1,63 @@
+﻿namespace Tasks.Queues.SlidingWindow;
+
+public class SlidingWindowMaximumTests
+{
+
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var arr = new int[]
+        {
+            1,3,-1,-3,5,3,6,7
+        };
+        int k = 3;
+
+        // Act
+        var res = SlidingWindowMaximum.Run( arr, k );
+
+        // Assert
+        Assert.Equal( 6, res.Count );
+        Assert.Equal( 3, res[ 0 ] );
+        Assert.Equal( 3, res[ 1 ] );
+        Assert.Equal( 5, res[ 2 ] );
+        Assert.Equal( 5, res[ 3 ] );
+        Assert.Equal( 6, res[ 4 ] );
+        Assert.Equal( 7, res[ 5 ] );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var arr = new int[]
+        {
+            9,11
+        };
+        int k = 2;
+
+        // Act
+        var res = SlidingWindowMaximum.Run( arr, k );
+
+        // Assert
+        Assert.Equal( 1, res.Count );
+        Assert.Equal( 11, res[ 0 ] );
+    }
+    
+    [Fact]
+    public void Run_Case3_ShouldBeOk( )
+    {
+        // Arrange
+        var arr = new int[]
+        {
+            9,11
+        };
+        int k = 3;
+
+        // Act
+        var res = SlidingWindowMaximum.Run( arr, k );
+
+        // Assert
+        Assert.Equal( 0, res.Count );
+    }
+}
