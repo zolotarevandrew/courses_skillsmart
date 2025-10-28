323f88737c97d169573b792df9dbb427868b6ec3
a.zolotarev <a.zolotarev@travelline.ru>
2025-10-28 20:36:29 +0300

    DynamicProgramming: ClimbingStairs

---
+++ algorithms/Tasks/DynamicProgramming/Climbing/ClimbingStairs.cs
@@ -0,0 +1,58 @@
+﻿namespace Tasks.DynamicProgramming.Climbing;
+
+public class ClimbingStairs
+{
+    /*
+     * You are given an integer n, which represents a total number of steps in a staircase.
+     * You can climb either 1 step or 2 steps at a time.
+     * Return the number of unique ways you can reach the top.
+     */
+    public static int Run( int n )
+    {
+        if ( n <= 2 )
+        {
+            return n;
+        }
+
+        int oneStep = Run( n - 1 );
+        int secondStep = Run( n - 2 );
+        return oneStep + secondStep;
+    }
+    
+    public static int RunMemo( int n, Dictionary<int, int> memo )
+    {
+        if ( memo.TryGetValue( n, out int runMemo ) )
+        {
+            return runMemo;
+        }
+        
+        if ( n <= 2 )
+        {
+            return n;
+        }
+
+        int oneStep = RunMemo( n - 1, memo );
+        int secondStep = RunMemo( n - 2, memo );
+        memo[n] = oneStep + secondStep;
+        return memo[n];
+    }
+    
+    public static int RunTabulation( int n )
+    {
+        if ( n <= 2 )
+        {
+            return n;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for ( int idx = 3; idx <= n; idx++ )
+        {
+            dp[idx] = dp[idx - 1] + dp[idx - 2];
+        }
+
+        return dp[n];
+    }
+}
---
+++ algorithms/Tasks/DynamicProgramming/Climbing/ClimbingStairsTests.cs
@@ -0,0 +1,40 @@
+﻿namespace Tasks.DynamicProgramming.Climbing;
+
+public class ClimbingStairsTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        Assert.Equal( 5, ClimbingStairs.Run( 4 ) );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        Assert.Equal( 8, ClimbingStairs.Run( 5 ) );
+    }
+    
+    [Fact]
+    public void RunMemo_Case1_ShouldBeOk( )
+    {
+        Assert.Equal( 5, ClimbingStairs.RunMemo( 4, new Dictionary<int, int>() ) );
+    }
+    
+    [Fact]
+    public void RunMemo_Case2_ShouldBeOk( )
+    {
+        Assert.Equal( 8, ClimbingStairs.RunMemo( 5, new Dictionary<int, int>() ) );
+    }
+    
+    [Fact]
+    public void RunTabulation_Case1_ShouldBeOk( )
+    {
+        Assert.Equal( 5, ClimbingStairs.RunTabulation( 4 ) );
+    }
+    
+    [Fact]
+    public void RunTabulation_Case2_ShouldBeOk( )
+    {
+        Assert.Equal( 8, ClimbingStairs.RunTabulation( 5 ) );
+    }
+}
