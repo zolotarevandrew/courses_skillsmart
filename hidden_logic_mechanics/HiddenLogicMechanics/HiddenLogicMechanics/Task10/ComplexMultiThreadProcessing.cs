1ac7005aecf2c4683178386bbdc90aea61f63e38
a.zolotarev <a.zolotarev@travelline.ru>
2025-11-09 09:36:34 +0300

    HiddenLogicMechanics: Task11

--- hidden_logic_mechanics/HiddenLogicMechanics/HiddenLogicMechanics/Task10/ComplexMultiThreadProcessing.cs
+++ hidden_logic_mechanics/HiddenLogicMechanics/HiddenLogicMechanics/Task10/ComplexMultiThreadProcessing.cs
@@ -4,7 +4,7 @@ namespace HiddenLogicMechanics.Task10;
 
 public class ComplexMultiThreadProcessing
 {
-    private static readonly int SIZE = 1000000;
+    private static readonly int SIZE = 6;
     private static readonly int THREADS = 4;
     private static readonly int[] data = new int[SIZE];
     private static long sum = 0;
@@ -14,19 +14,20 @@ public class ComplexMultiThreadProcessing
         Random random = new Random( );
         for ( int i = 0; i < SIZE; i++ )
         {
-            data[i] = random.Next( 100 );
+            data[i] = i;
         }
         ParallelOptions options = new ParallelOptions
         {
             MaxDegreeOfParallelism = THREADS
         };
-        int chunkSize = SIZE / THREADS;
+        int chunkSize = 2;
         OrderablePartitioner<Tuple<int, int>> ranges = Partitioner.Create( 0, data.Length, chunkSize );
         Parallel.ForEach( ranges, options, range =>
         {
             long local = 0;
             for ( int i = range.Item1; i < range.Item2; i++ )
                 local += data[i];
+            Console.WriteLine(range);
             Interlocked.Add( ref sum, local );
         } );
         Console.WriteLine( sum );
---
+++ hidden_logic_mechanics/HiddenLogicMechanics/HiddenLogicMechanics/Task11/Result.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+namespace HiddenLogicMechanics.Task11;
+
+/*
+ * Ранее было много императивного кода для работы с путем к универсальной анкете.
+ * Переделываем на функциональный стиль.
+ */
+public readonly record struct UniversalSurveyPath
+{
+    private const char Separator = '.';
+    public string Value { get; }
+
+    private UniversalSurveyPath( string value )
+    {
+        ArgumentNullException.ThrowIfNull( value );
+        Value = value;
+    }
+
+    public static UniversalSurveyPath Empty = new UniversalSurveyPath( string.Empty );
+
+    public static UniversalSurveyPath FromUshort( ushort value )
+    {
+        return new UniversalSurveyPath( value.ToString( CultureInfo.InvariantCulture ) );
+    }
+
+    public static UniversalSurveyPath FromUshortArray( ushort[] value )
+    {
+        return value.Aggregate( Empty, ( current, item ) => current.MergeWith( FromUshort( item ) ) );
+    }
+
+    public UniversalSurveyPath MergeWith( UniversalSurveyPath other )
+    {
+        if ( string.IsNullOrEmpty( other.Value ) ) return this;
+        if ( string.IsNullOrEmpty( Value ) ) return new UniversalSurveyPath( other.Value );
+        return new UniversalSurveyPath( $"{Value}{Separator}{other.Value}" );
+    }
+}
