44cd8c7eafebc4740e8da8d1564332322677b437
a.zolotarev <a.zolotarev@travelline.ru>
2025-11-11 15:16:22 +0300

    HiddenLogicMechanics: Task12

---
+++ hidden_logic_mechanics/HiddenLogicMechanics/HiddenLogicMechanics/Task12/Polymorphism.cs
@@ -0,0 +1,76 @@
+﻿namespace HiddenLogicMechanics.Task12;
+
+public interface IShortAnswerInfoConverter
+{
+    ShortAnswerDto Convert( ShortAnswerInfo answerInfo );
+}
+
+public class Answer
+{
+    public SurveyKind SurveyKind { get; init; }
+}
+public class ShortAnswerInfo( Answer answer, KeyValuePair<string, string>[] parameters )
+{
+    public Answer Answer { get; set; } = answer;
+    public KeyValuePair<string, string>[] Parameters { get; set; } = parameters;
+}
+
+public class ShortAnswerDto
+{
+}
+
+public class FeedbackShortAnswerInfoConverter : IShortAnswerInfoConverter
+{
+    public ShortAnswerDto Convert( ShortAnswerInfo answerInfo )
+    {
+        return new ShortAnswerDto(  );
+    }
+}
+
+public class NoneShortAnswerInfoConverter : IShortAnswerInfoConverter
+{
+    public ShortAnswerDto Convert( ShortAnswerInfo answerInfo )
+    {
+        return new ShortAnswerDto(  );
+    }
+}
+
+public class UniversalSurveyShortAnswerInfoConverter : IShortAnswerInfoConverter
+{
+    public ShortAnswerDto Convert( ShortAnswerInfo answerInfo )
+    {
+        return new ShortAnswerDto(  );
+    }
+}
+
+public enum SurveyKind
+{
+    Feedback,
+    Universal
+}
+
+public class Usage
+{
+    /*
+     * Простейший полиморфизм без DI, для разных типов анкет в рамках слоя контроллера.
+     */
+    public static void Main( )
+    {
+        Dictionary<SurveyKind, IShortAnswerInfoConverter> converters =
+            new Dictionary<SurveyKind, IShortAnswerInfoConverter>
+            {
+                { SurveyKind.Feedback, new FeedbackShortAnswerInfoConverter( ) },
+                { SurveyKind.Universal, new UniversalSurveyShortAnswerInfoConverter( ) }
+            };
+        NoneShortAnswerInfoConverter noneConverter = new NoneShortAnswerInfoConverter( );
+
+        ShortAnswerInfo[] answers = [];
+        ShortAnswerDto[] res = answers
+            .Select( i => converters.TryGetValue( i.Answer.SurveyKind, out IShortAnswerInfoConverter? converter )
+                ? converter.Convert( i )
+                : noneConverter.Convert( i ) )
+            .ToArray( );
+    }
+}
+
+
