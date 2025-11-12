1e8a2783a019ecbd6a08c4bdcc52703a10c527c4
a.zolotarev <a.zolotarev@travelline.ru>
2025-11-12 09:40:16 +0300

    DynamicProgramming: Longest Palindromic Substring

---
+++ algorithms/Tasks/DynamicProgramming/LongestPalindromicSubstring/LongestPalindromicSubstring.cs
@@ -0,0 +1,45 @@
+﻿namespace Tasks.DynamicProgramming.LongestPalindromicSubstring;
+
+public class LongestPalindromicSubstring
+{
+    /*
+     * Given a string s, find and return the longest substring in s that
+     * reads same forward and backward
+     */
+    public static string Run( string s )
+    {
+        string max = string.Empty;
+        for ( int i = 0; i < s.Length; i++ )
+        {
+            for ( int j = i; j < s.Length; j++ )
+            {
+                if ( !IsPalindrome( s, i, j ) ) continue;
+
+                string substr = s[i..( j + 1 )];
+                if ( substr.Length < max.Length ) continue;
+                
+                max = substr;
+            }
+        }
+        return max;
+    }
+
+    public static bool IsPalindrome( string s )
+    {
+        int left = 0;
+        int right = s.Length - 1;
+        return IsPalindrome( s, left, right );
+    }
+    
+    static bool IsPalindrome( string s, int left, int right )
+    {
+        while ( left < right )
+        {
+            if ( s[left] != s[right] ) return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
---
+++ algorithms/Tasks/DynamicProgramming/LongestPalindromicSubstring/LongestPalindromicSubstringTests.cs
@@ -0,0 +1,76 @@
+﻿namespace Tasks.DynamicProgramming.LongestPalindromicSubstring;
+
+public class LongestPalindromicSubstringTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        Assert.Equal( "racecar", LongestPalindromicSubstring.Run( "racecarfun" ) );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        Assert.Equal( "anana", LongestPalindromicSubstring.Run( "banana" ) );
+    }
+    
+    [Fact]
+    public void IsPalindrome_Empty_ShouldBeOk( )
+    {
+        Assert.True( LongestPalindromicSubstring.IsPalindrome( "" ) );
+    }
+    
+    [Fact]
+    public void IsPalindrome_OneChar_ShouldBeOk( )
+    {
+        Assert.True( LongestPalindromicSubstring.IsPalindrome( "1" ) );
+    }
+    
+    [Fact]
+    public void IsPalindrome_TwoEqualChar_ShouldBeTrue( )
+    {
+        Assert.True( LongestPalindromicSubstring.IsPalindrome( "11" ) );
+    }
+    
+    [Fact]
+    public void IsPalindrome_TwoDifferentChar_ShouldBeFalse( )
+    {
+        Assert.False( LongestPalindromicSubstring.IsPalindrome( "12" ) );
+    }
+    
+    [Fact]
+    public void IsPalindrome_ThreeDifferentChar_ShouldBeFalse( )
+    {
+        Assert.False( LongestPalindromicSubstring.IsPalindrome( "123" ) );
+    }
+    
+    [Fact]
+    public void IsPalindrome_ThreePalindromeChar_ShouldBeTrue( )
+    {
+        Assert.True( LongestPalindromicSubstring.IsPalindrome( "121" ) );
+    }
+    
+    [Fact]
+    public void IsPalindrome_FourPalindromeChar_ShouldBeTrue( )
+    {
+        Assert.True( LongestPalindromicSubstring.IsPalindrome( "1221" ) );
+    }
+    
+    [Fact]
+    public void IsPalindrome_FourDifferentChar_ShouldBeFalse( )
+    {
+        Assert.False( LongestPalindromicSubstring.IsPalindrome( "1224" ) );
+    }
+    
+    [Fact]
+    public void IsPalindrome_FiveDifferentChar_ShouldBeFalse( )
+    {
+        Assert.False( LongestPalindromicSubstring.IsPalindrome( "12225" ) );
+    }
+    
+    [Fact]
+    public void IsPalindrome_FivePalindromeChar_ShouldBeTrue( )
+    {
+        Assert.True( LongestPalindromicSubstring.IsPalindrome( "anana" ) );
+    }
+}
