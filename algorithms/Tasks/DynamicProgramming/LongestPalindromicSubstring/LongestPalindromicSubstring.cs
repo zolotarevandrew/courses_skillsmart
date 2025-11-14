2534bb6fa633c27036bddf2bdba73d0086834129
a.zolotarev <a.zolotarev@travelline.ru>
2025-11-14 09:38:50 +0300

    DynamicProgramming: Longest Palindromic Substring

--- algorithms/Tasks/DynamicProgramming/LongestPalindromicSubstring/LongestPalindromicSubstring.cs
+++ algorithms/Tasks/DynamicProgramming/LongestPalindromicSubstring/LongestPalindromicSubstring.cs
@@ -1,4 +1,6 @@
-﻿namespace Tasks.DynamicProgramming.LongestPalindromicSubstring;
+﻿using System.Text;
+
+namespace Tasks.DynamicProgramming.LongestPalindromicSubstring;
 
 public class LongestPalindromicSubstring
 {
@@ -57,6 +59,48 @@ public class LongestPalindromicSubstring
         }
 
         return s.Substring( start, max_l );
+    }
+    
+    public static string RunPointer( string s )
+    {
+        if ( string.IsNullOrEmpty( s ) || s.Length == 1 ) return s;
+
+        string longest = string.Empty;
+        for ( int i = 1; i < s.Length; i++ )
+        {
+            string odd = ExpandAroundCenter( s, i, i );
+            string even = ExpandAroundCenter( s, i, i + 1 );
+
+            if ( odd.Length > longest.Length ) longest = odd;
+            if ( even.Length > longest.Length ) longest = even;
+        }
+        return longest;
+    }
+
+    public static string ExpandAroundCenter( string s, int left, int right )
+    {
+        /* кейсы
+          1) Строка длиной = 1, a
+          2) Строка длиной > 1, aa, ab, aba, abc, abba, ababa, abiki
+         */
+        StringBuilder builder = new StringBuilder( );
+        while ( left >= 0 && right < s.Length && s[left] == s[right] )
+        {
+            if ( left == right )
+            {
+                builder.Insert( 0, s[left] );
+            }
+            else
+            {
+                builder.Insert( 0, s[left] );
+                builder.Append( s[right] );
+            }
+
+            left--;
+            right++;
+        }
+
+        return builder.ToString( );
     }
 
     public static bool IsPalindrome( string s )
---
+++ algorithms/Tasks/DynamicProgramming/LongestPalindromicSubstring/LongestPalindromicSubstringPointerTests.cs
@@ -0,0 +1,58 @@
+﻿namespace Tasks.DynamicProgramming.LongestPalindromicSubstring;
+
+public partial class LongestPalindromicSubstringTests
+{
+    [Fact]
+    public void RunPointer_Case1_ShouldBeOk( )
+    {
+        Assert.Equal( "racecar", LongestPalindromicSubstring.RunPointer( "racecarfun" ) );
+    }
+    
+    [Fact]
+    public void RunPointer_Case2_ShouldBeOk( )
+    {
+        Assert.Equal( "anana", LongestPalindromicSubstring.RunPointer( "banana" ) );
+    }
+    
+    [Fact]
+    public void RunPointer_Case3_ShouldBeOk( )
+    {
+        Assert.Equal( "civic", LongestPalindromicSubstring.RunPointer( "civic" ) );
+    }
+    
+    [Fact]
+    public void ExpandAroundCenter_OneChar_ShouldBeOk( )
+    {
+        Assert.Equal( "c", LongestPalindromicSubstring.ExpandAroundCenter( "c", 0, 0 ) );
+    }
+    
+    [Fact]
+    public void ExpandAroundCenter_TwoEqualsChar_ShouldBeOk( )
+    {
+        Assert.Equal( "cc", LongestPalindromicSubstring.ExpandAroundCenter( "cc", 0, 1 ) );
+    }
+    
+    [Fact]
+    public void ExpandAroundCenter_Case3_ShouldBeOk( )
+    {
+        Assert.Equal( "aba", LongestPalindromicSubstring.ExpandAroundCenter( "aba", 1, 1 ) );
+    }
+    
+    [Fact]
+    public void ExpandAroundCenter_Case4_ShouldBeOk( )
+    {
+        Assert.Equal( "abba", LongestPalindromicSubstring.ExpandAroundCenter( "abba", 1, 2 ) );
+    }
+    
+    [Fact]
+    public void ExpandAroundCenter_Case5_ShouldBeOk( )
+    {
+        Assert.Equal( "ababa", LongestPalindromicSubstring.ExpandAroundCenter( "ababa", 2, 2 ) );
+    }
+    
+    [Fact]
+    public void ExpandAroundCenter_Case6_ShouldBeOk( )
+    {
+        Assert.Equal( "i", LongestPalindromicSubstring.ExpandAroundCenter( "abiki", 2, 2 ) );
+    }
+}
