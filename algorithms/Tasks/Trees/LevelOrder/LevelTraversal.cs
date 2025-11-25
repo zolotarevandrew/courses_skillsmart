af2f088d95bcda9be6ad434623ab1f7a25a83790
a.zolotarev <a.zolotarev@travelline.ru>
2025-11-25 09:29:50 +0300

    Trees: level traversal

---
+++ algorithms/Tasks/Trees/LevelOrder/LevelTraversal.cs
@@ -0,0 +1,45 @@
+﻿namespace Tasks.Trees.LevelOrder;
+
+public class LevelTraversal
+{
+    public class TreeNode<T>
+    {
+        public T Value { get; init; }
+        public TreeNode<T>? Left { get; init; }
+        public TreeNode<T>? Right { get; init; }
+    }
+    /*
+     * Return the level order traversal of a binary tree's node values
+     */
+    public static List<List<T>> Run<T>( TreeNode<T> tree )
+    {
+        List<List<T>> res = [];
+
+        Queue<TreeNode<T>> queue = new Queue<TreeNode<T>>( );
+        queue.Enqueue( tree );
+        while ( queue.Count > 0 )
+        {
+            List<T> curLevel = [];
+            int cnt = queue.Count;
+            for ( int i = 0; i < cnt; i++ )
+            {
+                TreeNode<T> node = queue.Dequeue( );
+                curLevel.Add( node.Value );
+
+                if ( node.Left != null )
+                {
+                    queue.Enqueue( node.Left );
+                }
+                
+                if ( node.Right != null )
+                {
+                    queue.Enqueue( node.Right );
+                }
+            }
+            
+            
+            res.Add( curLevel );
+        }
+        return res;
+    }
+}
---
+++ algorithms/Tasks/Trees/LevelOrder/LevelTraversalTests.cs
@@ -0,0 +1,65 @@
+﻿namespace Tasks.Trees.LevelOrder;
+
+public class LevelTraversalTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new LevelTraversal.TreeNode<int>
+        {
+            Value = 1,
+            Left = new LevelTraversal.TreeNode<int>
+            {
+                Value = 2,
+            },
+            Right = new LevelTraversal.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+
+        // Act
+        var res = LevelTraversal.Run( node );
+        
+        // Assert
+        Assert.Equal( res.Count, 2 );
+        Assert.Equal( res[0], [1] );
+        Assert.Equal( res[1], [2, 3] );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new LevelTraversal.TreeNode<int>
+        {
+            Value = 1,
+            Left = new LevelTraversal.TreeNode<int>
+            {
+                Value = 2,
+                Left = new LevelTraversal.TreeNode<int>
+                {
+                    Value = 4,
+                },
+                Right = new LevelTraversal.TreeNode<int>
+                {
+                    Value = 5,
+                },
+            },
+            Right = new LevelTraversal.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+
+        // Act
+        var res = LevelTraversal.Run( node );
+        
+        // Assert
+        Assert.Equal( res.Count, 3 );
+        Assert.Equal( res[0], [1] );
+        Assert.Equal( res[1], [2, 3] );
+        Assert.Equal( res[2], [4, 5] );
+    }
+}
