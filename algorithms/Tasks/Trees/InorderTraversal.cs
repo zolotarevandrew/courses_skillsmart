52ea13898e98411c85c1e1455a4517708661a4f4
a.zolotarev <a.zolotarev@travelline.ru>
2025-11-25 09:12:10 +0300

    Trees: inorder traversal

---
+++ algorithms/Tasks/Trees/InorderTraversal.cs
@@ -0,0 +1,29 @@
+﻿namespace Tasks.Trees;
+
+public class InorderTraversal
+{
+    public class TreeNode<T>
+    {
+        public T Value { get; init; }
+        public TreeNode<T>? Left { get; init; }
+        public TreeNode<T>? Right { get; init; }
+    }
+    /*
+     * Return the inorder traversal (left, root, right) of a binary tree's node values
+     */
+    public static List<T> Run<T>( TreeNode<T> tree )
+    {
+        List<T> res = new List<T>( );
+        RunInternal( tree );
+        return res;
+        
+        void RunInternal( TreeNode<T>? curNode )
+        {
+            if ( curNode == null ) return;
+            
+            RunInternal( curNode.Left );
+            res.Add( curNode.Value );
+            RunInternal( curNode.Right );
+        }
+    }
+}
---
+++ algorithms/Tasks/Trees/InorderTraversalTests.cs
@@ -0,0 +1,60 @@
+﻿namespace Tasks.Trees;
+
+public class InorderTraversalTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new InorderTraversal.TreeNode<int>
+        {
+            Value = 1,
+            Left = new InorderTraversal.TreeNode<int>
+            {
+                Value = 2,
+            },
+            Right = new InorderTraversal.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+
+        // Act
+        List<int> res = InorderTraversal.Run( node );
+        
+        // Assert
+        Assert.Equal( res, [2, 1, 3] );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new InorderTraversal.TreeNode<int>
+        {
+            Value = 1,
+            Left = new InorderTraversal.TreeNode<int>
+            {
+                Value = 2,
+                Left = new InorderTraversal.TreeNode<int>
+                {
+                    Value = 4,
+                },
+                Right = new InorderTraversal.TreeNode<int>
+                {
+                    Value = 5,
+                },
+            },
+            Right = new InorderTraversal.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+
+        // Act
+        List<int> res = InorderTraversal.Run( node );
+        
+        // Assert
+        Assert.Equal( res, [4, 2, 5, 1, 3] );
+    }
+}
