3bd74e6b7896d9f050e58f5dc97d67dd660b4f48
a.zolotarev <a.zolotarev@travelline.ru>
2025-12-01 09:32:07 +0300

    Trees: Same trees

---
+++ algorithms/Tasks/Trees/SameTrees/SameTrees.cs
@@ -0,0 +1,26 @@
+﻿namespace Tasks.Trees.SameTrees;
+
+public class SameTrees
+{
+    public class TreeNode<T>
+    {
+        public T Value { get; init; }
+        public TreeNode<T>? Left { get; init; }
+        public TreeNode<T>? Right { get; init; }
+    }
+    /*
+     * Check if two binary trees are structurally identical and have the same values for corresponding nodes
+     */
+    public static bool IsEqual<T>( TreeNode<T>? first, TreeNode<T>? second )
+    {
+        if ( first == null && second == null ) return true;
+        if ( first == null || second == null ) return false;
+        if ( !first.Value.Equals( second.Value ) ) return false;
+
+        bool left = IsEqual( first.Left, second.Left );
+        bool right = IsEqual( first.Right, second.Right );
+
+        return left && right;
+
+    }
+}
---
+++ algorithms/Tasks/Trees/SameTrees/SameTreesTests.cs
@@ -0,0 +1,247 @@
+﻿namespace Tasks.Trees.SameTrees;
+
+public class SameTreesTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var tree1 = new SameTrees.TreeNode<int>
+        {
+            Value = 1,
+            Left = new SameTrees.TreeNode<int>
+            {
+                Value = 2,
+            },
+            Right = new SameTrees.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+        var tree2 = new SameTrees.TreeNode<int>
+        {
+            Value = 1,
+            Left = new SameTrees.TreeNode<int>
+            {
+                Value = 2,
+            },
+            Right = new SameTrees.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+
+        // Act
+        
+        // Assert
+        Assert.Equal( true, SameTrees.IsEqual( tree1, tree2 ) );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var tree1 = new SameTrees.TreeNode<int>
+        {
+            Value = 1,
+            Left = new SameTrees.TreeNode<int>
+            {
+                Value = 2,
+                Left = new SameTrees.TreeNode<int>
+                {
+                    Value = 4,
+                },
+                Right = new SameTrees.TreeNode<int>
+                {
+                    Value = 5,
+                },
+            },
+            Right = new SameTrees.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+        var tree2 = new SameTrees.TreeNode<int>
+        {
+            Value = 1,
+            Left = new SameTrees.TreeNode<int>
+            {
+                Value = 2,
+            },
+            Right = new SameTrees.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+
+        // Act
+        
+        // Assert
+        Assert.Equal( false, SameTrees.IsEqual( tree1, tree2 ) );
+    }
+    
+    [Fact]
+    public void Run_Case3_ShouldBeOk( )
+    {
+        // Arrange
+        var tree1 = new SameTrees.TreeNode<int>
+        {
+            Value = 1,
+            Left = new SameTrees.TreeNode<int>
+            {
+                Value = 2,
+                Left = new SameTrees.TreeNode<int>
+                {
+                    Value = 4,
+                },
+                Right = new SameTrees.TreeNode<int>
+                {
+                    Value = 5,
+                },
+            },
+            Right = new SameTrees.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+        var tree2 = new SameTrees.TreeNode<int>
+        {
+            Value = 1,
+            Left = new SameTrees.TreeNode<int>
+            {
+                Value = 2,
+                Left = new SameTrees.TreeNode<int>
+                {
+                    Value = 4,
+                },
+                Right = new SameTrees.TreeNode<int>
+                {
+                    Value = 5,
+                },
+            },
+            Right = new SameTrees.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+
+        // Act
+        
+        // Assert
+        Assert.Equal( true, SameTrees.IsEqual( tree1, tree2 ) );
+    }
+    
+    [Fact]
+    public void Run_Case4_ShouldBeOk( )
+    {
+        // Arrange
+        var tree1 = new SameTrees.TreeNode<int>
+        {
+            Value = 1,
+            Left = new SameTrees.TreeNode<int>
+            {
+                Value = 2,
+                Right = new SameTrees.TreeNode<int>
+                {
+                    Value = 3,
+                    Right = new SameTrees.TreeNode<int>
+                    {
+                        Value = 4
+                    },
+                },
+            },
+            Right = new SameTrees.TreeNode<int>
+            {
+                Value = 5,
+                Right = new SameTrees.TreeNode<int>
+                {
+                    Value = 6
+                },
+            }
+        };
+        var tree2 = new SameTrees.TreeNode<int>
+        {
+            Value = 1,
+            Left = new SameTrees.TreeNode<int>
+            {
+                Value = 2,
+                Left = new SameTrees.TreeNode<int>
+                {
+                    Value = 4,
+                },
+                Right = new SameTrees.TreeNode<int>
+                {
+                    Value = 5,
+                },
+            },
+            Right = new SameTrees.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+
+        // Act
+        
+        // Assert
+        Assert.Equal( false, SameTrees.IsEqual( tree1, tree2 ) );
+    }
+    
+    [Fact]
+    public void Run_Case5_ShouldBeOk( )
+    {
+        // Arrange
+        var tree1 = new SameTrees.TreeNode<int>
+        {
+            Value = 1,
+            Left = new SameTrees.TreeNode<int>
+            {
+                Value = 2,
+                Right = new SameTrees.TreeNode<int>
+                {
+                    Value = 3,
+                    Right = new SameTrees.TreeNode<int>
+                    {
+                        Value = 4
+                    },
+                },
+            },
+            Right = new SameTrees.TreeNode<int>
+            {
+                Value = 5,
+                Right = new SameTrees.TreeNode<int>
+                {
+                    Value = 6
+                },
+            }
+        };
+        var tree2 = new SameTrees.TreeNode<int>
+        {
+            Value = 1,
+            Left = new SameTrees.TreeNode<int>
+            {
+                Value = 2,
+                Right = new SameTrees.TreeNode<int>
+                {
+                    Value = 3,
+                    Right = new SameTrees.TreeNode<int>
+                    {
+                        Value = 4
+                    },
+                },
+            },
+            Right = new SameTrees.TreeNode<int>
+            {
+                Value = 5,
+                Right = new SameTrees.TreeNode<int>
+                {
+                    Value = 6
+                },
+            }
+        };
+
+        // Act
+        
+        // Assert
+        Assert.Equal( true, SameTrees.IsEqual( tree1, tree2 ) );
+    }
+}
