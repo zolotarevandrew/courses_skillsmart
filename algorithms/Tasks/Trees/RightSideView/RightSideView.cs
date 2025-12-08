52fb080c23f6f737d4956528d87b7bc716b0081a
a.zolotarev <a.zolotarev@travelline.ru>
2025-12-08 09:28:21 +0300

    Trees: right side view

---
+++ algorithms/Tasks/Trees/RightSideView/RightSideView.cs
@@ -0,0 +1,40 @@
+﻿namespace Tasks.Trees.RightSideView;
+
+public class RightSideView
+{
+    public class TreeNode<T>
+    {
+        public T Value { get; init; }
+        public TreeNode<T>? Left { get; init; }
+        public TreeNode<T>? Right { get; init; }
+    }
+
+    public static List<T> Visit<T>( TreeNode<T>? node )
+    {
+        if ( node == null ) return [];
+
+        List<T> result = [];
+        Queue<TreeNode<T>> queue = new Queue<TreeNode<T>>();
+        queue.Enqueue( node );
+        while ( queue.Count > 0 )
+        {
+            TreeNode<T> last = null;
+            int size = queue.Count;
+            for ( int i = 0; i < size; i++ )
+            {
+                last = queue.Dequeue();
+                if ( last.Left != null )
+                {
+                    queue.Enqueue( last.Left );
+                }
+
+                if ( last.Right != null )
+                {
+                    queue.Enqueue( last.Right );
+                }
+            }
+            result.Add( last!.Value );
+        }
+        return result;
+    }
+}
---
+++ algorithms/Tasks/Trees/RightSideView/RightSideViewTests.cs
@@ -0,0 +1,100 @@
+﻿namespace Tasks.Trees.RightSideView;
+
+public class RightSideViewTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new RightSideView.TreeNode<int>
+        {
+            Value = 1,
+            Left = new RightSideView.TreeNode<int>
+            {
+                Value = 2,
+            },
+            Right = new RightSideView.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+
+        // Act
+        List<int> res = RightSideView.Visit( node );
+        
+        // Assert
+        Assert.Equal( res, [1, 3] );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new RightSideView.TreeNode<int>
+        {
+            Value = 1,
+            Left = new RightSideView.TreeNode<int>
+            {
+                Value = 2,
+                Left = new RightSideView.TreeNode<int>
+                {
+                    Value = 4,
+                },
+                Right = new RightSideView.TreeNode<int>
+                {
+                    Value = 5,
+                }
+            },
+            Right = new RightSideView.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+
+        // Act
+        List<int> res = RightSideView.Visit( node );
+        
+        // Assert
+        Assert.Equal( res, [1, 3, 5] );
+    }
+    
+    [Fact]
+    public void Run_Case3_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new RightSideView.TreeNode<int>
+        {
+            Value = 1,
+            Left = new RightSideView.TreeNode<int>
+            {
+                Value = 2,
+                Left = new RightSideView.TreeNode<int>
+                {
+                    Value = 4,
+                },
+                Right = new RightSideView.TreeNode<int>
+                {
+                    Value = 5,
+                }
+            },
+            Right = new RightSideView.TreeNode<int>
+            {
+                Value = 3,
+                Left = new RightSideView.TreeNode<int>
+                {
+                    Value = 6,
+                },
+                Right = new RightSideView.TreeNode<int>
+                {
+                    Value = 7,
+                }
+            }
+        };
+
+        // Act
+        List<int> res = RightSideView.Visit( node );
+        
+        // Assert
+        Assert.Equal( res, [1, 3, 7] );
+    }
+}
