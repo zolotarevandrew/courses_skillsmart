e033d9f6388d25c0ef7a8d87702a1851e1f63f4d
a.zolotarev <a.zolotarev@travelline.ru>
2025-12-09 09:33:28 +0300

    Trees: zig zag traversal

---
+++ algorithms/Tasks/Trees/ZigZag/TreeZigZagTraversal.cs
@@ -0,0 +1,51 @@
+﻿namespace Tasks.Trees.ZigZag;
+
+public class TreeZigZagTraversal
+{
+    public class TreeNode<T>
+    {
+        public T Value { get; init; }
+        public TreeNode<T>? Left { get; init; }
+        public TreeNode<T>? Right { get; init; }
+    }
+
+    /*
+     * Perform a zigzag traversal, alternating between left-to-right, right-to-left traversal
+     */
+    public static List<T[]> Visit<T>( TreeNode<T>? tree )
+    {
+        List<T[]> res = [];
+        if ( tree == null ) return res;
+
+        Queue<TreeNode<T>> queue = new Queue<TreeNode<T>>();
+        queue.Enqueue( tree );
+
+        bool isLeftToRight = true;
+        while ( queue.Count > 0 )
+        {
+            int size = queue.Count;
+            T[] arr = new T[size];
+            for ( int i = 0; i < size; i++ )
+            {
+                TreeNode<T> node = queue.Dequeue( );
+
+                int idx = isLeftToRight ? i : size - 1 - i;
+                arr[idx] = node.Value; 
+                
+                if ( node.Left != null )
+                {
+                    queue.Enqueue( node.Left );
+                }
+                
+                if ( node.Right != null )
+                {
+                    queue.Enqueue( node.Right );
+                }
+            }
+
+            res.Add( arr );
+            isLeftToRight = !isLeftToRight;
+        }
+        return res;
+    }
+}
---
+++ algorithms/Tasks/Trees/ZigZag/TreeZigZagTraversalTests.cs
@@ -0,0 +1,183 @@
+﻿namespace Tasks.Trees.ZigZag;
+
+public class TreeZigZagTraversalTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreeZigZagTraversal.TreeNode<int>
+        {
+            Value = 1,
+            Left = new TreeZigZagTraversal.TreeNode<int>
+            {
+                Value = 2,
+            },
+            Right = new TreeZigZagTraversal.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+
+        // Act
+        List<int[]> res = TreeZigZagTraversal.Visit( node );
+        
+        // Assert
+        Assert.Equal( 2, res.Count );
+        Assert.Equal( [1], res[0] );
+        Assert.Equal( [3, 2], res[1] );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreeZigZagTraversal.TreeNode<int>
+        {
+            Value = 1
+        };
+
+        // Act
+        List<int[]> res = TreeZigZagTraversal.Visit( node );
+        
+        // Assert
+        Assert.Equal( 1, res.Count );
+        Assert.Equal( [1], res[0] );
+    }
+    
+    [Fact]
+    public void Run_Case3_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreeZigZagTraversal.TreeNode<int>
+        {
+            Value = 1,
+            Left = new TreeZigZagTraversal.TreeNode<int>
+            {
+                Value = 2,
+                Left = new TreeZigZagTraversal.TreeNode<int>
+                {
+                    Value = 4,
+                },
+                Right = new TreeZigZagTraversal.TreeNode<int>
+                {
+                    Value = 5,
+                }
+            },
+            Right = new TreeZigZagTraversal.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+
+        // Act
+        List<int[]> res = TreeZigZagTraversal.Visit( node );
+        
+        // Assert
+        Assert.Equal( 3, res.Count );
+        Assert.Equal( [1], res[0] );
+        Assert.Equal( [3, 2], res[1] );
+        Assert.Equal( [4, 5], res[2] );
+    }
+    
+    [Fact]
+    public void Run_Case4_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreeZigZagTraversal.TreeNode<int>
+        {
+            Value = 1,
+            Left = new TreeZigZagTraversal.TreeNode<int>
+            {
+                Value = 2,
+                Left = new TreeZigZagTraversal.TreeNode<int>
+                {
+                    Value = 4,
+                },
+                Right = new TreeZigZagTraversal.TreeNode<int>
+                {
+                    Value = 5,
+                }
+            },
+            Right = new TreeZigZagTraversal.TreeNode<int>
+            {
+                Value = 3,
+                Left = new TreeZigZagTraversal.TreeNode<int>
+                {
+                    Value = 6,
+                },
+                Right = new TreeZigZagTraversal.TreeNode<int>
+                {
+                    Value = 7,
+                }
+            }
+        };
+
+        // Act
+        List<int[]> res = TreeZigZagTraversal.Visit( node );
+        
+        // Assert
+        Assert.Equal( 3, res.Count );
+        
+        Assert.Equal( [1], res[0] );
+        Assert.Equal( [3, 2], res[1] );
+        Assert.Equal( [4, 5, 6, 7], res[2] );
+    }
+    
+    [Fact]
+    public void Run_Case5_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreeZigZagTraversal.TreeNode<int>
+        {
+            Value = 1,
+            Left = new TreeZigZagTraversal.TreeNode<int>
+            {
+                Value = 2,
+                Left = new TreeZigZagTraversal.TreeNode<int>
+                {
+                    Value = 4,
+                    Left = new TreeZigZagTraversal.TreeNode<int>
+                    {
+                        Value = 8,
+                    },
+                    Right = new TreeZigZagTraversal.TreeNode<int>
+                    {
+                        Value = 9,
+                    }
+                },
+                Right = new TreeZigZagTraversal.TreeNode<int>
+                {
+                    Value = 5,
+                    Left = new TreeZigZagTraversal.TreeNode<int>
+                    {
+                        Value = 10,
+                    },
+                }
+            },
+            Right = new TreeZigZagTraversal.TreeNode<int>
+            {
+                Value = 3,
+                Left = new TreeZigZagTraversal.TreeNode<int>
+                {
+                    Value = 6,
+                },
+                Right = new TreeZigZagTraversal.TreeNode<int>
+                {
+                    Value = 7,
+                }
+            }
+        };
+
+        // Act
+        List<int[]> res = TreeZigZagTraversal.Visit( node );
+        
+        // Assert
+        Assert.Equal( 4, res.Count );
+        
+        Assert.Equal( [1], res[0] );
+        Assert.Equal( [3, 2], res[1] );
+        Assert.Equal( [4, 5, 6, 7], res[2] );
+        Assert.Equal( [10, 9, 8], res[3] );
+    }
+}
