15751904838f305cd4b2ee9c32b39c52890b55e7
a.zolotarev <a.zolotarev@travelline.ru>
2025-12-10 09:06:55 +0300

    Tree: largest row value

---
+++ algorithms/Tasks/Trees/LargestRow/LargestRowValue.cs
@@ -0,0 +1,46 @@
+﻿using System.Numerics;
+
+namespace Tasks.Trees.LargestRow;
+
+public class LargestRowValue
+{
+    public class TreeNode<T>
+    {
+        public T Value { get; init; }
+        public TreeNode<T>? Left { get; init; }
+        public TreeNode<T>? Right { get; init; }
+    }
+    
+    /*
+     * Return an array of the largest value in each row of a binary tree.
+     */
+    public static List<T> Visit<T>( TreeNode<T> tree )
+        where T : INumber<T>
+    {
+        List<T> res = [];
+        Queue<TreeNode<T>> queue = new Queue<TreeNode<T>>( );
+        queue.Enqueue( tree );
+        while ( queue.Count > 0 )
+        {
+            int size = queue.Count;
+            T max = T.Zero;
+            for ( int i = 0; i < size; i++ )
+            {
+                TreeNode<T> node = queue.Dequeue( );
+
+                max = T.Max( node.Value, max );
+                if ( node.Left != null )
+                {
+                    queue.Enqueue( node.Left );
+                }
+                
+                if ( node.Right != null )
+                {
+                    queue.Enqueue( node.Right );
+                }
+            }
+            res.Add( max );
+        }
+        return res;
+    }
+}
---
+++ algorithms/Tasks/Trees/LargestRow/LargestRowValueTests.cs
@@ -0,0 +1,100 @@
+﻿namespace Tasks.Trees.LargestRow;
+
+public class LargestRowValueTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new LargestRowValue.TreeNode<int>
+        {
+            Value = 1,
+            Left = new LargestRowValue.TreeNode<int>
+            {
+                Value = 2,
+            },
+            Right = new LargestRowValue.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+
+        // Act
+        List<int> res = LargestRowValue.Visit( node );
+        
+        // Assert
+        Assert.Equal( res, [1, 3] );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new LargestRowValue.TreeNode<int>
+        {
+            Value = 1,
+            Left = new LargestRowValue.TreeNode<int>
+            {
+                Value = 2,
+                Left = new LargestRowValue.TreeNode<int>
+                {
+                    Value = 4,
+                },
+                Right = new LargestRowValue.TreeNode<int>
+                {
+                    Value = 5,
+                }
+            },
+            Right = new LargestRowValue.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+
+        // Act
+        List<int> res = LargestRowValue.Visit( node );
+        
+        // Assert
+        Assert.Equal( res, [1, 3, 5] );
+    }
+    
+    [Fact]
+    public void Run_Case3_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new LargestRowValue.TreeNode<int>
+        {
+            Value = 1,
+            Left = new LargestRowValue.TreeNode<int>
+            {
+                Value = 2,
+                Left = new LargestRowValue.TreeNode<int>
+                {
+                    Value = 4,
+                },
+                Right = new LargestRowValue.TreeNode<int>
+                {
+                    Value = 5,
+                }
+            },
+            Right = new LargestRowValue.TreeNode<int>
+            {
+                Value = 3,
+                Left = new LargestRowValue.TreeNode<int>
+                {
+                    Value = 6,
+                },
+                Right = new LargestRowValue.TreeNode<int>
+                {
+                    Value = 7,
+                }
+            }
+        };
+
+        // Act
+        List<int> res = LargestRowValue.Visit( node );
+        
+        // Assert
+        Assert.Equal( res, [1, 3, 7] );
+    }
+}
