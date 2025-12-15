0284e9a98f001cf549dbbef5ee058b7c0f16db32
a.zolotarev <a.zolotarev@travelline.ru>
2025-12-15 09:19:16 +0300

    Trees: subtree

---
+++ algorithms/Tasks/Trees/Subtree/SubTree.cs
@@ -0,0 +1,35 @@
+﻿namespace Tasks.Trees.Subtree;
+
+public static class SubTree
+{
+    public class TreeNode<T>
+    {
+        public T Value { get; init; }
+        public TreeNode<T>? Left { get; init; }
+        public TreeNode<T>? Right { get; init; }
+    }
+
+    /*
+     * Determine if a tree subroot is a subtree of a root.
+     * A subtree consists of a node in root and all its descendants
+     * with the same structure and values as subroot.
+     */
+    public static bool Check<T>( TreeNode<T>? root, TreeNode<T>? subtree )
+    {
+        if ( root == null ) return false;
+
+        bool IsIdentical( TreeNode<T>? a, TreeNode<T>? b )
+        {
+            if ( a == null && b == null ) return true;
+            if ( a == null || b == null ) return false;
+            
+            return a.Value.Equals( b.Value )
+                && IsIdentical( a.Left, b.Left )
+                && IsIdentical( a.Right, b.Right );
+        }
+
+        return IsIdentical( root, subtree ) ||
+               IsIdentical( root.Left, subtree ) ||
+               IsIdentical( root.Right, subtree );
+    }
+}
---
+++ algorithms/Tasks/Trees/Subtree/SubTreeTests.cs
@@ -0,0 +1,205 @@
+﻿namespace Tasks.Trees.Subtree;
+
+public class SubTreeTests
+{
+    [Fact]
+    public void Run_IdenticalTrees_ShouldBeOk( )
+    {
+        // Arrange
+        var tree1 = new SubTree.TreeNode<int>
+        {
+            Value = 1,
+            Left = new SubTree.TreeNode<int>
+            {
+                Value = 2,
+            },
+            Right = new SubTree.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+        var tree2 = new SubTree.TreeNode<int>
+        {
+            Value = 1,
+            Left = new SubTree.TreeNode<int>
+            {
+                Value = 2,
+            },
+            Right = new SubTree.TreeNode<int>
+            {
+                Value = 3,
+            }
+        };
+
+        // Act
+        
+        // Assert
+        Assert.Equal( true, SubTree.Check( tree1, tree2 ) );
+    }
+    
+    [Fact]
+    public void Run_IdenticalTreesInRight_ShouldBeOk( )
+    {
+        // Arrange
+        var tree1 = new SubTree.TreeNode<int>
+        {
+            Value = 2,
+            Right = new SubTree.TreeNode<int>
+            {
+                Value = 3,
+                Right = new SubTree.TreeNode<int>
+                {
+                    Value = 4,
+                }
+            }
+        };
+        var tree2 = new SubTree.TreeNode<int>
+        {
+            Value = 2,
+            Right = new SubTree.TreeNode<int>
+            {
+                Value = 3,
+                Right = new SubTree.TreeNode<int>
+                {
+                    Value = 4,
+                }
+            }
+        };
+
+        // Act
+        
+        // Assert
+        Assert.Equal( true, SubTree.Check( tree1, tree2 ) );
+    }
+    
+    [Fact]
+    public void Run_SubTreeRight_ShouldBeOk( )
+    {
+        // Arrange
+        var tree1 = new SubTree.TreeNode<int>
+        {
+            Value = 1,
+            Left = new SubTree.TreeNode<int>
+            {
+                Value = 2,
+                Right = new SubTree.TreeNode<int>
+                {
+                    Value = 3,
+                    Right = new SubTree.TreeNode<int>
+                    {
+                        Value = 4,
+                    }
+                }
+            }
+        };
+        var tree2 = new SubTree.TreeNode<int>
+        {
+            Value = 2,
+            Right = new SubTree.TreeNode<int>
+            {
+                Value = 3,
+                Right = new SubTree.TreeNode<int>
+                {
+                    Value = 4,
+                }
+            }
+        };
+
+        // Act
+        
+        // Assert
+        Assert.Equal( true, SubTree.Check( tree1, tree2 ) );
+    }
+    
+    [Fact]
+    public void Run_PartialSubTree_ShouldBeFalse( )
+    {
+        // Arrange
+        var tree1 = new SubTree.TreeNode<int>
+        {
+            Value = 1,
+            Left = new SubTree.TreeNode<int>
+            {
+                Value = 2,
+                Left = new SubTree.TreeNode<int>
+                {
+                    Value = 3,
+                    Left = new SubTree.TreeNode<int>
+                    {
+                        Value = 4,
+                    }
+                }
+            },
+            
+            Right = new SubTree.TreeNode<int>
+            {
+                Value = 5,
+                Right = new SubTree.TreeNode<int>
+                {
+                    Value = 6,
+                }
+            }
+        };
+        var tree2 = new SubTree.TreeNode<int>
+        {
+            Value = 5,
+            Right = new SubTree.TreeNode<int>
+            {
+                Value = 6,
+                Right = new SubTree.TreeNode<int>
+                {
+                    Value = 8,
+                }
+            }
+        };
+
+        // Act
+        
+        // Assert
+        Assert.Equal( false, SubTree.Check( tree1, tree2 ) );
+    }
+    
+    [Fact]
+    public void Run_FullSubTree_ShouldBeTrue( )
+    {
+        // Arrange
+        var tree1 = new SubTree.TreeNode<int>
+        {
+            Value = 1,
+            Left = new SubTree.TreeNode<int>
+            {
+                Value = 2,
+                Left = new SubTree.TreeNode<int>
+                {
+                    Value = 3,
+                    Left = new SubTree.TreeNode<int>
+                    {
+                        Value = 4,
+                    }
+                }
+            },
+            
+            Right = new SubTree.TreeNode<int>
+            {
+                Value = 5,
+                Right = new SubTree.TreeNode<int>
+                {
+                    Value = 6,
+                }
+            }
+        };
+        var tree2 = new SubTree.TreeNode<int>
+        {
+            Value = 5,
+            Right = new SubTree.TreeNode<int>
+            {
+                Value = 6
+            }
+        };
+
+        // Act
+        
+        // Assert
+        Assert.Equal( true, SubTree.Check( tree1, tree2 ) );
+    }
+}
