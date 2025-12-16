88a4f4926a86a8dd69b1fc3d699e19eb8642d262
a.zolotarev <a.zolotarev@travelline.ru>
2025-12-16 09:18:27 +0300

    Trees: Depth

---
+++ algorithms/Tasks/Trees/Depth/TreeDepth.cs
@@ -0,0 +1,21 @@
+﻿namespace Tasks.Trees.Depth;
+
+public class TreeDepth
+{
+    public class TreeNode<T>
+    {
+        public T Value { get; init; }
+        public TreeNode<T>? Left { get; init; }
+        public TreeNode<T>? Right { get; init; }
+    }
+
+    public static int Get<T>( TreeNode<T>? root )
+    {
+        if ( root == null ) return 0;
+        
+        int left = Get( root.Left );
+        int right = Get( root.Right );
+
+        return + 1 + Math.Max( left, right );
+    }
+}
---
+++ algorithms/Tasks/Trees/Depth/TreeDepthTests.cs
@@ -0,0 +1,128 @@
+﻿namespace Tasks.Trees.Depth;
+
+public class TreeDepthTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreeDepth.TreeNode<int>
+        {
+            Value = 1,
+            Left = new TreeDepth.TreeNode<int>
+            {
+                Value = 2,
+            },
+            Right = new TreeDepth.TreeNode<int>
+            {
+                Value = 3
+            }
+        };
+        
+        // Act
+        var res = TreeDepth.Get( node );
+
+        // Assert
+        Assert.Equal( 2, res );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreeDepth.TreeNode<int>
+        {
+            Value = 1,
+            Left = new TreeDepth.TreeNode<int>
+            {
+                Value = 2,
+            }
+        };
+        
+        // Act
+        var res = TreeDepth.Get( node );
+
+        // Assert
+        Assert.Equal( 2, res );
+    }
+    
+    [Fact]
+    public void Run_Case3_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreeDepth.TreeNode<int>
+        {
+            Value = 1,
+            Left = new TreeDepth.TreeNode<int>
+            {
+                Value = 2,
+                Left = new TreeDepth.TreeNode<int>
+                {
+                    Value = 3,
+                }
+            }
+        };
+        
+        // Act
+        var res = TreeDepth.Get( node );
+
+        // Assert
+        Assert.Equal( 3, res );
+    }
+    
+    [Fact]
+    public void Run_Case4_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreeDepth.TreeNode<int>
+        {
+            Value = 1,
+            Right = new TreeDepth.TreeNode<int>
+            {
+                Value = 2,
+                Right = new TreeDepth.TreeNode<int>
+                {
+                    Value = 3,
+                }
+            }
+        };
+        
+        // Act
+        var res = TreeDepth.Get( node );
+
+        // Assert
+        Assert.Equal( 3, res );
+    }
+    
+    [Fact]
+    public void Run_Case5_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreeDepth.TreeNode<int>
+        {
+            Value = 1,
+            Left = new TreeDepth.TreeNode<int>
+            {
+                Value = 2,
+                Left = new TreeDepth.TreeNode<int>
+                {
+                    Value = 3,
+                    Left = new TreeDepth.TreeNode<int>
+                    {
+                        Value = 5,
+                    }
+                },
+                Right = new TreeDepth.TreeNode<int>
+                {
+                    Value = 4,
+                }
+            }
+        };
+        
+        // Act
+        var res = TreeDepth.Get( node );
+
+        // Assert
+        Assert.Equal( 4, res );
+    }
+}
