7ae19b337747ac4f93c8388b6ae7e54daef472be
a.zolotarev <a.zolotarev@travelline.ru>
2025-12-23 09:19:32 +0300

    Trees: TreeBalanced

---
+++ algorithms/Tasks/Trees/Balanced/TreeBalanced.cs
@@ -0,0 +1,42 @@
+﻿namespace Tasks.Trees.Balanced;
+
+public class TreeBalanced
+{
+    public class TreeNode<T>
+    {
+        public T Value { get; init; }
+        public TreeNode<T>? Left { get; init; }
+        public TreeNode<T>? Right { get; init; }
+    }
+    
+    /*
+     * A binary tree is height-balanced if the depths of the two subtrees
+     * of every node differ by at most 1.
+     * 
+     */
+    public static bool Run<T>( TreeNode<T>? root )
+    {
+        if ( root == null ) return false;
+
+        int Height( TreeNode<T>? node )
+        {
+            if ( node == null ) return 0;
+
+            int left = Height( node.Left );
+            if ( left == -1 ) return -1;
+            
+            int right = Height( node.Right );
+            if ( right == -1 ) return -1;
+
+            int diff = Math.Abs( left - right );
+            if ( diff > 1 )
+            {
+                return -1;
+            }
+
+            return 1 + Math.Max( left, right );
+        }
+        
+        return Height( root ) != -1;
+    }
+}
---
+++ algorithms/Tasks/Trees/Balanced/TreeBalancedTests.cs
@@ -0,0 +1,88 @@
+﻿namespace Tasks.Trees.Balanced;
+
+public class TreeBalancedTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreeBalanced.TreeNode<int>
+        {
+            Value = 1,
+            Left = new TreeBalanced.TreeNode<int>
+            {
+                Value = 2,
+            },
+            Right = new TreeBalanced.TreeNode<int>
+            {
+                Value = 3
+            }
+        };
+        
+        // Act
+        var res = TreeBalanced.Run( node );
+
+        // Assert
+        Assert.True( res );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreeBalanced.TreeNode<int>
+        {
+            Value = 1,
+            Left = new TreeBalanced.TreeNode<int>
+            {
+                Value = 2,
+                Left = new TreeBalanced.TreeNode<int>
+                {
+                    Value = 4,
+                },
+            },
+            Right = new TreeBalanced.TreeNode<int>
+            {
+                Value = 3
+            }
+        };
+        
+        // Act
+        var res = TreeBalanced.Run( node );
+
+        // Assert
+        Assert.True( res );
+    }
+    
+    [Fact]
+    public void Run_Case3_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreeBalanced.TreeNode<int>
+        {
+            Value = 1,
+            Left = new TreeBalanced.TreeNode<int>
+            {
+                Value = 2,
+                Left = new TreeBalanced.TreeNode<int>
+                {
+                    Value = 4,
+                    Left = new TreeBalanced.TreeNode<int>
+                    {
+                        Value = 6,
+                    },
+                },
+            },
+            Right = new TreeBalanced.TreeNode<int>
+            {
+                Value = 3
+            }
+        };
+        
+        // Act
+        bool res = TreeBalanced.Run( node );
+
+        // Assert
+        Assert.False( res );
+    }
+}
