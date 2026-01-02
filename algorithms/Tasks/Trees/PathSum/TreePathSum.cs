0f88155aae8e808f1de7c376246527a6039903e5
a.zolotarev <a.zolotarev@travelline.ru>
2026-01-02 14:31:00 +0300

    Tree: path sum

---
+++ algorithms/Tasks/Trees/PathSum/TreePathSum.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+
+namespace Tasks.Trees.PathSum;
+
+public class TreePathSum
+{
+    public class TreeNode<T>
+    {
+        public T Value { get; init; }
+        public TreeNode<T>? Left { get; init; }
+        public TreeNode<T>? Right { get; init; }
+    }
+
+    public static bool Run<T>( TreeNode<T>? root, T sum )
+        where T : INumber<T>
+    {
+        bool Dfs( TreeNode<T>? node, T curSum )
+        {
+            if ( node == null ) return false;
+            
+            curSum += node.Value;
+
+            if ( node.Left == null && node.Right == null )
+            {
+                return curSum == sum;
+            }
+
+            return Dfs( node.Left, curSum ) || Dfs( node.Right, curSum );
+        }
+
+        return Dfs( root, T.Zero );
+    }
+}
---
+++ algorithms/Tasks/Trees/PathSum/TreePathSumTests.cs
@@ -0,0 +1,120 @@
+﻿namespace Tasks.Trees.PathSum;
+
+public class TreePathSumTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        
+        // Act
+        var res = TreePathSum.Run( null, 0 );
+
+        // Assert
+        Assert.False( res );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreePathSum.TreeNode<int>
+        {
+            Value = 1
+        };
+        
+        // Act
+        var res = TreePathSum.Run( node, 1 );
+
+        // Assert
+        Assert.True( res );
+    }
+    
+    [Fact]
+    public void Run_Case3_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreePathSum.TreeNode<int>
+        {
+            Value = 1,
+            Left = new TreePathSum.TreeNode<int>
+            {
+                Value = 3
+            }
+        };
+        
+        // Act
+        var res = TreePathSum.Run( node, 4 );
+
+        // Assert
+        Assert.True( res );
+    }
+    
+    [Fact]
+    public void Run_Case4_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreePathSum.TreeNode<int>
+        {
+            Value = 1,
+            Left = new TreePathSum.TreeNode<int>
+            {
+                Value = 2
+            }
+        };
+        
+        // Act
+        var res = TreePathSum.Run( node, 4 );
+
+        // Assert
+        Assert.False( res );
+    }
+    
+    [Fact]
+    public void Run_Case5_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreePathSum.TreeNode<int>
+        {
+            Value = 1,
+            Left = new TreePathSum.TreeNode<int>
+            {
+                Value = 2
+            },
+            Right = new TreePathSum.TreeNode<int>
+            {
+                Value = 3
+            }
+        };
+        
+        // Act
+        var res = TreePathSum.Run( node, 4 );
+
+        // Assert
+        Assert.True( res );
+    }
+    
+    [Fact]
+    public void Run_Case6_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new TreePathSum.TreeNode<int>
+        {
+            Value = 1,
+            Left = new TreePathSum.TreeNode<int>
+            {
+                Value = 2
+            },
+            Right = new TreePathSum.TreeNode<int>
+            {
+                Value = 3
+            }
+        };
+        
+        // Act
+        var res = TreePathSum.Run( node, 5 );
+
+        // Assert
+        Assert.False( res );
+    }
+}
