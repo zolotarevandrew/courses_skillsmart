0643c0e41ded4912facb75e0eb2ef3f71a9cb9bb
a.zolotarev <a.zolotarev@travelline.ru>
2026-01-05 12:09:13 +0300

    Heap: K largest

---
+++ algorithms/Tasks/Heaps/KLargest/HeapKLargest.cs
@@ -0,0 +1,37 @@
+﻿namespace Tasks.Heaps.KLargest;
+
+public class HeapKLargest
+{
+    /*
+     * Given an integer array nums and an integer k.
+     * Find the element that ranks as kth highest in the array
+     * When sorted in descending order.
+     * Note that it is the kth largest element in the sorted sequence,
+     * not necessarily a distinct value.
+     */
+    public static int Run( int[] nums, int k )
+    {
+        if ( nums.Length == 0 ) return -1;
+        if ( k <= 0 || k > nums.Length ) return -1;
+
+        PriorityQueue<int, int> priorityQueue = new PriorityQueue<int, int>( k );
+
+        for ( int i = 0; i < k; i++ )
+        {
+            priorityQueue.Enqueue( nums[i], nums[i] );
+        }
+        
+        for ( int i = k; i < nums.Length; i++ )
+        {
+            int element = priorityQueue.Peek( );
+            if ( nums[i] > element )
+            {
+                priorityQueue.DequeueEnqueue( nums[i], nums[i] );    
+            }
+        }
+
+        int res = priorityQueue.Dequeue( );
+        priorityQueue.Clear( );
+        return res;
+    }
+}
---
+++ algorithms/Tasks/Heaps/KLargest/HeapKLargestTests.cs
@@ -0,0 +1,104 @@
+﻿namespace Tasks.Heaps.KLargest;
+
+public class HeapKLargestTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Act
+        int res = HeapKLargest.Run( [0, 3, 1, 5, 2, 7, 8, 4], 4 );
+
+        // Assert
+        Assert.Equal( 4, res );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Act
+        int res = HeapKLargest.Run( [0, 3, 1], 4 );
+
+        // Assert
+        Assert.Equal( -1, res );
+    }
+    
+    [Fact]
+    public void Run_Case3_ShouldBeOk( )
+    {
+        // Act
+        int res = HeapKLargest.Run( [0, 3, 1], 0 );
+
+        // Assert
+        Assert.Equal( -1, res );
+    }
+    
+    [Fact]
+    public void Run_Case4_ShouldBeOk( )
+    {
+        // Act
+        int res = HeapKLargest.Run( [0, 3, 1], -1 );
+
+        // Assert
+        Assert.Equal( -1, res );
+    }
+    
+    [Fact]
+    public void Run_Case5_ShouldBeOk( )
+    {
+        // Act
+        int res = HeapKLargest.Run( [0, 3, 1, 5], 4 );
+
+        // Assert
+        Assert.Equal( 0, res );
+    }
+    
+    [Fact]
+    public void Run_Case6_ShouldBeOk( )
+    {
+        // Act
+        int res = HeapKLargest.Run( [0, 3, 1, 5], 3 );
+
+        // Assert
+        Assert.Equal( 1, res );
+    }
+    
+    [Fact]
+    public void Run_Case7_ShouldBeOk( )
+    {
+        // Act
+        int res = HeapKLargest.Run( [0, 3, 1, 5], 2 );
+
+        // Assert
+        Assert.Equal( 3, res );
+    }
+    
+    [Fact]
+    public void Run_Case8_ShouldBeOk( )
+    {
+        // Act
+        int res = HeapKLargest.Run( [0, 3, 1, 5], 1 );
+
+        // Assert
+        Assert.Equal( 5, res );
+    }
+    
+    [Fact]
+    public void Run_Case9_ShouldBeOk( )
+    {
+        // Act
+        int res = HeapKLargest.Run( [8, 7, 6, 5, 4, 3, 2, 1], 4 );
+
+        // Assert
+        Assert.Equal( 5, res );
+    }
+    
+    [Fact]
+    public void Run_Case10_ShouldBeOk( )
+    {
+        // Act
+        int res = HeapKLargest.Run( [1, 2, 3, 4, 8, 5, 7, 6], 4 );
+
+        // Assert
+        Assert.Equal( 5, res );
+    }
+}
