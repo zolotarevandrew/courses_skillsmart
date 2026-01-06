6248ec425c47f3438e5fee12b7d8fc9e30cf4c61
a.zolotarev <a.zolotarev@travelline.ru>
2026-01-06 11:21:55 +0300

    Heap: K frequent

---
+++ algorithms/Tasks/Heaps/KFrequent/HeapKFrequent.cs
@@ -0,0 +1,42 @@
+﻿namespace Tasks.Heaps.KFrequent;
+
+public class HeapKFrequent
+{
+    /*
+     * Given an integer array nums and an integer k.
+     * Find the k elements that appear most frequently in the array.
+     */
+    public static List<int> Run( int[] nums, int k )
+    {
+        if ( nums.Length == 0 || k <= 0 ) return [];
+        
+        Dictionary<int, int> frequencyByNum = new Dictionary<int, int>();
+        for ( int i = 0; i < nums.Length; i++ )
+        {
+            int num = nums[i];
+            if ( !frequencyByNum.TryAdd( num, 1 ) )
+            {
+                frequencyByNum[num]++;
+            }
+        }
+
+        PriorityQueue<int, int> heap = new PriorityQueue<int, int>( k );
+        foreach ( ( int num, int frequency ) in frequencyByNum )
+        {
+            heap.Enqueue( num, frequency );
+
+            if ( heap.Count > k )
+            {
+                heap.Dequeue( );
+            }
+        }
+
+        List<int> result = [];
+        while ( heap.Count > 0 )
+        {
+            result.Add( heap.Dequeue() );
+        }
+
+        return result;
+    }
+}
---
+++ algorithms/Tasks/Heaps/KFrequent/HeapKFrequentTests.cs
@@ -0,0 +1,75 @@
+﻿namespace Tasks.Heaps.KFrequent;
+
+public class HeapKFrequentTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Act
+        List<int> res = HeapKFrequent.Run( [4, 4, 4, 6, 6, 7, 7, 7, 7], 2 );
+
+        Assert.NotNull( res );
+        Assert.Equal( 2, res.Count );
+        Assert.Equal( 4, res[0] );
+        Assert.Equal( 7, res[1] );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Act
+        List<int> res = HeapKFrequent.Run( [0, 3, 1, 5], 0 );
+
+        Assert.NotNull( res );
+        Assert.Equal( 0, res.Count );
+    }
+    
+    [Fact]
+    public void Run_Case3_ShouldBeOk( )
+    {
+        // Act
+        List<int> res = HeapKFrequent.Run( [0, 3, 1, 5], -1 );
+
+        Assert.NotNull( res );
+        Assert.Equal( 0, res.Count );
+    }
+
+    [Fact]
+    public void Run_Case4_ShouldBeOk( )
+    {
+        // Act
+        List<int> res = HeapKFrequent.Run( [0, 3, 1, 5], 4 );
+
+        Assert.NotNull( res );
+        Assert.Equal( 4, res.Count );
+        Assert.Equal( 0, res[0] );
+        Assert.Equal( 5, res[1] );
+        Assert.Equal( 1, res[2] );
+        Assert.Equal( 3, res[3] );
+    }
+    
+    [Fact]
+    public void Run_Case5_ShouldBeOk( )
+    {
+        // Act
+        List<int> res = HeapKFrequent.Run( [0, 3, 1, 5], 3 );
+
+        Assert.NotNull( res );
+        Assert.Equal( 3, res.Count );
+        Assert.Equal( 5, res[0] );
+        Assert.Equal( 1, res[1] );
+        Assert.Equal( 3, res[2] );
+    }
+    
+    [Fact]
+    public void Run_Case6_ShouldBeOk( )
+    {
+        // Act
+        List<int> res = HeapKFrequent.Run( [0, 3, 1, 5], 2 );
+
+        Assert.NotNull( res );
+        Assert.Equal( 2, res.Count );
+        Assert.Equal( 5, res[0] );
+        Assert.Equal( 3, res[1] );
+    }
+}
