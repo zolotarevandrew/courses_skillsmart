4691d02cf734eb2612f8718d3edae8e272c4371b
a.zolotarev <a.zolotarev@travelline.ru>
2026-01-07 12:01:21 +0300

    Heap: K closest

---
+++ algorithms/Tasks/Heaps/KClosest/HeapKClosest.cs
@@ -0,0 +1,42 @@
+﻿namespace Tasks.Heaps.KClosest;
+
+public record struct HeapPoint( int X, int Y );
+
+public class HeapKClosest
+{
+    /*
+     * Given the list of coordinates where points[i] = [xi,yi]
+     * represents a point on the X-Y plane, and an integer k,
+     * Find the k closest point to the origin (0,0).
+     *
+     * The closeness of a point is determined by its Euclidean distance from the origin
+     * distance = sqrt(x^2 + y^2)
+     * Return the answer in any order. The answer is unique except for the order of elements.
+     */
+    public static List<HeapPoint> Run( HeapPoint[] points, int k )
+    {
+        if ( points.Length == 0 || k <= 0 || k >= points.Length ) return [];
+
+        PriorityQueue<HeapPoint, double> heap = new PriorityQueue<HeapPoint, double>( points.Length );
+        for ( int i = 0; i < points.Length; i++ )
+        {
+            HeapPoint point = points[i];
+            double distance = CalcDistance( point );
+            heap.Enqueue( point, distance );
+        }
+
+        List<HeapPoint> res = [];
+        for ( int i = 0; i < k; i++ )
+        {
+            HeapPoint min = heap.Dequeue( );
+            res.Add( min );
+        }
+
+        return res;
+    }
+
+    private static double CalcDistance( HeapPoint point )
+    {
+        return point.X * point.X + point.Y * point.Y;
+    }
+}
---
+++ algorithms/Tasks/Heaps/KClosest/HeapKClosestTests.cs
@@ -0,0 +1,59 @@
+﻿namespace Tasks.Heaps.KClosest;
+
+
+public class HeapKClosestTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Act
+        List<HeapPoint> res = HeapKClosest.Run( [
+            new HeapPoint( 4, 4 ),
+            new HeapPoint( 1, 2 ),
+            new HeapPoint( 2, 3 )
+        ], 2 );
+
+        // Assert
+        Assert.NotNull( res );
+        Assert.Equal( 2, res.Count );
+        Assert.Equal( new HeapPoint( 1, 2 ), res[0] );
+        Assert.Equal( new HeapPoint( 2, 3 ), res[1] );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Act
+        List<HeapPoint> res = HeapKClosest.Run( [
+            new HeapPoint( 4, 4 ),
+            new HeapPoint( 1, 2 ),
+            new HeapPoint( 2, 3 ),
+            new HeapPoint( 1, 1 )
+        ], 2 );
+
+        // Assert
+        Assert.NotNull( res );
+        Assert.Equal( 2, res.Count );
+        Assert.Equal( new HeapPoint(1, 1), res[0] );
+        Assert.Equal( new HeapPoint(1, 2), res[1] );
+    }
+    
+    [Fact]
+    public void Run_Case3_ShouldBeOk( )
+    {
+        // Act
+        List<HeapPoint> res = HeapKClosest.Run( [
+            new HeapPoint( -1, -1 ),
+            new HeapPoint( 2, 2 ),
+            new HeapPoint( 0, 1 ),
+            new HeapPoint( 1, -1 )
+        ], 3 );
+
+        // Assert
+        Assert.NotNull( res );
+        Assert.Equal( 3, res.Count );
+        Assert.Equal( new HeapPoint( 0, 1 ), res[0] );
+        Assert.Equal( new HeapPoint( 1, -1 ), res[1] );
+        Assert.Equal( new HeapPoint( -1, -1 ), res[2] );
+    }
+}
