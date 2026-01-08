2d911f193bfc0fa2de92dbc195441a9c50df2103
a.zolotarev <a.zolotarev@travelline.ru>
2026-01-08 10:09:25 +0300

    BST: validate

---
+++ algorithms/Tasks/BinaryTrees/Validate/BinaryTreeValidate.cs
@@ -0,0 +1,60 @@
+﻿using System.Numerics;
+
+namespace Tasks.BinaryTrees.Validate;
+
+public class BinaryTreeValidate
+{
+    public class TreeNode<T>
+    {
+        public T Value { get; init; }
+        public TreeNode<T>? Left { get; init; }
+        public TreeNode<T>? Right { get; init; }
+    }
+    
+    /*
+     * Determine if a binary tree is a valid BST.
+     * A valid BST satisfies:
+     * - All left descendants are less than node's value
+     * - All right descendants are greater than node's value
+     */
+    public static bool Check<T>( TreeNode<T>? root ) where T: INumber<T>
+    {
+        bool IsValid( TreeNode<T>? node, Option<T> min, Option<T> max )
+        {
+            if ( node == null ) return true;
+
+            if ( min.HasValue && node.Value <= min.Value! )
+            {
+                return false;
+            }
+            
+            if ( max.HasValue && node.Value >= max.Value! )
+            {
+                return false;
+            }
+
+            return IsValid( node.Left, min, new Option<T>( node.Value ) ) 
+                   && IsValid( node.Right, new Option<T>( node.Value ), max );
+        }
+
+        return IsValid( root, new Option<T>( ), new Option<T>( ) );
+    }
+
+    readonly struct Option<T>
+    {
+        public T? Value { get; }
+        public bool HasValue { get; }
+
+        public Option( T val )
+        {
+            Value = val;
+            HasValue = true;
+        }
+
+        public Option( )
+        {
+            HasValue = false;
+            Value = default;
+        }
+    }
+}
---
+++ algorithms/Tasks/BinaryTrees/Validate/BinaryTreeValidateTests.cs
@@ -0,0 +1,226 @@
+﻿using System.Numerics;
+
+namespace Tasks.BinaryTrees.Validate;
+
+public class BinaryTreeValidateTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new BinaryTreeValidate.TreeNode<int>
+        {
+            Value = 1
+        };
+        
+        // Act
+        var res = BinaryTreeValidate.Check( node );
+
+        // Assert
+        Assert.True( res );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new BinaryTreeValidate.TreeNode<int>
+        {
+            Value = 2,
+            Left = new BinaryTreeValidate.TreeNode<int>
+            {
+                Value = 1
+            }
+        };
+        
+        // Act
+        var res = BinaryTreeValidate.Check( node );
+
+        // Assert
+        Assert.True( res );
+    }
+    
+    [Fact]
+    public void Run_Case3_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new BinaryTreeValidate.TreeNode<int>
+        {
+            Value = 2,
+            Left = new BinaryTreeValidate.TreeNode<int>
+            {
+                Value = 1
+            },
+            Right = new BinaryTreeValidate.TreeNode<int>
+            {
+                Value = 3
+            }
+        };
+        
+        // Act
+        var res = BinaryTreeValidate.Check( node );
+
+        // Assert
+        Assert.True( res );
+    }
+    
+    [Fact]
+    public void Run_Case4_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new BinaryTreeValidate.TreeNode<int>
+        {
+            Value = 2,
+            Left = new BinaryTreeValidate.TreeNode<int>
+            {
+                Value = 1,
+                Left = new BinaryTreeValidate.TreeNode<int>
+                {
+                    Value = 5
+                },
+            },
+            Right = new BinaryTreeValidate.TreeNode<int>
+            {
+                Value = 3
+            }
+        };
+        
+        // Act
+        var res = BinaryTreeValidate.Check( node );
+
+        // Assert
+        Assert.False( res );
+    }
+    
+    [Fact]
+    public void Run_Case5_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new BinaryTreeValidate.TreeNode<int>
+        {
+            Value = 2,
+            Left = new BinaryTreeValidate.TreeNode<int>
+            {
+                Value = 1,
+                Left = new BinaryTreeValidate.TreeNode<int>
+                {
+                    Value = 0
+                },
+            },
+            Right = new BinaryTreeValidate.TreeNode<int>
+            {
+                Value = 3,
+                Right = new BinaryTreeValidate.TreeNode<int>
+                {
+                    Value = 2
+                }
+            }
+        };
+        
+        // Act
+        var res = BinaryTreeValidate.Check( node );
+
+        // Assert
+        Assert.False( res );
+    }
+    
+    [Fact]
+    public void Run_Case6_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new BinaryTreeValidate.TreeNode<int>
+        {
+            Value = 2,
+            Left = new BinaryTreeValidate.TreeNode<int>
+            {
+                Value = 1,
+                Left = new BinaryTreeValidate.TreeNode<int>
+                {
+                    Value = 0
+                },
+            },
+            Right = new BinaryTreeValidate.TreeNode<int>
+            {
+                Value = 3
+            }
+        };
+        
+        // Act
+        var res = BinaryTreeValidate.Check( node );
+
+        // Assert
+        Assert.True( res );
+    }
+    
+    [Fact]
+    public void Run_Case7_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new BinaryTreeValidate.TreeNode<int>
+        {
+            Value = 2,
+            Left = new BinaryTreeValidate.TreeNode<int>
+            {
+                Value = 1,
+                Left = new BinaryTreeValidate.TreeNode<int>
+                {
+                    Value = 0
+                },
+                Right = new BinaryTreeValidate.TreeNode<int>
+                {
+                    Value = 15
+                },
+            },
+            Right = new BinaryTreeValidate.TreeNode<int>
+            {
+                Value = 3,
+                Right = new BinaryTreeValidate.TreeNode<int>
+                {
+                    Value = 4
+                }
+            }
+        };
+        
+        // Act
+        var res = BinaryTreeValidate.Check( node );
+
+        // Assert
+        Assert.False( res );
+    }
+    
+    [Fact]
+    public void Run_Case8_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new BinaryTreeValidate.TreeNode<int>
+        {
+            Value = 6,
+            Left = new BinaryTreeValidate.TreeNode<int>
+            {
+                Value = 4,
+                Left = new BinaryTreeValidate.TreeNode<int>
+                {
+                    Value = 2,
+                    Left = new BinaryTreeValidate.TreeNode<int>
+                    {
+                        Value = 1
+                    },
+                },
+                Right = new BinaryTreeValidate.TreeNode<int>
+                {
+                    Value = 5
+                },
+            },
+            Right = new BinaryTreeValidate.TreeNode<int>
+            {
+                Value = 7
+            }
+        };
+        
+        // Act
+        var res = BinaryTreeValidate.Check( node );
+
+        // Assert
+        Assert.True( res );
+    }
+}
