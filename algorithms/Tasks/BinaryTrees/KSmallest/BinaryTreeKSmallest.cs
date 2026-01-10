157ca1d2f8e779eff88d07dc32cacc02b2467517
a.zolotarev <a.zolotarev@travelline.ru>
2026-01-10 09:54:21 +0300

    BST: k smallest

---
+++ algorithms/Tasks/BinaryTrees/KSmallest/BinaryTreeKSmallest.cs
@@ -0,0 +1,42 @@
+﻿namespace Tasks.BinaryTrees.KSmallest;
+
+public class BinaryTreeKSmallest
+{
+    public class TreeNode<T>
+    {
+        public T Value { get; init; }
+        public TreeNode<T>? Left { get; init; }
+        public TreeNode<T>? Right { get; init; }
+    }
+    
+    /*
+     * Given the root of BST, return the K-smallest element in the tree (1-indexed).
+     */
+    public static int Get( TreeNode<int>? root, int k = 1 )
+    {
+        if ( k <= 0 ) return 0;
+        
+        int kStart = 0;
+        int res = 0;
+
+        void Run( TreeNode<int>? node )
+        {
+            if ( node == null ) return;
+
+            Run( node.Left );
+
+            kStart++;
+            if ( kStart == k )
+            {
+                res = node.Value;
+                return;
+            }
+
+            Run( node.Right );
+        }
+
+        Run( root );
+
+        return res;
+    }
+}
---
+++ algorithms/Tasks/BinaryTrees/KSmallest/BinaryTreeSmallestDifferenceTests.cs
@@ -0,0 +1,192 @@
+﻿namespace Tasks.BinaryTrees.KSmallest;
+
+public class BinaryTreeKSmallestTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new BinaryTreeKSmallest.TreeNode<int>
+        {
+            Value = 6
+        };
+        
+        // Act
+        var res = BinaryTreeKSmallest.Get( node, 1 );
+
+        // Assert
+        Assert.Equal( 6, res );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new BinaryTreeKSmallest.TreeNode<int>
+        {
+            Value = 6,
+            Left = new BinaryTreeKSmallest.TreeNode<int>
+            {
+                Value = 5
+            }
+        };
+        
+        // Act
+        var res = BinaryTreeKSmallest.Get( node, 1 );
+
+        // Assert
+        Assert.Equal( 5, res );
+    }
+    
+    [Fact]
+    public void Run_Case3_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new BinaryTreeKSmallest.TreeNode<int>
+        {
+            Value = 6,
+            Left = new BinaryTreeKSmallest.TreeNode<int>
+            {
+                Value = 5
+            },
+            Right = new BinaryTreeKSmallest.TreeNode<int>
+            {
+                Value = 7
+            }
+        };
+        
+        // Act
+        var res = BinaryTreeKSmallest.Get( node, 1 );
+
+        // Assert
+        Assert.Equal( 5, res );
+    }
+    
+    [Fact]
+    public void Run_Case4_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new BinaryTreeKSmallest.TreeNode<int>
+        {
+            Value = 6,
+            Left = new BinaryTreeKSmallest.TreeNode<int>
+            {
+                Value = 4,
+                Left = new BinaryTreeKSmallest.TreeNode<int>
+                {
+                    Value = 2
+                },
+                Right = new BinaryTreeKSmallest.TreeNode<int>
+                {
+                    Value = 5
+                }
+            },
+            Right = new BinaryTreeKSmallest.TreeNode<int>
+            {
+                Value = 7
+            }
+        };
+        
+        // Act
+        var res = BinaryTreeKSmallest.Get( node, 2 );
+
+        // Assert
+        Assert.Equal( 4, res );
+    }
+    
+    [Fact]
+    public void Run_Case5_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new BinaryTreeKSmallest.TreeNode<int>
+        {
+            Value = 6,
+            Left = new BinaryTreeKSmallest.TreeNode<int>
+            {
+                Value = 4,
+                Left = new BinaryTreeKSmallest.TreeNode<int>
+                {
+                    Value = 2
+                },
+                Right = new BinaryTreeKSmallest.TreeNode<int>
+                {
+                    Value = 5
+                }
+            },
+            Right = new BinaryTreeKSmallest.TreeNode<int>
+            {
+                Value = 7
+            }
+        };
+        
+        // Act
+        var res = BinaryTreeKSmallest.Get( node, 3 );
+
+        // Assert
+        Assert.Equal( 5, res );
+    }
+    
+    [Fact]
+    public void Run_Case6_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new BinaryTreeKSmallest.TreeNode<int>
+        {
+            Value = 6,
+            Left = new BinaryTreeKSmallest.TreeNode<int>
+            {
+                Value = 4,
+                Left = new BinaryTreeKSmallest.TreeNode<int>
+                {
+                    Value = 2
+                },
+                Right = new BinaryTreeKSmallest.TreeNode<int>
+                {
+                    Value = 5
+                }
+            },
+            Right = new BinaryTreeKSmallest.TreeNode<int>
+            {
+                Value = 7
+            }
+        };
+        
+        // Act
+        var res = BinaryTreeKSmallest.Get( node, 1 );
+
+        // Assert
+        Assert.Equal( 2, res );
+    }
+    
+    [Fact]
+    public void Run_Case7_ShouldBeOk( )
+    {
+        // Arrange
+        var node = new BinaryTreeKSmallest.TreeNode<int>
+        {
+            Value = 6,
+            Left = new BinaryTreeKSmallest.TreeNode<int>
+            {
+                Value = 4,
+                Left = new BinaryTreeKSmallest.TreeNode<int>
+                {
+                    Value = 2
+                },
+                Right = new BinaryTreeKSmallest.TreeNode<int>
+                {
+                    Value = 5
+                }
+            },
+            Right = new BinaryTreeKSmallest.TreeNode<int>
+            {
+                Value = 7
+            }
+        };
+        
+        // Act
+        var res = BinaryTreeKSmallest.Get( node, 4 );
+
+        // Assert
+        Assert.Equal( 6, res );
+    }
+}
