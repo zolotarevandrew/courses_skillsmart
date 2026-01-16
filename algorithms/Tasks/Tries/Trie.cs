a0bc2f0d90af66c0b0ed8d46a5169d4e37d2a375
a.zolotarev <a.zolotarev@travelline.ru>
2026-01-16 09:16:29 +0300

    Trie: with stack and tests

---
+++ algorithms/Tasks/Tries/Trie.cs
@@ -0,0 +1,167 @@
+﻿namespace Tasks.Tries;
+
+public class TrieNode<T>( char val, T? metadata )
+{
+    public char Value { get; set; } = val;
+    public Dictionary<char, TrieNode<T>> Children { get; init; } = new ( );
+    public T? Metadata { get; init; } = metadata;
+    public bool IsEnd { get; set; }
+    
+    public bool HasNoChildren => Children.Count == 0;
+}
+
+public class Trie<T>
+{
+    private TrieNode<T> _root;
+
+    public Trie( TrieNode<T> root )
+    {
+        _root = root;
+    }
+
+    public bool Exists( string word )
+    {
+        TrieNode<T> node = _root;
+        foreach ( char c in word )
+        {
+            if ( !node.Children.TryGetValue( c, out TrieNode<T>? child ) )
+            {
+                return false;
+            }
+
+            node = child;
+        }
+
+        return node.IsEnd;
+    }
+    
+    public void Insert( string word )
+    {
+        TrieNode<T> node = _root;
+        foreach ( char c in word )
+        {
+            if ( !node.Children.TryGetValue( c, out TrieNode<T>? child ) )
+            {
+                node.Children[c] = child = new TrieNode<T>( c, default(T) );
+            }
+
+            node = child;
+        }
+
+        node.IsEnd = true;
+    }
+    
+    public bool SearchPrefix( string word )
+    {
+        TrieNode<T> node = _root;
+        foreach ( char c in word )
+        {
+            if ( !node.Children.TryGetValue( c, out TrieNode<T>? child ) )
+            {
+                return false;
+            }
+
+            node = child;
+        }
+
+        return true;
+    }
+    
+    public bool Delete( string word )
+    {
+        //var res = DeleteInternal( word, _root, 0 );
+        //return res.WasDeleted;
+        return DeleteStack( word );
+    }
+
+    private bool DeleteStack( string word )
+    {
+        if ( string.IsNullOrEmpty( word ) )
+        {
+            return false;
+        }
+        
+        TrieNode<T> node = _root;
+        Stack<TrieNode<T>> stack = new();
+        stack.Push( _root );
+        foreach ( char c in word )
+        {
+            if ( !node.Children.TryGetValue( c, out TrieNode<T>? child ) )
+            {
+                return false;
+            }
+
+            node = child;
+            stack.Push( node );
+        }
+
+        if ( !node.IsEnd )
+        {
+            stack.Clear( );
+            return false;
+        }
+
+        node.IsEnd = false;
+        if ( !node.HasNoChildren )
+        {
+            stack.Clear( );
+            return true;
+        }
+
+        // удаляем текущую Node - мы ее итак проверили
+        stack.Pop( );
+        int idx = word.Length;
+        while ( stack.Count > 0 )
+        {
+            TrieNode<T> curNode = stack.Pop( );
+            char ch = word[--idx];
+            curNode.Children.Remove( ch );
+
+            bool canDelete = !curNode.IsEnd && curNode.HasNoChildren;
+            if ( !canDelete )
+            {
+                break;
+            }
+        }
+
+        stack.Clear(  );
+        return true;
+    }
+
+    private record struct DeleteResult( bool CanDelete, bool WasDeleted );
+
+    private DeleteResult DeleteInternal( string word, TrieNode<T>? node, int idx )
+    {
+        if ( node is null ) return new DeleteResult( false, false );
+
+        if ( idx == word.Length )
+        {
+            if ( !node.IsEnd )
+            {
+                return new DeleteResult( false, false );
+            }
+
+            node.IsEnd = false;
+            return new DeleteResult( node.HasNoChildren, true );
+        }
+
+        char ch = word[idx];
+        if ( !node.Children.TryGetValue( ch, out TrieNode<T>? child ) )
+        {
+            return new DeleteResult( false, false );
+        }
+        
+        DeleteResult deleteResult = DeleteInternal( word, child, idx + 1 );
+        if ( !deleteResult.WasDeleted )
+        {
+            return new DeleteResult( false, false );
+        }
+
+        if ( deleteResult.CanDelete )
+        {
+            node.Children.Remove( ch );
+        }
+        
+        return new DeleteResult( !node.IsEnd && node.HasNoChildren, true );
+    }
+}
---
+++ algorithms/Tasks/Tries/TrieTests.cs
@@ -0,0 +1,402 @@
+﻿namespace Tasks.Tries;
+
+public class TrieTests
+{
+    [Fact]
+    public void Search_NotExists_ShouldBeOk( )
+    {
+        // Arrange
+        Trie<int> trie = new Trie<int>( new TrieNode<int>( '-', 0 )
+        {
+            Children = new Dictionary<char, TrieNode<int>>
+            {
+                {'a', new TrieNode<int>( 'a', 1 )},
+                {'b', new TrieNode<int>( 'b', 1 )}
+            }
+        } );
+
+        // Act
+        var res = trie.Exists( "word" );
+
+        // Assert
+        Assert.False( res );
+    }
+    
+    [Fact]
+    public void Search_NotEnd_ShouldBeOk( )
+    {
+        // Arrange
+        Trie<int> trie = new Trie<int>( new TrieNode<int>( '-', 0 )
+        {
+            Children = new Dictionary<char, TrieNode<int>>
+            {
+                {
+                    'w', 
+                    new TrieNode<int>( 'w', 1 )
+                    {
+                        Children = new Dictionary<char, TrieNode<int>>()
+                        {
+                            {
+                                'o', 
+                                new TrieNode<int>( 'o', 1 )
+                                {
+                                    Children = new Dictionary<char, TrieNode<int>>
+                                    {
+                                        {
+                                            'r', 
+                                            new TrieNode<int>( 'r', 1 )
+                                            {
+                                                Children = new Dictionary<char, TrieNode<int>>
+                                                {
+                                                    {
+                                                        'd', 
+                                                        new TrieNode<int>( 'd', 1 )
+                                                        {
+                                                            Children = new Dictionary<char, TrieNode<int>>
+                                                            {
+                                                                {
+                                                                    'e', new TrieNode<int>( 'e', 1 )
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            },
+                        }
+                    }
+                    
+                },
+                {'b', new TrieNode<int>( 'b', 1 )}
+            }
+        } );
+
+        // Act
+        var res = trie.Exists( "word" );
+
+        // Assert
+        Assert.False( res );
+    }
+    
+    [Fact]
+    public void Search_HasEnd_ShouldBeOk( )
+    {
+        // Arrange
+        Trie<int> trie = new Trie<int>( new TrieNode<int>( '-', 0 )
+        {
+            Children = new Dictionary<char, TrieNode<int>>
+            {
+                {
+                    'w', 
+                    new TrieNode<int>( 'w', 1 )
+                    {
+                        Children = new Dictionary<char, TrieNode<int>>()
+                        {
+                            {
+                                'o', 
+                                new TrieNode<int>( 'o', 1 )
+                                {
+                                    Children = new Dictionary<char, TrieNode<int>>
+                                    {
+                                        {
+                                            'r', 
+                                            new TrieNode<int>( 'r', 1 )
+                                            {
+                                                Children = new Dictionary<char, TrieNode<int>>
+                                                {
+                                                    {
+                                                        'd', 
+                                                        new TrieNode<int>( 'd', 1 )
+                                                        {
+                                                            IsEnd = true
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            },
+                        }
+                    }
+                    
+                },
+                {'b', new TrieNode<int>( 'b', 1 )}
+            }
+        } );
+
+        // Act
+        var res = trie.Exists( "word" );
+
+        // Assert
+        Assert.True( res );
+    }
+    
+    [Fact]
+    public void Delete_HasWord_ShouldBeOk( )
+    {
+        // Arrange
+        Trie<int> trie = new Trie<int>( new TrieNode<int>( '-', 0 )
+        {
+            Children = new Dictionary<char, TrieNode<int>>
+            {
+                {
+                    'w', 
+                    new TrieNode<int>( 'w', 1 )
+                    {
+                        Children = new Dictionary<char, TrieNode<int>>()
+                        {
+                            {
+                                'o', 
+                                new TrieNode<int>( 'o', 1 )
+                                {
+                                    Children = new Dictionary<char, TrieNode<int>>
+                                    {
+                                        {
+                                            'r', 
+                                            new TrieNode<int>( 'r', 1 )
+                                            {
+                                                Children = new Dictionary<char, TrieNode<int>>
+                                                {
+                                                    {
+                                                        'd', 
+                                                        new TrieNode<int>( 'd', 1 )
+                                                        {
+                                                            IsEnd = true
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            },
+                        }
+                    }
+                    
+                },
+                {'b', new TrieNode<int>( 'b', 1 )}
+            }
+        } );
+
+        // Act
+        var res = trie.Delete( "word" );
+
+        // Assert
+        Assert.True( res );
+        Assert.False( trie.Exists( "word" ) );
+    }
+    
+    [Fact]
+    public void Delete_HasSingleCharWord_ShouldBeOk( )
+    {
+        // Arrange
+        Trie<int> trie = new Trie<int>( new TrieNode<int>( '-', 0 )
+        {
+            Children = new Dictionary<char, TrieNode<int>>
+            {
+                {'d', new TrieNode<int>( 'd', 1 )
+                {
+                    IsEnd = true
+                } },
+                {'b', new TrieNode<int>( 'b', 1 )
+                {
+                    IsEnd = true
+                }}
+            }
+        } );
+
+        // Act
+        var res = trie.Delete( "d" );
+
+        // Assert
+        Assert.True( res );
+        Assert.False( trie.Exists( "d" ) );
+    }
+    
+    [Fact]
+    public void Delete_HasTwoEndWords_ShouldBeTrue( )
+    {
+        // Arrange
+        Trie<int> trie = new Trie<int>( new TrieNode<int>( '-', 0 )
+        {
+            Children = new Dictionary<char, TrieNode<int>>
+            {
+                {
+                    'w', 
+                    new TrieNode<int>( 'w', 1 )
+                    {
+                        Children = new Dictionary<char, TrieNode<int>>()
+                        {
+                            {
+                                'o', 
+                                new TrieNode<int>( 'o', 1 )
+                                {
+                                    Children = new Dictionary<char, TrieNode<int>>
+                                    {
+                                        {
+                                            'r', 
+                                            new TrieNode<int>( 'r', 1 )
+                                            {
+                                                Children = new Dictionary<char, TrieNode<int>>
+                                                {
+                                                    {
+                                                        'd', 
+                                                        new TrieNode<int>( 'd', 1 )
+                                                        {
+                                                            IsEnd = true,
+                                                            Children = new Dictionary<char, TrieNode<int>>
+                                                            {
+                                                                {
+                                                                    'c',
+                                                                    new TrieNode<int>( 'c', 1 )
+                                                                    {
+                                                                        IsEnd = true
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            },
+                        }
+                    }
+                    
+                },
+                {'b', new TrieNode<int>( 'b', 1 )}
+            }
+        } );
+
+        // Act
+        var res = trie.Delete( "word" );
+
+        // Assert
+        Assert.True( res );
+        Assert.False( trie.Exists( "word" ) );
+    }
+    
+    [Fact]
+    public void Delete_HasTwoEndWords_ShouldNotBeDeleted( )
+    {
+        // Arrange
+        Trie<int> trie = new Trie<int>( new TrieNode<int>( '-', 0 )
+        {
+            Children = new Dictionary<char, TrieNode<int>>
+            {
+                {
+                    'w', 
+                    new TrieNode<int>( 'w', 1 )
+                    {
+                        Children = new Dictionary<char, TrieNode<int>>()
+                        {
+                            {
+                                'o', 
+                                new TrieNode<int>( 'o', 1 )
+                                {
+                                    Children = new Dictionary<char, TrieNode<int>>
+                                    {
+                                        {
+                                            'r', 
+                                            new TrieNode<int>( 'r', 1 )
+                                            {
+                                                Children = new Dictionary<char, TrieNode<int>>
+                                                {
+                                                    {
+                                                        'd', 
+                                                        new TrieNode<int>( 'd', 1 )
+                                                        {
+                                                            IsEnd = false,
+                                                            Children = new Dictionary<char, TrieNode<int>>
+                                                            {
+                                                                {
+                                                                    'c',
+                                                                    new TrieNode<int>( 'c', 1 )
+                                                                    {
+                                                                        IsEnd = true
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            },
+                        }
+                    }
+                    
+                },
+                {'b', new TrieNode<int>( 'b', 1 )}
+            }
+        } );
+
+        // Act
+        var res = trie.Delete( "word" );
+
+        // Assert
+        Assert.False( res );
+        Assert.False( trie.Exists( "word" ) );
+    }
+    
+    [Fact]
+    public void Delete_HasTwoEndSimilarLengthWords_ShouldBeTrue( )
+    {
+        // Arrange
+        Trie<int> trie = new Trie<int>( new TrieNode<int>( '-', 0 )
+        {
+            Children = new Dictionary<char, TrieNode<int>>
+            {
+                {
+                    'c', 
+                    new TrieNode<int>( 'c', 1 )
+                    {
+                        Children = new Dictionary<char, TrieNode<int>>()
+                        {
+                            {
+                                'a', 
+                                new TrieNode<int>( 'a', 1 )
+                                {
+                                    Children = new Dictionary<char, TrieNode<int>>
+                                    {
+                                        {
+                                            'r', 
+                                            new TrieNode<int>( 'r', 1 )
+                                            {
+                                                IsEnd = true
+                                            }
+                                        },
+                                        {
+                                            'm', 
+                                            new TrieNode<int>( 'm', 1 )
+                                            {
+                                                IsEnd = true
+                                            }
+                                        }
+                                    }
+                                }
+                            },
+                        }
+                    }
+                    
+                },
+                {'b', new TrieNode<int>( 'b', 1 )}
+            }
+        } );
+
+        // Act
+        var res = trie.Delete( "car" );
+
+        // Assert
+        Assert.True( res );
+        Assert.False( trie.Exists( "car" ) );
+    }
+}
