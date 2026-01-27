1c7976080024a0946e8c1fa28a5bf6e55c0d2826
a.zolotarev <a.zolotarev@travelline.ru>
2026-01-27 09:24:41 +0300

    attempt_v2: part1 doubly_linked_list additional tasks done

--- algorithms/attempt_v2/part1/doubly_linked_list/task2_2.py
+++ algorithms/attempt_v2/part1/doubly_linked_list/task2_2.py
@@ -68,3 +68,246 @@ def hasCycle(src: LinkedList2):
         cur = cur.prev
 
     return False
+
+# порядковый номер самого задания на курсе - 2
+#
+# номер задачи из задания - 2.12
+#
+# краткое название - Добавьте метод, сортирующий список.
+#
+# сложность решения (O-большое)
+# временнАя - O(N Log N)
+# Каждый merge - O(N) * log N операций (делим пополам)
+
+# пространственная - O(N log n) - 
+# O(Log N) - памяти под стэк
+# O(N) - под каждый merge и split, создаем новые ноды. Сделал так, ради удобства реализации (в том числе для следующей задачи)
+# итого O (Log n) под стэк + O(N log n) под создание новых нод - примерно O(N log n)
+
+# рефлексия по эталонному варианту решения:
+# В целом merge sort - самый подходящий вариант для сортировки LinkedList, его легко разделить пополам и смерджить, просто двигаясь по указателям без индексного доступа.
+# Можно улучшить решение, отказавшись от копирования Нод, тогда по памяти будет O(1) + O(log n) на стэк = O(log n). 
+def sort(src: LinkedList2):
+    if src.head is None or src.head.next is None: 
+        return src
+    left, right = split(src)
+
+    sortedLeft = sort(left)
+    sortedRight = sort(right)
+
+    return merge(sortedLeft, sortedRight)
+
+def split(src: LinkedList2):
+    def splitToNodes(lst: LinkedList2):
+        cur: Node = lst.head
+        n = lst.len()
+        if n == 0: return (None, None)
+        if n == 1: return (lst.head, None)
+
+        mid = n // 2
+        start = 0
+        while start != mid and cur is not None:
+            cur = cur.next
+            start = start + 1
+
+        cur.prev.next = None
+        cur.prev = None
+
+        return (lst.head, cur)
+    
+    def addRemaining(newList: LinkedList2, cur: Node):
+        while cur is not None:
+            newList.add_in_tail(Node(cur.value))
+            cur = cur.next
+    
+    left, right = splitToNodes(src)
+    leftList = LinkedList2()
+    addRemaining(leftList, left)
+    rightList = LinkedList2()
+    addRemaining(rightList, right)
+    return (leftList, rightList)
+
+
+def merge(list1: LinkedList2, list2: LinkedList2):
+    res = LinkedList2()
+    def addRemaining(remaining: Node):
+        while remaining is not None:
+            res.add_in_tail(Node(remaining.value))
+            remaining = remaining.next
+
+    left = list1.head
+    right = list2.head
+    while left is not None and right is not None:
+        if left.value < right.value:
+            res.add_in_tail(Node(left.value))
+            left = left.next
+        else:
+            res.add_in_tail(Node(right.value))
+            right = right.next
+
+    if left is not None: addRemaining(left)
+    if right is not None: addRemaining(right)
+
+    return res
+
+# порядковый номер самого задания на курсе - 2
+#
+# номер задачи из задания - 2.13
+#
+# краткое название - Добавьте метод, объединяющий два списка в третий.
+#
+# сложность решения (O-большое)
+# временнАя - O(N Log N) + O(M Log M)
+# на сортировки - O(N Log N) + O(M Log M), где N - list1, M - list2
+# на merge - O(N + M)
+
+# пространственная - O(N log n) +  O(M Log M) 
+# так как внутри каждого sort выделяются новые элементы
+
+# рефлексия по эталонному варианту решения:
+# Здесь более идеального алгоритма придумать, кажется, нельзя.
+# Единственное, можно также отказаться от копирования Нод и уменьшить количество выделяемой памяти.
+
+def mergeLists(list1: LinkedList2, list2: LinkedList2):
+    sortedList1 = sort(list1)
+    sortedList2 = sort(list2)
+    return merge(sortedList1, sortedList2)
+
+# порядковый номер самого задания на курсе - 2
+#
+# номер задачи из задания - 2.14
+#
+# краткое название - фиктивный/пустой (dummy) узел.
+#
+# сложность решения (O-большое)
+# временнАя - такая же как в LinkedList2
+# пространственная - такая же как в LinkedList2
+
+# рефлексия по эталонному варианту решения:
+# DummyNodes сильно упрощают реализацию, 
+
+class Node3:
+    def __init__(self, v):
+        self.value = v
+        self._prev = None
+        self._next = None
+
+    @property
+    def prev(self):
+        node = self._prev
+        if isinstance(node, DummyNode): return None
+        return node
+    
+    @property
+    def next(self):
+        node = self._next
+        if isinstance(node, DummyNode): return None
+        return node
+
+class DummyNode(Node3):
+    def __init__(self, v):
+        super().__init__(v)
+
+class LinkedList3:  
+    def __init__(self):
+        self.clean()
+
+    def add_in_tail(self, item: Node3):
+        # dh -> [] -> dt
+        # dh -> 1 -> dt
+        # dh -> 1 -> 2 -> dt
+        oldPrev = self.dummyTail._prev
+        self.dummyTail._prev = item
+        item._prev = oldPrev
+        item._next = self.dummyTail
+        oldPrev._next = item
+        
+        self.count = self.count + 1
+
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+
+    def find_all(self, val):
+        res = []
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                res.append(node)
+            node = node.next
+        return res
+
+    def delete(self, val, all=False):
+        def _deleteNode(selectedNode: Node):
+            # [1]
+            # [1] -> 2
+            # [1] -> 2 -> 3
+            if selectedNode == self.head:
+                self.head = self.head.next
+                if self.head is None:
+                    self.tail = None
+                else:
+                    self.head.prev = None
+                return
+            # 1 -> [2]
+            # 1 -> 2 -> [3]
+            if selectedNode == self.tail:
+                self.tail = self.tail.prev
+                self.tail.next = None
+                return
+            
+            # 1 -> [2] -> 3
+            selectedNode.prev.next = selectedNode.next
+            selectedNode.next.prev = selectedNode.prev
+            selectedNode.next = None
+
+        node = self.head
+        while node is not None:
+            next = node.next
+            if node.value == val:
+                _deleteNode(node)
+                self.count = self.count - 1
+                if all == False: 
+                    break
+            node = next
+
+    @property
+    def head(self):
+        return self.dummyHead.next
+    
+    @property
+    def tail(self):
+        return self.dummyTail.prev
+    
+    def clean(self):
+        self.dummyHead = DummyNode()
+        self.dummyTail = DummyNode()
+        self.dummyHead.next = self.dummyTail
+        self.dummyTail.prev = self.dummyHead
+        self.count = 0
+
+    def len(self):
+        return self.count
+
+    def insert(self, afterNode: Node3, newNode: Node3):
+        curNode = None
+        if afterNode is None and self.len() == 0:
+            curNode = self.dummyHead
+        elif afterNode is None and self.len() > 0:
+            curNode = self.dummyHead if self.tail is None else self.tail
+        else:
+            curNode = self.dummyHead
+       
+        oldNext = curNode.next
+        curNode.next = newNode
+        newNode.prev = curNode
+        newNode.next = oldNext
+        oldNext.prev = newNode
+        self.count = self.count + 1
+
+    def add_in_head(self, newNode: Node3):
+        self.insert(self.dummyHead, newNode)
--- algorithms/attempt_v2/part1/doubly_linked_list/task2_3.py
+++ algorithms/attempt_v2/part1/doubly_linked_list/task2_3.py
@@ -1,6 +1,6 @@
 import unittest
 from task2 import LinkedList2, Node
-from task2_2 import reverse, hasCycle
+from task2_2 import reverse, hasCycle, split, merge, sort
 
 class Delete_LinkedList2Tests(unittest.TestCase):
     def test_delete_allFalseEmpty_shouldBeEmpty(self):
@@ -1106,6 +1106,484 @@ class HasCycle_LinkedListTests(unittest.TestCase):
 
         # Assert
         self.assertTrue(hasCycle(s_list))
+
+class Split_LinkedListTests(unittest.TestCase):
+    def test_empty_shouldBeEmpty(self):
+        # Arrange
+        s_list = LinkedList2()
+        
+        # Act
+        list1, list2 = split(s_list)
+
+        # Assert
+        self.assertIsNotNone(list1)
+        self.assertIsNotNone(list2)
+        self.assertEqual(0, list1.len())
+        self.assertEqual(0, list2.len())
+
+    def test_oneElement_shouldBeCorrect(self):
+        # Arrange
+        s_list = LinkedList2()
+        s_list.add_in_tail(Node(1))
+        
+        # Act
+        list1, list2 = split(s_list)
+
+        # Assert
+        self.assertIsNotNone(list1)
+        self.assertIsNotNone(list2)
+        self.assertEqual(1, list1.len())
+        self.assertEqual(0, list2.len())
+        self.assertEqual(1, list1.head.value)
+        self.assertIsNone(list1.head.next)
+        self.assertIsNone(list1.head.prev)
+    
+    def test_twoElement_shouldBeCorrect(self):
+        # Arrange
+        s_list = LinkedList2()
+        s_list.add_in_tail(Node(1))
+        s_list.add_in_tail(Node(2))
+        
+        # Act
+        list1, list2 = split(s_list)
+
+        # Assert
+        self.assertIsNotNone(list1)
+        self.assertIsNotNone(list2)
+        self.assertEqual(1, list1.len())
+        self.assertEqual(1, list2.len())
+
+        self.assertEqual(1, list1.head.value)
+        self.assertIsNone(list1.head.next)
+        self.assertIsNone(list1.head.prev)
+
+        self.assertEqual(2, list2.head.value)
+        self.assertIsNone(list2.head.next)
+        self.assertIsNone(list2.head.prev)
+        
+
+    def test_threeElement_shouldBeCorrect(self):
+        # Arrange
+        s_list = LinkedList2()
+        s_list.add_in_tail(Node(1))
+        s_list.add_in_tail(Node(2))
+        s_list.add_in_tail(Node(3))
+        
+        # Act
+        list1, list2 = split(s_list)
+
+        # Assert
+        self.assertIsNotNone(list1)
+        self.assertIsNotNone(list2)
+        self.assertEqual(1, list1.len())
+        self.assertEqual(2, list2.len())
+
+        self.assertEqual(1, list1.head.value)
+        self.assertIsNone(list1.head.next)
+        self.assertIsNone(list1.head.prev)
+        
+        self.assertEqual(2, list2.head.value)
+        self.assertEqual(3, list2.head.next.value)
+        self.assertEqual(2, list2.tail.prev.value)
+        self.assertIsNone(list2.head.next.next)
+        self.assertIsNone(list2.head.prev)
+
+    def test_fourElement_shouldBeCorrect(self):
+        # Arrange
+        s_list = LinkedList2()
+        s_list.add_in_tail(Node(1))
+        s_list.add_in_tail(Node(2))
+        s_list.add_in_tail(Node(3))
+        s_list.add_in_tail(Node(4))
+        
+        # Act
+        list1, list2 = split(s_list)
+
+        # Assert
+        self.assertIsNotNone(list1)
+        self.assertIsNotNone(list2)
+        self.assertEqual(2, list1.len())
+        self.assertEqual(2, list2.len())
+
+        self.assertEqual(1, list1.head.value)
+        self.assertEqual(2, list1.head.next.value)
+        self.assertEqual(1, list1.tail.prev.value)
+        self.assertIsNone(list1.head.next.next)
+        self.assertIsNone(list1.head.prev)
+        
+        self.assertEqual(3, list2.head.value)
+        self.assertEqual(4, list2.head.next.value)
+        self.assertEqual(3, list2.tail.prev.value)
+        self.assertIsNone(list2.head.next.next)
+        self.assertIsNone(list2.head.prev)
+
+    def test_fiveElement_shouldBeCorrect(self):
+        # Arrange
+        s_list = LinkedList2()
+        s_list.add_in_tail(Node(1))
+        s_list.add_in_tail(Node(2))
+        s_list.add_in_tail(Node(3))
+        s_list.add_in_tail(Node(4))
+        s_list.add_in_tail(Node(5))
+        
+        # Act
+        list1, list2 = split(s_list)
+
+        # Assert
+        self.assertIsNotNone(list1)
+        self.assertIsNotNone(list2)
+        self.assertEqual(2, list1.len())
+        self.assertEqual(3, list2.len())
+
+        self.assertEqual(1, list1.head.value)
+        self.assertEqual(2, list1.head.next.value)
+        self.assertEqual(1, list1.tail.prev.value)
+        self.assertIsNone(list1.head.next.next)
+        self.assertIsNone(list1.head.prev)
+        
+        self.assertEqual(3, list2.head.value)
+        self.assertEqual(4, list2.head.next.value)
+        self.assertEqual(5, list2.head.next.next.value)
+        self.assertEqual(4, list2.tail.prev.value)
+        self.assertEqual(3, list2.tail.prev.prev.value)
+        self.assertIsNone(list2.head.next.next.next)
+        self.assertIsNone(list2.head.prev)
+
+class Merge_LinkedListTests(unittest.TestCase):
+    def test_rightListIsEmpty_shouldBeOk(self):
+        # Arrange
+        list1 = LinkedList2()
+        list1.add_in_tail(Node(1))
+        
+        # Act
+        res = merge(list1, LinkedList2())
+
+        # Assert
+        self.assertIsNotNone(res)
+        self.assertEqual(1, res.len())
+        self.assertEqual(1, list1.head.value)
+        self.assertIsNone(list1.head.next)
+        self.assertIsNone(list1.head.prev)
+
+    def test_leftListIsEmpty_shouldBeOk(self):
+        # Arrange
+        list1 = LinkedList2()
+        list1.add_in_tail(Node(1))
+        
+        # Act
+        res = merge(LinkedList2(), list1)
+
+        # Assert
+        self.assertIsNotNone(res)
+        self.assertEqual(1, res.len())
+        self.assertEqual(1, list1.head.value)
+        self.assertIsNone(list1.head.next)
+        self.assertIsNone(list1.head.prev)
+
+    def test_empty_shouldBeOk(self):
+        # Arrange
+        
+        # Act
+        res = merge(LinkedList2(), LinkedList2())
+
+        # Assert
+        self.assertIsNotNone(res)
+        self.assertEqual(0, res.len())
+
+    def test_oneElementInEach_shouldBeOk(self):
+        # Arrange
+        left = LinkedList2()
+        left.add_in_tail(Node(2))
+        right = LinkedList2()
+        right.add_in_tail(Node(1))
+        
+        # Act
+        res = merge(left, right)
+
+        # Assert
+        self.assertEqual(2, res.len())
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertIsNone(res.head.next.next)
+        self.assertIsNone(res.head.prev)
+
+    def test_twoInLeftOneInRight_shouldBeOk(self):
+        # Arrange
+        left = LinkedList2()
+        left.add_in_tail(Node(2))
+        left.add_in_tail(Node(3))
+        right = LinkedList2()
+        right.add_in_tail(Node(1))
+        
+        # Act
+        res = merge(left, right)
+
+        # Assert
+        self.assertEqual(3, res.len())
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(3, res.head.next.next.value)
+        self.assertEqual(2, res.tail.prev.value)
+        self.assertEqual(1, res.tail.prev.prev.value)
+        self.assertIsNone(res.head.next.next.next)
+        self.assertIsNone(res.head.prev)
+
+    def test_oneInLeftTwoInRight_shouldBeOk(self):
+        # Arrange
+        left = LinkedList2()
+        left.add_in_tail(Node(3))
+        right = LinkedList2()
+        right.add_in_tail(Node(1))
+        right.add_in_tail(Node(2))
+        
+        # Act
+        res = merge(left, right)
+
+        # Assert
+        self.assertEqual(3, res.len())
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(3, res.head.next.next.value)
+        self.assertEqual(2, res.tail.prev.value)
+        self.assertEqual(1, res.tail.prev.prev.value)
+        self.assertIsNone(res.head.next.next.next)
+        self.assertIsNone(res.head.prev)
+
+    def test_twoInLeftTwoInRight_shouldBeOk(self):
+        # Arrange
+        left = LinkedList2()
+        left.add_in_tail(Node(3))
+        left.add_in_tail(Node(4))
+        right = LinkedList2()
+        right.add_in_tail(Node(1))
+        right.add_in_tail(Node(2))
+        
+        # Act
+        res = merge(left, right)
+
+        # Assert
+        self.assertEqual(4, res.len())
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(3, res.head.next.next.value)
+        self.assertEqual(4, res.head.next.next.next.value)
+        self.assertEqual(3, res.tail.prev.value)
+        self.assertEqual(2, res.tail.prev.prev.value)
+        self.assertEqual(1, res.tail.prev.prev.prev.value)
+        self.assertIsNone(res.head.next.next.next.next)
+        self.assertIsNone(res.head.prev)
+
+    def test_threeInLeftTwoInRight_shouldBeOk(self):
+        # Arrange
+        left = LinkedList2()
+        left.add_in_tail(Node(3))
+        left.add_in_tail(Node(4))
+        left.add_in_tail(Node(5))
+        right = LinkedList2()
+        right.add_in_tail(Node(1))
+        right.add_in_tail(Node(2))
+        
+        # Act
+        res = merge(left, right)
+
+        # Assert
+        self.assertEqual(5, res.len())
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(3, res.head.next.next.value)
+        self.assertEqual(4, res.head.next.next.next.value)
+        self.assertEqual(5, res.head.next.next.next.next.value)
+        self.assertEqual(4, res.tail.prev.value)
+        self.assertEqual(3, res.tail.prev.prev.value)
+        self.assertEqual(2, res.tail.prev.prev.prev.value)
+        self.assertEqual(1, res.tail.prev.prev.prev.prev.value)
+        self.assertIsNone(res.head.next.next.next.next.next)
+        self.assertIsNone(res.head.prev)
+    
+    def test_twoInLeftThreeInRight_shouldBeOk(self):
+        # Arrange
+        left = LinkedList2()
+        left.add_in_tail(Node(2))
+        left.add_in_tail(Node(4))
+        right = LinkedList2()
+        right.add_in_tail(Node(1))
+        right.add_in_tail(Node(2))
+        right.add_in_tail(Node(3))
+        
+        # Act
+        res = merge(left, right)
+
+        # Assert
+        self.assertEqual(5, res.len())
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(2, res.head.next.next.value)
+        self.assertEqual(3, res.head.next.next.next.value)
+        self.assertEqual(4, res.head.next.next.next.next.value)
+        self.assertEqual(3, res.tail.prev.value)
+        self.assertEqual(2, res.tail.prev.prev.value)
+        self.assertEqual(2, res.tail.prev.prev.prev.value)
+        self.assertEqual(1, res.tail.prev.prev.prev.prev.value)
+        self.assertIsNone(res.head.next.next.next.next.next)
+        self.assertIsNone(res.head.prev)
+
+class Sort_LinkedListTests(unittest.TestCase):
+    def test_sort_empty_shouldBeOk(self):
+        # Arrange
+
+        
+        # Act
+        res = sort(LinkedList2())
+
+        # Assert
+        self.assertIsNotNone(res)
+        self.assertEqual(0, res.len())
+
+    def test_srt_oneNode_shouldBeOk(self):
+        # Arrange
+        list1 = LinkedList2()
+        list1.add_in_tail(Node(1))
+        
+        # Act
+        res = sort(list1)
+
+        # Assert
+        self.assertIsNotNone(res)
+        self.assertEqual(1, res.len())
+        self.assertEqual(1, list1.head.value)
+        self.assertIsNone(list1.head.next)
+        self.assertIsNone(list1.head.prev)
+
+    def test_sort_twoElements_shouldBeOk(self):
+        # Arrange
+        lst = LinkedList2()
+        lst.add_in_tail(Node(2))
+        lst.add_in_tail(Node(1))
+        
+        # Act
+        res = sort(lst)
+
+        # Assert
+        self.assertEqual(2, res.len())
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertIsNone(res.head.next.next)
+        self.assertIsNone(res.head.prev)
+
+    def test_sort_threeElements_shouldBeOk(self):
+        # Arrange
+        lst = LinkedList2()
+        lst.add_in_tail(Node(2))
+        lst.add_in_tail(Node(3))
+        lst.add_in_tail(Node(1))
+        
+        # Act
+        res = sort(lst)
+
+        # Assert
+        self.assertEqual(3, res.len())
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(3, res.head.next.next.value)
+        self.assertEqual(2, res.tail.prev.value)
+        self.assertEqual(1, res.tail.prev.prev.value)
+        self.assertIsNone(res.head.next.next.next)
+        self.assertIsNone(res.head.prev)
+
+    def test_threeElementsTwoEqual_shouldBeOk(self):
+        # Arrange
+        lst = LinkedList2()
+        lst.add_in_tail(Node(3))
+        lst.add_in_tail(Node(2))
+        lst.add_in_tail(Node(3))
+        
+        # Act
+        res = sort(lst)
+
+        # Assert
+        self.assertEqual(3, res.len())
+        self.assertEqual(2, res.head.value)
+        self.assertEqual(3, res.head.next.value)
+        self.assertEqual(3, res.head.next.next.value)
+        self.assertEqual(3, res.tail.prev.value)
+        self.assertEqual(2, res.tail.prev.prev.value)
+        self.assertIsNone(res.head.next.next.next)
+        self.assertIsNone(res.head.prev)
+
+    def test_sort_fourElements_shouldBeOk(self):
+        # Arrange
+        lst = LinkedList2()
+        lst.add_in_tail(Node(3))
+        lst.add_in_tail(Node(4))
+        lst.add_in_tail(Node(1))
+        lst.add_in_tail(Node(2))
+        
+        # Act
+        res = sort(lst)
+
+        # Assert
+        self.assertEqual(4, res.len())
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(3, res.head.next.next.value)
+        self.assertEqual(4, res.head.next.next.next.value)
+        self.assertEqual(3, res.tail.prev.value)
+        self.assertEqual(2, res.tail.prev.prev.value)
+        self.assertEqual(1, res.tail.prev.prev.prev.value)
+        self.assertIsNone(res.head.next.next.next.next)
+        self.assertIsNone(res.head.prev)
+
+    def test_sort_fiveElements_shouldBeOk(self):
+        # Arrange
+        lst = LinkedList2()
+        lst.add_in_tail(Node(3))
+        lst.add_in_tail(Node(4))
+        lst.add_in_tail(Node(5))
+        lst.add_in_tail(Node(1))
+        lst.add_in_tail(Node(2))
+        
+        # Act
+        res = sort(lst)
+
+        # Assert
+        self.assertEqual(5, res.len())
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(3, res.head.next.next.value)
+        self.assertEqual(4, res.head.next.next.next.value)
+        self.assertEqual(5, res.head.next.next.next.next.value)
+        self.assertEqual(4, res.tail.prev.value)
+        self.assertEqual(3, res.tail.prev.prev.value)
+        self.assertEqual(2, res.tail.prev.prev.prev.value)
+        self.assertEqual(1, res.tail.prev.prev.prev.prev.value)
+        self.assertIsNone(res.head.next.next.next.next.next)
+        self.assertIsNone(res.head.prev)
+    
+    def test_fiveElementsWithDuplicates_shouldBeOk(self):
+        # Arrange
+        lst = LinkedList2()
+        lst.add_in_tail(Node(2))
+        lst.add_in_tail(Node(4))
+        lst.add_in_tail(Node(1))
+        lst.add_in_tail(Node(2))
+        lst.add_in_tail(Node(3))
+        
+        # Act
+        res = sort(lst)
+
+        # Assert
+        self.assertEqual(5, res.len())
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(2, res.head.next.next.value)
+        self.assertEqual(3, res.head.next.next.next.value)
+        self.assertEqual(4, res.head.next.next.next.next.value)
+        self.assertEqual(3, res.tail.prev.value)
+        self.assertEqual(2, res.tail.prev.prev.value)
+        self.assertEqual(2, res.tail.prev.prev.prev.value)
+        self.assertEqual(1, res.tail.prev.prev.prev.prev.value)
+        self.assertIsNone(res.head.next.next.next.next.next)
+        self.assertIsNone(res.head.prev)
 
 if __name__ == "__main__":
     unittest.main()
