aca37c7f0da6b5b4479478d3a6c87d03b60eb383
a.zolotarev <a.zolotarev@travelline.ru>
2026-02-14 09:51:33 +0300

    attempt_v2/part1/ordered_list/task7_2.py

--- algorithms/attempt_v2/part1/ordered_list/task7_2.py
+++ algorithms/attempt_v2/part1/ordered_list/task7_2.py
@@ -1,11 +1,66 @@
-# порядковый номер самого задания на курсе - 6
+# порядковый номер самого задания на курсе - 7
 #
-# номер задачи из задания - 7.1
+# номер задачи из задания - 8
 #
-# Почему и как будет различаться мера сложности для addHead/removeHead и addTail/removeTail.
-# Взял обычную реализацию list-а в python и здесь выбор будет зависеть от выбора head/tail.
-# При таком выборе, в любом случае, одна сторона будет иметь сложность O(N).
-# Для addTail - O(1) вставляем в конец массива, даже при расширении амортизационно будет O(1).
-# Для addHead - O(N) вставляем в начало массива, сдвигаем элементы.
-# Для removeTail - O(1) удаляем с конца массива.
-# Для removeHead - O(N) удаляем с начала массива, сдвигаем элементы.
+# краткое название -  Добавьте метод удаления всех дубликатов из упорядоченного списка.
+#
+# сложность решения (O-большое)
+# временнАя - O(N) проходимся 1 раз по всему списку
+# пространственная - O(1) не используем доп.память
+
+# рефлексия по эталонному варианту решения:
+# Первый вариант подумал про добавление set и проверку, если уже был удаляем. Но в таком случае приходится использовать доп. память.
+# Так как список упорядоченный, можем просто проверять соседние пары и удалять лишние элементы последовательно без доп. памяти.
+
+from task7 import OrderedList, Node
+
+def removeDuplicates(list: OrderedList):
+    cur = list.head
+    while cur is not None and cur.next is not None:
+        if list.compare(cur.value, cur.next.value) == 0:
+            list._deleteNode(cur.next)
+            list.count -= 1
+            continue
+        cur = cur.next
+    return list
+
+# порядковый номер самого задания на курсе - 7
+#
+# номер задачи из задания - 9
+#
+# краткое название -  Напишите алгоритм слияния двух упорядоченных списков в один, сохраняя порядок элементов
+#
+# сложность решения (O-большое)
+# временнАя -
+# O(N + M) проходимся 1 раз по обоим спискам
+# и O(1) на вставку в хвост, итого O(N + M)
+# пространственная - O(N + M) создаем новый список, не трогаем старые
+
+# рефлексия по эталонному варианту решения:
+# Здесь, как обычно merge sort, самое простое. Не трогаем старые списки создаем новый и вставляем в хвост за O(1)
+
+def merge(list1: OrderedList, list2: OrderedList):
+    if list1._OrderedList__ascending != list2._OrderedList__ascending:
+        raise ValueError('List asc param should be equal')
+    left = list1.head
+    right = list2.head
+    asc = list1._OrderedList__ascending
+    res = OrderedList(asc)
+    def append(node: Node):
+        cur = node
+        while cur is not None:
+            res._insert(res.tail, Node(cur.value))
+            cur = cur.next
+
+    while left is not None and right is not None:
+        compareRes = list1.compare(left.value, right.value)
+        shouldAddLeft = compareRes <= 0 if asc else compareRes >= 0
+        if shouldAddLeft:
+            res._insert(res.tail, Node(left.value))
+            left = left.next
+        else:
+            res._insert(res.tail, Node(right.value))
+            right = right.next
+    if left is not None: append(left)
+    if right is not None:  append(right)
+    return res
--- algorithms/attempt_v2/part1/ordered_list/task7_3.py
+++ algorithms/attempt_v2/part1/ordered_list/task7_3.py
@@ -1,5 +1,6 @@
 import unittest
 from task7 import OrderedList, OrderedStringList
+from task7_2 import removeDuplicates, merge
 
 class AddAsc_OrderedListTests(unittest.TestCase):
     def test_add_asc_empty_shouldBeCorrect(self):
@@ -836,7 +837,7 @@ class FindDesc_OrderedListTests(unittest.TestCase):
         s_list.add(3)
         s_list.add(4)
         s_list.add(5)
-        s_list.add(6)
+        s_list.add(7)
 
 
         # Assert
@@ -852,11 +853,393 @@ class FindDesc_OrderedListTests(unittest.TestCase):
         self.assertIsNotNone(res)
         self.assertEqual(5, res.value)
 
-        res = s_list.find(6)
-        self.assertIsNotNone(res)
-        self.assertEqual(6, res.value)
-
         res = s_list.find(7)
+        self.assertIsNotNone(res)
+        self.assertEqual(7, res.value)
+
+        res = s_list.find(6)
         self.assertIsNone(res)
+
+        res = s_list.find(2)
+        self.assertIsNone(res)
+
+class RemoveDuplicates_OrderedListTests(unittest.TestCase):
+    def test_removeDuplicates_empty_shouldBeCorrect(self):
+        # Arrange
+        s_list = OrderedList(False)
+
+        # Act
+        res = removeDuplicates(s_list)
+
+        # Assert
+        self.assertIsNone(res.head)
+        self.assertIsNone(res.tail)
+        self.assertEqual(0, s_list.len())
+
+    def test_removeDuplicates_oneElement_shouldBeCorrect(self):
+        # Arrange
+        s_list = OrderedList(True)
+        s_list.add(1)
+
+        # Act
+        res = removeDuplicates(s_list)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(1, s_list.len())
+
+    def test_removeDuplicates_twoElements_shouldBeCorrect(self):
+        # Arrange
+        s_list = OrderedList(True)
+        s_list.add(1)
+        s_list.add(2)
+
+        # Act
+        res = removeDuplicates(s_list)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(2, s_list.len())
+
+    def test_removeDuplicates_threeElements_shouldBeCorrect(self):
+        # Arrange
+        s_list = OrderedList(True)
+        s_list.add(1)
+        s_list.add(2)
+        s_list.add(3)
+
+        # Act
+        res = removeDuplicates(s_list)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(3, res.head.next.next.value)
+        self.assertEqual(3, s_list.len())
+
+    def test_removeDuplicates_twoElementsDuplicates_shouldBeCorrect(self):
+        # Arrange
+        s_list = OrderedList(True)
+        s_list.add(2)
+        s_list.add(2)
+
+        # Act
+        res = removeDuplicates(s_list)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(2, res.head.value)
+        self.assertEqual(1, s_list.len())
+
+    def test_removeDuplicates_threeElementsDuplicates_shouldBeCorrect(self):
+        # Arrange
+        s_list = OrderedList(True)
+        s_list.add(2)
+        s_list.add(1)
+        s_list.add(2)
+
+        # Act
+        res = removeDuplicates(s_list)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(2, s_list.len())
+
+    def test_removeDuplicates_fourElementsDuplicates_shouldBeCorrect(self):
+        # Arrange
+        s_list = OrderedList(True)
+        s_list.add(2)
+        s_list.add(1)
+        s_list.add(2)
+        s_list.add(3)
+
+        # Act
+        res = removeDuplicates(s_list)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(3, res.head.next.next.value)
+        self.assertEqual(3, s_list.len())
+
+    def test_removeDuplicates_fiveElementsDuplicates_shouldBeCorrect(self):
+        # Arrange
+        s_list = OrderedList(True)
+        s_list.add(2)
+        s_list.add(1)
+        s_list.add(2)
+        s_list.add(3)
+        s_list.add(3)
+
+        # Act
+        res = removeDuplicates(s_list)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(3, res.head.next.next.value)
+        self.assertEqual(3, s_list.len())
+
+    def test_removeDuplicates_sixElementsDuplicates_shouldBeCorrect(self):
+        # Arrange
+        s_list = OrderedList(True)
+        s_list.add(2)
+        s_list.add(1)
+        s_list.add(2)
+        s_list.add(3)
+        s_list.add(3)
+        s_list.add(5)
+
+        # Act
+        res = removeDuplicates(s_list)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(3, res.head.next.next.value)
+        self.assertEqual(5, res.head.next.next.next.value)
+        self.assertEqual(4, s_list.len())
+
+    def test_removeDuplicates_threeDuplicates_shouldBeCorrect(self):
+        # Arrange
+        s_list = OrderedList(True)
+        s_list.add(1)
+        s_list.add(1)
+        s_list.add(1)
+        s_list.add(2)
+
+        # Act
+        res = removeDuplicates(s_list)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(2, s_list.len())
+
+class Merge_OrderedListTests(unittest.TestCase):
+    def test_merge_empty_shouldBeCorrect(self):
+        # Arrange
+        list1 = OrderedList(True)
+        list2 = OrderedList(True)
+
+        # Act
+        res = merge(list1, list2)
+
+        # Assert
+        self.assertIsNone(res.head)
+        self.assertIsNone(res.tail)
+        self.assertEqual(0, res.len())
+
+    def test_merge_rightEmpty_shouldBeCorrect(self):
+        # Arrange
+        list1 = OrderedList(True)
+        list1.add(1)
+        list2 = OrderedList(True)
+
+        # Act
+        res = merge(list1, list2)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(1, res.len())
+
+    def test_merge_leftEmpty_shouldBeCorrect(self):
+        # Arrange
+        list1 = OrderedList(True)
+        list2 = OrderedList(True)
+        list2.add(2)
+
+        # Act
+        res = merge(list1, list2)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(2, res.head.value)
+        self.assertEqual(1, res.len())
+
+    def test_merge_oneElementInEachLeft_shouldBeCorrect(self):
+        # Arrange
+        list1 = OrderedList(True)
+        list1.add(1)
+        list2 = OrderedList(True)
+        list2.add(2)
+
+        # Act
+        res = merge(list1, list2)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(2, res.len())
+
+    def test_merge_oneElementInEachRight_shouldBeCorrect(self):
+        # Arrange
+        list1 = OrderedList(True)
+        list1.add(2)
+        list2 = OrderedList(True)
+        list2.add(1)
+
+        # Act
+        res = merge(list1, list2)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(2, res.len())
+
+    def test_merge_twoInLeftOneInRight_shouldBeCorrect(self):
+        # Arrange
+        list1 = OrderedList(True)
+        list1.add(2)
+        list1.add(3)
+        list2 = OrderedList(True)
+        list2.add(1)
+
+        # Act
+        res = merge(list1, list2)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(3, res.head.next.next.value)
+        self.assertEqual(3, res.len())
+
+    def test_merge_twoInRightOneInLeft_shouldBeCorrect(self):
+        # Arrange
+        list1 = OrderedList(True)
+        list1.add(2)
+        list2 = OrderedList(True)
+        list2.add(1)
+        list2.add(2)
+
+        # Act
+        res = merge(list1, list2)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(2, res.head.next.next.value)
+        self.assertEqual(3, res.len())
+
+    def test_merge_twoInRightTwoInLeft_shouldBeCorrect(self):
+        # Arrange
+        list1 = OrderedList(True)
+        list1.add(2)
+        list1.add(3)
+        list2 = OrderedList(True)
+        list2.add(1)
+        list2.add(2)
+
+        # Act
+        res = merge(list1, list2)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(2, res.head.next.next.value)
+        self.assertEqual(3, res.head.next.next.next.value)
+        self.assertEqual(4, res.len())
+
+    def test_merge_threeInRightTwoInLeft_shouldBeCorrect(self):
+        # Arrange
+        list1 = OrderedList(True)
+        list1.add(4)
+        list1.add(5)
+        list2 = OrderedList(True)
+        list2.add(1)
+        list2.add(2)
+        list2.add(3)
+
+        # Act
+        res = merge(list1, list2)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(3, res.head.next.next.value)
+        self.assertEqual(4, res.head.next.next.next.value)
+        self.assertEqual(5, res.head.next.next.next.next.value)
+        self.assertEqual(5, res.len())
+
+    def test_merge_threeInLeftTwoInRight_shouldBeCorrect(self):
+        # Arrange
+        list1 = OrderedList(True)
+        list1.add(4)
+        list1.add(5)
+        list1.add(6)
+        list2 = OrderedList(True)
+        list2.add(1)
+        list2.add(2)
+
+        # Act
+        res = merge(list1, list2)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(1, res.head.value)
+        self.assertEqual(2, res.head.next.value)
+        self.assertEqual(4, res.head.next.next.value)
+        self.assertEqual(5, res.head.next.next.next.value)
+        self.assertEqual(6, res.head.next.next.next.next.value)
+        self.assertEqual(5, res.len())
+
+    def test_merge_desc_threeInLeftTwoInRight_shouldBeCorrect(self):
+        # Arrange
+        list1 = OrderedList(False)
+        list1.add(6)
+        list1.add(5)
+        list1.add(4)
+        list2 = OrderedList(False)
+        list2.add(5)
+        list2.add(1)
+
+        # Act
+        res = merge(list1, list2)
+
+        # Assert
+        self.assertIsNotNone(res.head)
+        self.assertIsNotNone(res.tail)
+        self.assertEqual(6, res.head.value)
+        self.assertEqual(5, res.head.next.value)
+        self.assertEqual(5, res.head.next.next.value)
+        self.assertEqual(4, res.head.next.next.next.value)
+        self.assertEqual(1, res.head.next.next.next.next.value)
+        self.assertEqual(5, res.len())
+
 if __name__ == "__main__":
     unittest.main()
