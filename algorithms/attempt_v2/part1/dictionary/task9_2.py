a7b75321f951bdeffc65159b47ef530c5028b058
a.zolotarev <a.zolotarev@travelline.ru>
2026-02-26 09:06:12 +0300

    attempt_v2/part1/dictionary/task9_2.py

--- algorithms/attempt_v2/part1/dictionary/task9_2.py
+++ algorithms/attempt_v2/part1/dictionary/task9_2.py
@@ -1,13 +1,142 @@
 # порядковый номер самого задания на курсе - 9
 #
-# номер задачи из задания - 3
+# номер задачи из задания - 5
 #
-# краткое название -  Реализуйте динамическую хэш-таблицу, которая автоматически удваивает свой размер, если уровень заполненности превышает заданный порог (например, 75%).
-#Реализация должна корректно перераспределять все элементы в новую, более крупную хэш-таблицу с минимальными затратами по времени.
+# краткое название -  Реализуйте словарь с использованием упорядоченного списка по ключу для оптимизации производительности поиска
 #
 # сложность решения (O-большое)
 # временнАя -
-# пространственная -
+# put - хоть и с бинарным поиском, но из-за insert O(N)
+# delete - O(log n) за счет бинарного поиска
+# get - O(log n) за счет бинарного поиска
+# пространственная - O(N) храним в массиве пары
 
 # рефлексия по эталонному варианту решения:
+# В данной реализации большинство операций выполняются за O(log n) за счет бинарного поиска индекса для вставки в отсортированном порядке.
+# Оптимизаций в таком контексте больше не придумал.
+
+
+
+from typing import Generic, TypeVar
+from dataclasses import dataclass
+
+V = TypeVar("V")
+
+@dataclass(slots=True)
+class Entry(Generic[V]):
+    key: str
+    value: V
+
+class OrderedListDict:
+    def __init__(self):
+        self.list: list[Entry[V]] = []
+
+    def contains_key(self, key: str):
+        idx = self._searchIndex(key)
+        if idx < len(self.list) and self.list[idx].key == key:
+            return True
+        return False
+
+    def put(self, key: str, value: V):
+        entry = Entry(key, value)
+        idx = self._searchIndex(key)
+        # когда попали на уже существующий ключ
+        if idx < len(self.list) and self.list[idx].key == key:
+            self.list[idx] = entry
+            return
+        # вставляем в конец или по нужному индексу
+        self.list.insert(idx, entry)
+
+    def get(self, key: str) -> V | None:
+        idx = self._searchIndex(key)
+        if idx < len(self.list) and self.list[idx].key == key:
+            return self.list[idx].value
+        return None
+
+    def delete(self, key: str):
+        idx = self._searchIndex(key)
+        if idx < len(self.list) and self.list[idx].key == key:
+            self.list.pop(idx)
+            return True
+        return False
+
+    def _compare(self, v1: str, v2: str) -> int :
+        s1 = ("" if v1 is None else str(v1)).strip()
+        s2 = ("" if v2 is None else str(v2)).strip()
+        if s1 < s2: return -1
+        if s1 > s2: return 1
+        return 0
+
+    def _searchIndex(self, key):
+        # cnt 0 - 0
+        # cnt 1 - 1 [2], idx = 1
+        # cnt 4 - 1,2,4,5 [3] idx = 2
+        # cnt 4 - 1,2,4,5 [0] idx = 0
+        # cnt 4 - 1,2,4,5 [6] idx = 4
+        # cnt 4 - 1,2,4,5 [5] idx = 3
+        left = 0
+        right = len(self.list)
+        while left < right:
+            mid = (left + right) // 2
+            midKey = self.list[mid].key
+            compare = self._compare(key, midKey)
+
+            if compare <= 0:
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
+# порядковый номер самого задания на курсе - 9
 #
+# номер задачи из задания - 6
+#
+# краткое название - Создайте словарь, в котором ключи представлены битовыми строками фиксированной длины. Реализуйте методы добавления, удаления и поиска элементов, используя битовые операции для ускорения работы
+#
+# сложность решения (O-большое)
+# временнАя - O(1) на все операции
+# пространственная - O(2^16) ячеек создаем потенциально для индексации в худшем случае
+
+# рефлексия по эталонному варианту решения:
+# Сделал какой-то глупенький page table, с двойной индексацией по первым 8 и последним 8 битам.
+# На практике такое будет наврядли использовано, а также будет занимать очень много места при более 16 бит.
+# В текущей реализации коллизии сведены к минимуму, но жертвуем аллокацией большого количества памяти.
+
+MAX_KEY = (1 << 16) - 1
+
+class BitKeyDict:
+    def __init__(self):
+        self.list = [None] * 256
+
+    def _checkKey(self, key: int):
+        if key < 0 or key > MAX_KEY:
+            raise ValueError('Key should be 16 bit')
+
+    def put(self, key: int, value: V):
+        self._checkKey(key)
+        idx1 = key >> 8
+        idx2 = key & 0b11111111
+
+        if self.list[idx1] is None:
+            self.list[idx1] = [None] * 256
+
+        self.list[idx1][idx2] = value
+
+    def get(self, key: int) -> V | None:
+        self._checkKey(key)
+        idx1 = key >> 8
+        idx2 = key & 0b11111111
+        if self.list[idx1] is None:
+            return None
+        return self.list[idx1][idx2]
+
+    def delete(self, key: int):
+        self._checkKey(key)
+        idx1 = key >> 8
+        idx2 = key & 0b11111111
+        if self.list[idx1] is None:
+            return False
+        if self.list[idx1][idx2] is None:
+            return False
+        self.list[idx1][idx2] = None
+        return True
--- algorithms/attempt_v2/part1/dictionary/task9_3.py
+++ algorithms/attempt_v2/part1/dictionary/task9_3.py
@@ -1,5 +1,6 @@
 import unittest
 from task9 import NativeDictionary
+from task9_2 import OrderedListDict
 
 class DictionaryTableTests(unittest.TestCase):
     def test_hashFun_shouldFoundSlot(self):
@@ -141,17 +142,6 @@ class DictionaryTableTests(unittest.TestCase):
         # Assert
         self.assertEqual('2', dct.get('1'))
 
-    def test_put_twoElements_shouldBeOk(self):
-        # Arrange
-        sz = 3
-        dct = NativeDictionary(sz)
-        dct.put('1', '2')
-        dct.put('2', '3')
-
-        # Assert
-        self.assertEqual('2', dct.get('1'))
-        self.assertEqual('3', dct.get('2'))
-
     def test_put_twoElementsWithRewrite_shouldBeOk(self):
         # Arrange
         sz = 3
@@ -188,7 +178,6 @@ class DictionaryTableTests(unittest.TestCase):
 
     def test_put_sizeOne_shouldBeOk(self):
         # Arrange
-        sz = 3
         dct = NativeDictionary(1)
         dct.put('1', '2')
 
@@ -198,7 +187,6 @@ class DictionaryTableTests(unittest.TestCase):
 
     def test_put_sizeTwo_shouldBeOk(self):
         # Arrange
-        sz = 3
         dct = NativeDictionary(2)
         dct.put('1', '2')
         dct.put('2', '3')
@@ -208,6 +196,196 @@ class DictionaryTableTests(unittest.TestCase):
         self.assertEqual('3', dct.get('2'))
         self.assertIsNone(dct.get('3'))
 
+    def test_step_shouldBeOk(self):
+        # Arrange
+        dct = NativeDictionary(1)
+        step = dct._step('1')
+        self.assertTrue( step >= 0 )
+
+        dct = NativeDictionary(2)
+        step = dct._step('11')
+        self.assertTrue( step >= 0)
+
+        dct = NativeDictionary(3)
+        step = dct._step('1232132132131')
+        self.assertTrue( step >= 0 )
+
+class OrderListDictTableTests(unittest.TestCase):
+    def test_containsKey_empty_shouldBeFalse(self):
+        # Arrange
+        dct = OrderedListDict()
+
+        # Assert
+        self.assertFalse(dct.contains_key('2'))
+
+    def test_contains_key_oneElementNoKey_shouldBeFalse(self):
+        # Arrange
+        dct = OrderedListDict()
+        dct.put('1', '2')
+
+        # Assert
+        self.assertFalse(dct.contains_key('2'))
+
+    def test_contains_key_oneElementHasKey_shouldBeTrue(self):
+        # Arrange
+
+        dct = OrderedListDict()
+        dct.put('1', '2')
+
+        # Assert
+        self.assertTrue(dct.contains_key('1'))
+
+    def test_contains_key_twoElementNoKey_shouldBeFalse(self):
+        # Arrange
+        dct = OrderedListDict()
+        dct.put('1', '2')
+        dct.put('2', '3')
+
+        # Assert
+        self.assertFalse(dct.contains_key('3'))
+
+    def test_contains_key_twoElementHasKeys_shouldBeTrue(self):
+        # Arrange
+        dct = OrderedListDict()
+        dct.put('1', '2')
+        dct.put('2', '3')
+
+        # Assert
+        self.assertTrue(dct.contains_key('1'))
+        self.assertTrue(dct.contains_key('2'))
+        self.assertFalse(dct.contains_key('3'))
+
+    def test_contains_key_threeElementsNoKeys_shouldBeFalse(self):
+        # Arrange
+        dct = OrderedListDict()
+        dct.put('1', '2')
+        dct.put('2', '3')
+        dct.put('3', '4')
+
+        # Assert
+        self.assertFalse(dct.contains_key('4'))
+        self.assertFalse(dct.contains_key('5'))
+        self.assertFalse(dct.contains_key('6'))
+
+    def test_get_empty_shouldBeNone(self):
+        # Arrange
+        dct = OrderedListDict()
+
+        # Assert
+        self.assertIsNone(dct.get('2'))
+
+    def test_get_oneElementNoKey_shouldBeNone(self):
+        # Arrange
+        dct = OrderedListDict()
+        dct.put('1', '2')
+
+        # Assert
+        self.assertIsNone(dct.get('2'))
+
+    def test_get_oneElementHasKey_shouldFound(self):
+        # Arrange
+        dct = OrderedListDict()
+        dct.put('1', '2')
+
+        # Assert
+        self.assertEqual('2', dct.get('1'))
+
+    def test_get_twoElementNoKey_shouldBeNone(self):
+        # Arrange
+        dct = OrderedListDict()
+        dct.put('1', '2')
+        dct.put('2', '3')
+
+        # Assert
+        self.assertIsNone(dct.get('3'))
+
+    def test_get_twoElementHasKeys_shouldBeCorrect(self):
+        # Arrange
+        dct = OrderedListDict()
+        dct.put('1', '2')
+        dct.put('2', '3')
+
+        # Assert
+        self.assertEqual('2',dct.get('1'))
+        self.assertEqual('3', dct.get('2'))
+        self.assertIsNone(dct.get('3'))
+
+    def test_get_threeElementsNoKeys_shouldBeNone(self):
+        # Arrange
+        dct = OrderedListDict()
+        dct.put('1', '2')
+        dct.put('2', '3')
+        dct.put('3', '4')
+
+        # Assert
+        self.assertIsNone(dct.get('4'))
+        self.assertIsNone(dct.get('5'))
+        self.assertIsNone(dct.get('6'))
+
+    def test_put_empty_shouldBeOk(self):
+        # Arrange
+        dct = OrderedListDict()
+        dct.put('1', '2')
+
+        # Assert
+        self.assertEqual('2', dct.get('1'))
+
+    def test_put_twoElementsWithRewrite_shouldBeOk(self):
+        # Arrange
+        dct = OrderedListDict()
+        dct.put('1', '2')
+        dct.put('2', '3')
+
+        # Assert
+        self.assertEqual('2', dct.get('1'))
+        self.assertEqual('3', dct.get('2'))
+
+        dct.put('2', '5')
+        self.assertEqual('5', dct.get('2'))
+
+    def test_put_threeElementsWithRewrite_shouldBeOk(self):
+        # Arrange
+        dct = OrderedListDict()
+        dct.put('1', '2')
+        dct.put('2', '3')
+        dct.put('3', '4')
+
+        # Assert
+        self.assertEqual('2', dct.get('1'))
+        self.assertEqual('3', dct.get('2'))
+        self.assertEqual('4', dct.get('3'))
+
+        dct.put('1', '6')
+        dct.put('2', '7')
+        dct.put('3', '8')
+        self.assertEqual('6', dct.get('1'))
+        self.assertEqual('7', dct.get('2'))
+        self.assertEqual('8', dct.get('3'))
+
+    def test_put_fiveElementsWithRewrite_shouldBeOk(self):
+        # Arrange
+        dct = OrderedListDict()
+        dct.put('1', '2')
+        dct.put('2', '3')
+        dct.put('3', '4')
+        dct.put('44', '41')
+        dct.put('55', '42')
+
+        # Assert
+        self.assertEqual('2', dct.get('1'))
+        self.assertEqual('3', dct.get('2'))
+        self.assertEqual('4', dct.get('3'))
+        self.assertEqual('41', dct.get('44'))
+        self.assertEqual('42', dct.get('55'))
+
+        dct.put('1', '6')
+        dct.put('2', '7')
+        dct.put('3', '8')
+        dct.put('44', '100')
+        self.assertEqual('6', dct.get('1'))
+        self.assertEqual('7', dct.get('2'))
+        self.assertEqual('8', dct.get('3'))
+        self.assertEqual('100', dct.get('44'))
 
 if __name__ == "__main__":
     unittest.main()
