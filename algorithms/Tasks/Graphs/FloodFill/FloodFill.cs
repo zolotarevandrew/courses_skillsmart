73ff99e1a517f7b9c71bb5d6ac311f07b7b63a79
a.zolotarev <a.zolotarev@travelline.ru>
2026-03-10 09:15:40 +0300

    Graph: FloodFill

---
+++ algorithms/Tasks/Graphs/FloodFill/FloodFill.cs
@@ -0,0 +1,44 @@
+﻿namespace Tasks.Graphs.FloodFill;
+
+public static class FloodFill
+{
+    /// <summary>
+    /// Given m x n grid represented an image, perform a flood fill from pixel at (sr, sc)
+    /// and change all connected pixels of the same color to a new color.
+    /// Connected pixels are those sharing a side and a having a same color as the starting pixel.
+    /// Return the modified image
+    /// </summary>
+    public static void Run( int[,] grid, (int sr, int sc) fill, int color )
+    {
+        int startColor = grid[fill.sr, fill.sc];
+        if ( startColor == color ) return;
+        Dfs( fill );
+        
+        void Dfs( (int curRow, int curCol) idx )
+        {
+            if ( !IsPathExists( grid, idx ) ) return;
+            if ( grid[idx.curRow, idx.curCol] != startColor ) return;
+
+            grid[idx.curRow, idx.curCol] = color;
+        
+            var left = ( idx.curRow , idx.curCol - 1 );
+            var right = ( idx.curRow, idx.curCol + 1 );
+            var bottom = ( idx.curRow + 1, idx.curCol );
+            var top = ( idx.curRow - 1, idx.curCol );
+        
+            Dfs( left );
+            Dfs( right );
+            Dfs( top );
+            Dfs( bottom );
+        }
+    }
+
+    static bool IsPathExists( int[,] grid, (int curRow, int curCol) idx )
+    {
+        int rows = grid.GetLength( 0 );
+        int cols = grid.GetLength( 1 );
+
+        return idx.curRow >= 0 && idx.curRow < rows
+                               && idx.curCol >= 0 && idx.curCol < cols;
+    }
+}
---
+++ algorithms/Tasks/Graphs/FloodFill/FloodFillTests.cs
@@ -0,0 +1,53 @@
+﻿namespace Tasks.Graphs.FloodFill;
+
+public class FloodFillTests
+{
+
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        int[,] grid =
+        {
+            { 1, 1, 1 },
+            { 1, 1, 0 },
+            { 1, 0, 1 }
+        };
+        
+        int[,] expected =
+        {
+            { 2, 2, 2 },
+            { 2, 2, 0 },
+            { 2, 0, 1 }
+        };
+
+        // Act
+        FloodFill.Run( grid, ( 1, 1 ), 2 );
+        
+        // Assert
+        Assert.Equal( expected, grid );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        int[,] grid =
+        {
+            { 0, 0, 0 },
+            { 0, 0, 0 },
+        };
+        
+        int[,] expected =
+        {
+            { 0, 0, 0 },
+            { 0, 0, 0 },
+        };
+
+        // Act
+        FloodFill.Run( grid, ( 0, 0 ), 0 );
+        
+        // Assert
+        Assert.Equal( expected, grid );
+    }
+}
