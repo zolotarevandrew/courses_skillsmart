e1319abd084476d394c6a9bbe05d38fc22c6ec99
a.zolotarev <a.zolotarev@travelline.ru>
2026-05-26 09:47:28 +0300

    graphs: Clone

---
+++ algorithms/Tasks/Graphs/Clone/CloneGraph.cs
@@ -0,0 +1,40 @@
+﻿namespace Tasks.Graphs.Clone;
+
+public static class CloneGraph
+{
+    public record Node
+    {
+        public int Value { get; init; }
+        public List<Node> Neighbors { get; init; } = [];
+    }
+    
+    /// <summary>
+    /// Given a reference to a node in a connected undirected graph, create a deep copy a clone of a graph.
+    /// Each node contains value and a list of neighboring nodes
+    /// </summary>
+    public static Node Run( Node node )
+    {
+        Dictionary<Node, Node> cloned = [];
+        return Dfs( node, cloned );
+    }
+    
+    static Node Dfs( Node current, Dictionary<Node, Node> cloned )
+    {
+        if ( cloned.TryGetValue( current, out Node? dfs ) )
+        {
+            return dfs;
+        }
+        
+        Node clonedNode = new Node
+        {
+            Value = current.Value
+        };
+        cloned[current] = clonedNode;
+        foreach ( Node neighbor in current.Neighbors )
+        {
+            clonedNode.Neighbors.Add( Dfs( neighbor, cloned ) );
+        }
+
+        return clonedNode;
+    }
+}
---
+++ algorithms/Tasks/Graphs/Clone/CloneGraphTests.cs
@@ -0,0 +1,58 @@
+﻿namespace Tasks.Graphs.Clone;
+
+public class CloneGraphTests
+{
+
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        var node1 = new CloneGraph.Node
+        {
+            Value = 1
+        };
+        CloneGraph.Node node = new CloneGraph.Node
+        {
+            Value = 0,
+            Neighbors =
+            [
+                node1,
+
+                new CloneGraph.Node
+                {
+                    Value = 4,
+                    Neighbors =
+                    [
+                        new CloneGraph.Node
+                        {
+                            Value = 8,
+                            Neighbors =
+                            [
+                                new CloneGraph.Node
+                                {
+                                    Value = 9,
+                                    Neighbors =
+                                    [
+                                        new CloneGraph.Node
+                                        {
+                                            Value = 5,
+                                            Neighbors =
+                                            [
+                                                node1
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        };
+
+        // Act
+        var res = CloneGraph.Run( node );
+        
+        Assert.NotNull( res );
+    }
+}
