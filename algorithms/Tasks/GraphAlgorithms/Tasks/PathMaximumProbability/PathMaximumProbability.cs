825a8bb1fbe7560434c4316a79f50fbfa3b3c98b
a.zolotarev <a.zolotarev@travelline.ru>
2026-06-03 09:29:48 +0300

    Graphs: max maximum probability

---
+++ algorithms/Tasks/GraphAlgorithms/Tasks/PathMaximumProbability/PathMaximumProbability.cs
@@ -0,0 +1,60 @@
+﻿namespace Tasks.GraphAlgorithms.Tasks.PathMaximumProbability;
+
+public class PathMaximumProbability
+{
+   
+    
+    // Given a weighted undirected graph with n nodes, an edge list edges.
+    // and an array succProb of edge probabilities.
+    // Find the path with maximum probability from start to end.
+    // Return the maximum probability, or 0 if no path exists.
+    public static double Run( 
+        int n, 
+        (int from, int to)[] edges, 
+        double[] probs, 
+        int start, 
+        int end )
+    {
+        Dictionary<int, double> probabilities = [];
+        Dictionary<int, List<(int, double)>> graph = [];
+        for ( int i = 0; i < n; i++ )
+        {
+            probabilities[i] = 0;
+            graph[i] = [];
+        }
+
+        for ( int i = 0; i < edges.Length; i++ )
+        {
+            (int from, int to) edge = edges[i];
+            double prob = probs[i];
+            graph[edge.from].Add( ( edge.to, prob ) );
+            graph[edge.to].Add( ( edge.from, prob ) );
+        }
+
+        probabilities[start] = 1.0;
+
+        var queue = new PriorityQueue<int, double>(
+            Comparer<double>.Create( ( x, y ) => y.CompareTo( x ) )
+        );
+        queue.Enqueue( start, probabilities[start] );
+        while ( queue.TryDequeue( out var curNode, out var curProb ) )
+        {
+            if ( curNode == end )
+            {
+                return curProb;
+            }
+
+            foreach ( (int neighbour, double prob) item in graph[curNode] )
+            {
+                double prob = curProb * item.prob;
+                if ( prob > probabilities[item.neighbour] )
+                {
+                    probabilities[item.neighbour] = prob;
+                    queue.Enqueue( item.neighbour, prob );
+                }
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ algorithms/Tasks/GraphAlgorithms/Tasks/PathMaximumProbability/PathMaximumProbabilityTests.cs
@@ -0,0 +1,59 @@
+﻿namespace Tasks.GraphAlgorithms.Tasks.PathMaximumProbability;
+
+public class PathMaximumProbabilityTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        int n = 3;
+        (int, int)[] edges = [
+            ( 0, 1 ),
+            ( 1, 2 ),
+            ( 0, 2 )
+        ];
+        double[] probs = [0.5, 0.5, 0.2];
+        int src = 0;
+        int dst = 2;
+        
+        // Act
+        
+        Assert.Equal( 0.25, PathMaximumProbability.Run( n, edges, probs, src, dst ) );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        int n = 3;
+        (int, int)[] edges = [
+            ( 0, 1 ),
+            ( 1, 2 ),
+            ( 0, 2 )
+        ];
+        double[] probs = [0.5, 0.5, 0.3];
+        int src = 0;
+        int dst = 2;
+        
+        // Act
+        
+        Assert.Equal( 0.3, PathMaximumProbability.Run( n, edges, probs, src, dst ) );
+    }
+    
+    [Fact]
+    public void Run_Case3_ShouldBeOk( )
+    {
+        // Arrange
+        int n = 3;
+        (int, int)[] edges = [
+            ( 0, 1 )
+        ];
+        double[] probs = [0.5];
+        int src = 0;
+        int dst = 2;
+        
+        // Act
+        
+        Assert.Equal( 0, PathMaximumProbability.Run( n, edges, probs, src, dst ) );
+    }
+}
