14db91545de73df2e38b089def4ab72b820ebaff
a.zolotarev <a.zolotarev@travelline.ru>
2026-06-05 09:29:28 +0300

    Graph: topological sort

---
+++ algorithms/Tasks/GraphAlgorithms/TopologicalSorting/TopologicalSorting.cs
@@ -0,0 +1,45 @@
+﻿namespace Tasks.GraphAlgorithms.TopologicalSorting;
+
+public class TopologicalSorting
+{
+    public class Graph
+    {
+        public Dictionary<Vertex, List<Vertex>> Value { get; set; }
+
+        public List<Vertex> Get( Vertex vertex )
+        {
+            return Value.TryGetValue( vertex, out List<Vertex>? value ) ? value : [ ];
+        }
+    }
+
+    public record Vertex( int Value );
+
+    public static List<Vertex> DfsSort( Graph graph )
+    {
+        Stack<Vertex> stack = [];
+        HashSet<Vertex> visited = [];
+        foreach ( Vertex vertex in graph.Value.Keys )
+        {
+            if ( !visited.Contains( vertex ) )
+            {
+                Dfs( vertex, visited, stack, graph );
+            }
+        }
+        return stack
+            .ToList(  );
+    }
+
+    static void Dfs( Vertex current, HashSet<Vertex> visited, Stack<Vertex> stack, Graph graph )
+    {
+        visited.Add( current );
+
+        foreach ( var neighbour in graph.Get( current ) )
+        {
+            if ( visited.Contains( neighbour ) ) continue;
+            Dfs( neighbour, visited, stack, graph );
+        }
+
+        stack.Push( current );
+    }
+    
+}
---
+++ algorithms/Tasks/GraphAlgorithms/TopologicalSorting/TopologicalSortingTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Tasks.GraphAlgorithms.TopologicalSorting;
+
+public class TopologicalSortingTests
+{
+    [Fact]
+    public void HasCycle_WithCycles_ShouldBeTrue( )
+    {
+        // Arrange
+        var node1 = new TopologicalSorting.Vertex( 1 );
+        var node2 = new TopologicalSorting.Vertex( 2 );
+        var node3 = new TopologicalSorting.Vertex( 3 );
+        var node4 = new TopologicalSorting.Vertex( 4 );
+        var node5 = new TopologicalSorting.Vertex( 5 );
+        var node6 = new TopologicalSorting.Vertex( 6 );
+        var graph = new TopologicalSorting.Graph
+        {
+            Value = new Dictionary<TopologicalSorting.Vertex, List<TopologicalSorting.Vertex>>
+            {
+                { node1, [node2, node4] },
+                { node2, [node3] },
+                { node3, [node5] },
+                { node5, [node6] },
+            }
+        };
+
+        // Assert
+        var res = TopologicalSorting.DfsSort( graph );
+        Assert.Equal( 6, res.Count );
+        var values = res.Select( c => c.Value ).ToList( );
+        Assert.Equal( values, [1, 4, 2, 3, 5, 6] );
+    }
+}
