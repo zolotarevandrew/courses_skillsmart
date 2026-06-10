9fb2a9baddeedc68ab517094aad5732805e915f4
a.zolotarev <a.zolotarev@travelline.ru>
2026-06-10 09:58:59 +0300

    union find

---
+++ algorithms/Tasks/UnionFind/Simple/RankUnionFind.cs
@@ -0,0 +1,40 @@
+﻿namespace Tasks.UnionFind.Simple;
+
+public class RankUnionFind
+{
+    private List<int> _parent;
+    private List<int> _ranks;
+    public RankUnionFind( int n )
+    {
+        _parent = Enumerable
+            .Range( 0, n )
+            .ToList( );
+        _ranks = new List<int>( n );
+    }
+
+    public int Find( int u )
+    {
+        if ( _parent[u] == u ) return _parent[u];
+        _parent[u] = Find( _parent[u] );
+        return _parent[u];
+    }
+
+    public void Union( int u, int v )
+    {
+        var rootU = Find( u );
+        var rootV = Find( v );
+        if ( rootU == rootV ) return;
+
+        if ( _ranks[rootU] > _ranks[rootV] )
+        {
+            _parent[rootV] = _ranks[rootU];
+            return;
+        }
+        
+        _parent[rootU] = _ranks[rootV];
+        if ( _ranks[rootU] == _ranks[rootV] )
+        {
+            _ranks[rootV]++;
+        }
+    }
+}
---
+++ algorithms/Tasks/UnionFind/Simple/SimpleUnionFind.cs
@@ -0,0 +1,32 @@
+﻿using Tasks.Arrays.ProductExceptSelf;
+
+namespace Tasks.UnionFind.Simple;
+
+public class SimpleUnionFind
+{
+    private List<int> _parent;
+    public SimpleUnionFind( int n )
+    {
+        _parent = Enumerable
+            .Range( 0, n )
+            .ToList( );
+    }
+
+    public int Find( int u )
+    {
+        return _parent[u] == u ? u : Find( _parent[u] );
+    }
+
+    public bool Union( int u, int v )
+    {
+        var rootU = Find( u );
+        var rootV = Find( v );
+        if ( rootU != rootV )
+        {
+            _parent[rootU] = rootV;
+            return true;    
+        }
+
+        return false;
+    }
+}
