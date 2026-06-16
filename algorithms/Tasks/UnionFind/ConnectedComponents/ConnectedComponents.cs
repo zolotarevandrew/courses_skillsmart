8c9c01138063f3482957878993f182df8a83b022
a.zolotarev <a.zolotarev@travelline.ru>
2026-06-16 09:28:56 +0300

    UnionFind: ConnectedComponents

---
+++ algorithms/Tasks/UnionFind/ConnectedComponents/ConnectedComponents.cs
@@ -0,0 +1,20 @@
+﻿using Tasks.UnionFind.Simple;
+
+namespace Tasks.UnionFind.ConnectedComponents;
+
+public class ConnectedComponents
+{
+    /*
+     * Given n nodes, from 0 to n-1, and a list of edges. Return the number of connected components in a graph
+     * directly connected to city j. Return the total number of provinces (connected components)
+     */
+    public static int Run( (int from, int to)[] edges, int n )
+    {
+        RankUnionFind find = new RankUnionFind( n );
+        foreach ( (int from, int to) item in edges )
+        {
+            find.Union( item.from, item.to );
+        }
+        return find.SetsCount(  );
+    }
+}
---
+++ algorithms/Tasks/UnionFind/ConnectedComponents/ConnectedComponentsTests.cs
@@ -0,0 +1,40 @@
+﻿namespace Tasks.UnionFind.ConnectedComponents;
+
+public class ConnectedComponentsTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Arrange
+        int n = 3;
+        (int, int)[] edges = [
+            ( 0, 1 ), 
+            ( 0, 2 )
+        ];
+
+        // Act
+        var res = ConnectedComponents.Run( edges, n );
+
+        // Assert
+        Assert.Equal( 1, res );
+    }
+    
+    [Fact]
+    public void Run_Case2_ShouldBeOk( )
+    {
+        // Arrange
+        int n = 6;
+        (int, int)[] edges = [
+            ( 0, 1 ), 
+            ( 1, 2 ),
+            ( 2, 3 ),
+            ( 4, 5 ),
+        ];
+
+        // Act
+        var res = ConnectedComponents.Run( edges, n );
+
+        // Assert
+        Assert.Equal( 2, res );
+    }
+}
