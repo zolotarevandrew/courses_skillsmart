de857eb82998d006a45112fbbe207e0d2fd73052
a.zolotarev <a.zolotarev@travelline.ru>
2026-06-17 09:36:42 +0300

    MinimumSpanningTrees: Prim

---
+++ algorithms/Tasks/MinimumSpanningTrees/PrimAlgorithm.cs
@@ -0,0 +1,44 @@
+﻿namespace Tasks.MinimumSpanningTrees;
+
+public class PrimAlgorithm
+{
+    public class Graph
+    {
+        public Dictionary<Vertex, List<(Vertex V, int W)>> Value { get; set; }
+
+        public List<(Vertex V, int W)> Get( Vertex vertex )
+        {
+            return Value.TryGetValue( vertex, out var value ) ? value : [];
+        }
+    }
+    
+    public record Vertex( int Value );
+
+    public static int Run( Graph graph )
+    {
+        int total = 0;
+        HashSet<Vertex> visited = [];
+        PriorityQueue<Vertex, int> queue = new PriorityQueue<Vertex, int>( );
+        queue.Enqueue( new Vertex( 0 ), 0 );
+        while ( visited.Count < graph.Value.Count )
+        {
+            if ( !queue.TryDequeue( out var current, out var currentWeight ) )
+            {
+                continue;
+            }
+
+            if ( !visited.Add( current ) ) continue;
+
+            foreach ( (Vertex V, int W) item in graph.Get( current ) )
+            {
+                if (visited.Contains( item.V )) continue;
+
+                queue.Enqueue( item.V, item.W );
+            }
+            
+            total += currentWeight;
+        }
+
+        return total;
+    }
+}
