2ed97dbddd4f4b942eb6a07a521894bc4f077753
a.zolotarev <a.zolotarev@travelline.ru>
2026-06-22 09:42:43 +0300

    Backtracking

---
+++ algorithms/Tasks/Backtraking/Backtracking.cs
@@ -0,0 +1,33 @@
+﻿namespace Tasks.Backtraking;
+
+public class Backtracking
+{
+    public static List<string> GenerateBinaryString( int n )
+    {
+        List<string> generated = [];
+        GenerateBinaryStringInternal( [], n, generated );
+        return generated;
+    }
+
+    private static void GenerateBinaryStringInternal( 
+        List<char> current, 
+        int maxLength, 
+        List<string> generated )
+    {
+        // check if valid solution
+        if ( current.Count == maxLength )
+        {
+            // process solution
+            generated.Add( string.Join( "", current ) );
+            return;
+        }
+        
+        current.Add( '0' );
+        GenerateBinaryStringInternal( current, maxLength, generated );
+        current.RemoveAt( current.Count - 1 );
+
+        current.Add( '1' );
+        GenerateBinaryStringInternal( current, maxLength, generated );
+        current.RemoveAt( current.Count - 1 );
+    }
+}
---
+++ algorithms/Tasks/Backtraking/BacktrackingTests.cs
@@ -0,0 +1,16 @@
+﻿using Tasks.UnionFind.ConnectedComponents;
+
+namespace Tasks.Backtraking;
+
+public class BacktrackingTests
+{
+    [Fact]
+    public void Run_Case1_ShouldBeOk( )
+    {
+        // Act
+        var res = Backtracking.GenerateBinaryString( 3 );
+
+        // Assert
+        Assert.NotEmpty( res );
+    }
+}
