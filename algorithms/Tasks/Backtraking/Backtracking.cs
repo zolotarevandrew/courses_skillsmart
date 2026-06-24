e406c661e2c3b06cccb92661c823569018b813de
a.zolotarev <a.zolotarev@travelline.ru>
2026-06-24 09:31:40 +0300

    Backtracking: permutation

--- algorithms/Tasks/Backtraking/Backtracking.cs
+++ algorithms/Tasks/Backtraking/Backtracking.cs
@@ -2,12 +2,109 @@
 
 public class Backtracking
 {
+    static char[] Choices =
+    [
+        '0', '1'
+    ];
+    
     public static List<string> GenerateBinaryString( int n )
     {
         List<string> generated = [];
         GenerateBinaryStringInternal( [], n, generated );
         return generated;
     }
+
+    public static List<List<int>> Permutation( int[] nums )
+    {
+        List<List<int>> res = [];
+        bool[] visited = new bool[nums.Length];
+        
+        BacktrackPerm( [], visited, res, nums );
+        
+        return res;
+    }
+
+    static void BacktrackPerm(
+        List<int> path,
+        bool[] visited,
+        List<List<int>> res,
+        int[] nums )
+    {
+        if ( path.Count == nums.Length )
+        {
+            res.Add( path.ToList( ) );
+            return;
+        }
+
+        for ( int idx = 0; idx < nums.Length; idx++ )
+        {
+            if ( visited[idx] ) continue;
+            path.Add( nums[idx] );
+            visited[idx] = true; 
+            
+            BacktrackPerm( path, visited, res, nums );
+
+            path.RemoveAt( path.Count - 1 );
+            visited[idx] = false;
+        }
+    }
+
+    public static string[] NQueen( int n )
+    {
+        string[] solutions = new string[n];
+        int[,] board = new int[n, n];
+        HashSet<int> cols = [];
+        HashSet<int> diags = [];
+        HashSet<int> antiDiags = [];
+        
+        Backtrack( 0 );
+
+        return solutions;
+
+        void Backtrack( int row )
+        {
+            if ( row == n )
+            {
+                for ( int r = 0; r < n; r++ )
+                {
+                    string cur = string.Empty;
+                    for ( int c = 0; c < n; c++ )
+                    {
+                        cur += board[r, c];
+                    }
+
+                    solutions[r] = cur;
+                }
+                
+                return;
+            }
+
+            for ( int col = 0; col < n; col++ )
+            {
+                if ( cols.Contains( col )
+                    || diags.Contains( row - col )
+                    || antiDiags.Contains( row + col )
+                    )
+                {
+                    continue;
+                }
+                
+                board[row, col] = 1;
+                cols.Add( col );
+                diags.Add( row - col );
+                antiDiags.Add( row + col );
+                
+                Backtrack( row + 1 );
+                
+                board[row, col] = 0;
+                cols.Remove( col );
+                diags.Remove( row - col );
+                antiDiags.Remove( row + col );
+            }
+        }
+    }
+    
+    
 
     private static void GenerateBinaryStringInternal( 
         List<char> current, 
@@ -22,12 +119,11 @@ public class Backtracking
             return;
         }
         
-        current.Add( '0' );
-        GenerateBinaryStringInternal( current, maxLength, generated );
-        current.RemoveAt( current.Count - 1 );
-
-        current.Add( '1' );
-        GenerateBinaryStringInternal( current, maxLength, generated );
-        current.RemoveAt( current.Count - 1 );
+        foreach ( var choice in  Choices )
+        {
+            current.Add( choice );
+            GenerateBinaryStringInternal( current, maxLength, generated );
+            current.RemoveAt( current.Count - 1 );
+        }
     }
 }
--- algorithms/Tasks/Backtraking/BacktrackingTests.cs
+++ algorithms/Tasks/Backtraking/BacktrackingTests.cs
@@ -1,16 +1,47 @@
-﻿using Tasks.UnionFind.ConnectedComponents;
-
-namespace Tasks.Backtraking;
+﻿namespace Tasks.Backtraking;
 
 public class BacktrackingTests
 {
     [Fact]
-    public void Run_Case1_ShouldBeOk( )
+    public void GenerateBinaryString_ShouldBeOk( )
     {
         // Act
         var res = Backtracking.GenerateBinaryString( 3 );
 
         // Assert
         Assert.NotEmpty( res );
+    }
+    
+    [Fact]
+    public void NQueen_ShouldBeOk( )
+    {
+        // Act
+        var res = Backtracking.NQueen( 4 );
+
+        // Assert
+        Assert.NotEmpty( res );
+        Assert.Equal( res.Length, 4 );
+    }
+    
+    [Fact]
+    public void Permutation_ThreeItems_ShouldBeOk( )
+    {
+        // Act
+        List<List<int>> res = Backtracking.Permutation( [4, 5, 6] );
+
+        // Assert
+        Assert.NotEmpty( res );
+        Assert.Equal( res.Count, 6 );
+    }
+    
+    [Fact]
+    public void Permutation_OneItem_ShouldBeOk( )
+    {
+        // Act
+        List<List<int>> res = Backtracking.Permutation( [9] );
+
+        // Assert
+        Assert.NotEmpty( res );
+        Assert.Equal( res.Count, 1 );
     }
 }
